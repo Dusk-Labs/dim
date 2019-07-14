5967bf62dbdcef6550a5b48945e5945deecb9848
HinamoreCh4n <motherrussia711@gmail.com>
2019-07-14 12:37:08 +0000

    added webui, new dependency

---
+++ web_ui/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Sidebar from './components/Sidebar.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-root">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
