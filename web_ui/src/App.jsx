26790b35330cbd43d3cdfbcf41a440437dc59c82
eray_chumak <eray_chumak@protonmail.com>
2019-08-07 18:39:31 +0100

    import Main.jsx

--- web_ui/src/App.jsx
+++ web_ui/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import Main from "./components/Main.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="app-root">
+			<div className="App">
 				<Sidebar/>
+				<Main/>
 			</div>
 		);
 	}
