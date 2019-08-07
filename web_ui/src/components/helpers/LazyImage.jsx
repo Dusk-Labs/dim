3eac54f04217cce92acd9aa9429929982918c090
HinamoreCh4n <motherrussia711@gmail.com>
2019-08-07 21:09:49 +0000

    Added lazyimage

---
+++ web_ui/src/components/helpers/LazyImage.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class LazyImage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            acquired: false,
+            blob: null,
+        };
+
+        fetch(this.props.src).then((resp) => {
+            if(resp.headers.get("content-type") == "image/jpeg") {
+                return resp.blob();
+            }
+        }).then((blob) => {
+            if(blob) {
+                let blobUrl = URL.createObjectURL(blob);
+                this.setState({acquired: true, blob: blobUrl});
+            }
+        });
+    }
+
+    render() {
+        const { acquired, blob } = this.state;
+        return acquired ? (
+            <div className="image-wrapper">
+                <img src={blob} alt={blob}></img>
+            </div>
+        ) : (
+            <div className="placeholder">
+            </div>
+        );
+    }
+}
+
+export default LazyImage;
