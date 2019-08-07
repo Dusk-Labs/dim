952b443789bacab97bdd93f96df2831f6977eab2
eray_chumak <eray_chumak@protonmail.com>
2019-08-07 23:54:03 +0100

    prettify

--- web_ui/src/components/helpers/LazyImage.jsx
+++ web_ui/src/components/helpers/LazyImage.jsx
@@ -3,33 +3,36 @@ import React, { Component } from "react";
 class LazyImage extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             acquired: false,
             blob: null,
         };
 
-        fetch(this.props.src).then((resp) => {
-            if(resp.headers.get("content-type") == "image/jpeg") {
-                return resp.blob();
-            }
-        }).then((blob) => {
-            if(blob) {
-                let blobUrl = URL.createObjectURL(blob);
-                this.setState({acquired: true, blob: blobUrl});
-            }
-        });
+        fetch(this.props.src)
+            .then(resp => {
+                if (resp.headers.get("content-type") === "image/jpeg") {
+                    return resp.blob();
+                }
+            })
+            .then((blob) => {
+                if (blob) {
+                    const blobUrl = URL.createObjectURL(blob);
+
+                    this.setState({
+                        acquired: true,
+                        blob: blobUrl
+                    });
+                }
+            });
     }
 
     render() {
         const { acquired, blob } = this.state;
-        return acquired ? (
-            <div className="image-wrapper">
-                <img src={blob} alt={blob}></img>
-            </div>
-        ) : (
-            <div className="placeholder">
-            </div>
-        );
+
+        return acquired
+            ? <div className="image-wrapper"><img src={blob} alt={blob}></img></div>
+            : <div className="placeholder"></div>;
     }
 }
 
