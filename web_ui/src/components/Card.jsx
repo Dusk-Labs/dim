a68b58a691114900eae758bc12010cb4d8d17dd1
eray_chumak <eray_chumak@protonmail.com>
2019-08-09 21:23:33 +0100

    add hover method to card component

--- web_ui/src/components/Card.jsx
+++ web_ui/src/components/Card.jsx
@@ -1,18 +1,56 @@
 import React, { Component } from "react";
 import LazyImage from "./helpers/LazyImage.jsx";
+import CardPopup from "./Card-Popup.jsx";
 import "./card.scss";
 
 class Card extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleMouseHover = this.handleMouseHover.bind(this);
+        this.handleMouseUnHover = this.handleMouseUnHover.bind(this);
+        this.state = {
+            hovering: false
+        };
+    }
+
+    handleMouseHover() {
+        this.setState({
+            hovering: true
+        });
+    }
+
+    handleMouseUnHover() {
+        this.setState({
+            hovering: false
+        });
+    }
+
     render() {
         const { name, src } = this.props;
 
         return (
-            <div className="card">
-                <a href="https://example.com/">
-                    <LazyImage alt={"cover-" + name} src={src}></LazyImage>
-                    <p>{name}</p>
-                </a>
-            </div>
+            !this.state.hovering
+                ? (
+                    <div className="card" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseUnHover}>
+                        <a href="https://example.com/">
+                            <LazyImage alt={"cover-" + name} src={src}></LazyImage>
+                            <p>{name}</p>
+                        </a>
+                    </div>
+                ) : (
+                    <CardPopup
+                        name="Spiderman: Far From Home"
+                        imdb="8.7"
+                        rotten_tomatoes="10"
+                        description="Cba"
+                        genre="Sci-Fi"
+                        year="2019"
+                        trailer=""
+                        length="02:00:00"
+                        play=""
+                    />
+                )
         );
     }
 }
