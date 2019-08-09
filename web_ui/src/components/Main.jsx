46e9d0eab0e42a56c7ac5e13e8a61ace8f0ee209
HinamoreCh4n <motherrussia711@gmail.com>
2019-08-09 00:22:34 +0000

    Minor fix

--- web_ui/src/components/Main.jsx
+++ web_ui/src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Card from "./Card.jsx";
+import LazyImage from "./helpers/LazyImage.jsx";
 import "./main.css";
 
 class Main extends Component {
@@ -7,7 +8,7 @@ class Main extends Component {
         return (
             <main>
                 <section className="banner">
-                    <img alt="banner" src="https://pbs.twimg.com/media/DBVhqetV0AE_yEd.jpg"></img>
+                    <LazyImage alt="banner" src="/banner1.jpg"></LazyImage>
                     <div className="info">
                         <h1>THE 100</h1>
                         <div className="desc">
@@ -29,43 +30,43 @@ class Main extends Component {
                         <div className="cards">
                             <Card
                                 name="Spider-Man: Far From Home"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="Men in Black: International"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="Fast & Furious Presents: Hobbs & Shaw"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="The Lion King"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="Toy Story 4"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="The Angry Birds 2"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="Annabelle Comes Home"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="Playmobil: The Movie"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="The Sun is Also a Star"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                             <Card
                                 name="Alladin"
-                                src="http://localhost:3000/img1.jpg"
+                                src="/img1.jpg"
                             ></Card>
                         </div>
                     </div>
