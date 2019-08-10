ebf8e2cdd79f7255b72a180354ca1ba958602791
eray_chumak <eray_chumak@protonmail.com>
2019-08-10 01:58:23 +0100

    prep progress bar for api

--- web_ui/src/components/progress-bar.jsx
+++ web_ui/src/components/progress-bar.jsx
@@ -8,25 +8,69 @@ import { faCircle } from "@fortawesome/free-solid-svg-icons";
 library.add(faCircle);
 
 class ProgressBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: {}
+        };
+    }
+
+    componentDidMount() {
+        // fetch(`api/${this.props.id}`)
+        //     .then(res => res.json())
+        //     .then(({data}) => {
+        //         this.setState({
+        //             data: {
+        //                 current: ,
+        //                 currentUnit: ,
+        //                 duration: ,
+        //                 durationUnit: ,
+        //                 season: ,
+        //                 episode:
+        //             }
+        //         })
+        //     });
+        this.setState({
+            data: {
+                current: 23,
+                currentUnit: "min",
+                duration: 53,
+                durationUnit: "min",
+                season: "01",
+                episode: "01"
+            }
+        })
+    }
+
     render() {
+        const {
+            current,
+            currentUnit,
+            duration,
+            durationUnit,
+            season,
+            episode
+        } = this.state.data;
+
         return (
             <div className="progress-bar">
                 <div className="s-e">
-                    <p>S01</p>
+                    <p>S{season}</p>
                     <FontAwesomeIcon icon="circle"/>
-                    <p>E01</p>
+                    <p>E{episode}</p>
                 </div>
                 <div className="progress">
                     <div className="current">
-                        <p>23</p>
-                        <p>min</p>
+                        <p>{current}</p>
+                        <p>{currentUnit}</p>
                     </div>
                     <div className="bar">
-                        <progress max="53" value="23"></progress>
+                        <progress max={duration} value={current}></progress>
                     </div>
                     <div className="duration">
-                        <p>23</p>
-                        <p>min</p>
+                        <p>{duration}</p>
+                        <p>{durationUnit}</p>
                     </div>
                 </div>
             </div>
