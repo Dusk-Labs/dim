de0528dbadf53b83253496f8f4676e722a293681
eray_chumak <eray_chumak@protonmail.com>
2019-08-12 03:29:43 +0100

    move banners to component state

--- web_ui/src/components/Main.jsx
+++ web_ui/src/components/Main.jsx
@@ -15,17 +15,33 @@ class Main extends Component {
 
         this.state = {
             cards: {},
+            banners: [
+                {
+                    name: "The 100",
+                    src: "/banner1.jpg",
+                    desc: "Set ninety-seven years after a nuclear war has destroyed civilization, when a spaceship housing humanity's lone survivors sends one hundred juvenile delinquents back to Earth, in hopes of possibly re-populating the planet."
+                },
+                {
+                    name: "Once Upon a Time in Hollywood",
+                    src: "/banner4.jpg",
+                    desc: "A faded television actor and his stunt double strive to achieve fame and success in the film industry during the final years of Hollywood's Golden Age in 1969 Los Angeles."
+                },
+                {
+                    name: "The Expanse",
+                    src: "/banner3.jpg",
+                    desc: "A thriller set two hundred years in the future following the case of a missing young woman who brings a hardened detective and a rogue ship's captain together in a race across the solar system to expose the greatest conspiracy in human history."
+                }
+            ]
         };
-        this.fetchCards();
     }
 
-    fetchCards = async () => {
-        const cardReq = await fetch(`http://86.21.150.167:8000/api/v1/library/1/media`);
+    async componentDidMount() {
+        const cardReq = await fetch("http://86.21.150.167:8000/api/v1/library/1/media");
         const json = await cardReq.json();
         const cards = json.map(item => <Card key={item.id} data={item} src={item.poster_path}/>);
 
         this.setState({
-            cards: { RECOMMENDED: cards },
+            cards: { recommended: cards },
         });
     }
 
@@ -41,22 +57,11 @@ class Main extends Component {
             </div>
         });
 
-        const title1 = "THE 100";
-        const desc1 = "Set ninety-seven years after a nuclear war has destroyed civilization, when a spaceship housing humanity's lone survivors sends one hundred juvenile delinquents back to Earth, in hopes of possibly re-populating the planet.";
-
-        const title2 = "Once Upon a Time in Hollywood";
-        const desc2 = "A faded television actor and his stunt double strive to achieve fame and success in the film industry during the final years of Hollywood's Golden Age in 1969 Los Angeles.";
-
-        const title3 = "THE EXPANSE";
-        const desc3 = "A thriller set two hundred years in the future following the case of a missing young woman who brings a hardened detective and a rogue ship's captain together in a race across the solar system to expose the greatest conspiracy in human history.";
-
         return (
             <main>
                 <section className="banner">
                     <BannerPages>
-                        <Banner src="/banner1.jpg" title={title1} description={desc1}/>
-                        <Banner src="/result2.jpg" title={title2} description={desc2}/>
-                        <Banner src="/result1.jpg" title={title3} description={desc3}/>
+                        {this.state.banners.map(({name, src, desc}) => <Banner src={src} title={name} description={desc}/>)}
                     </BannerPages>
                 </section>
                 <section className="libraries">
