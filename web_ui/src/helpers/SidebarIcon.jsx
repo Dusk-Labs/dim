b4e111ff3b10a6f663368766ce731df6a19981ac
eray_chumak <eray_chumak@protonmail.com>
2019-08-22 01:06:15 +0100

    add helper component to find fontawesome icons based on name and media_type

---
+++ web_ui/src/helpers/SidebarIcon.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class SidebarIcon extends Component {
+    render() {
+        let icon;
+
+        switch(this.props.icon.toLowerCase()) {
+            case "movies":
+                icon = "film";
+                break;
+            case "desktop":
+                icon = "desktop";
+                break;
+            case "laptop":
+                icon = "laptop";
+                break;
+            case "phone":
+                icon = "mobile-alt";
+                break;
+            default:
+                icon = "folder";
+                break;
+        }
+
+        return <FontAwesomeIcon icon={icon}/>
+    }
+}
+
+export default SidebarIcon;
