ffaff7acb651a7c48c054c5dcc701a5338adfef4
eray_chumak <eray_chumak@protonmail.com>
2019-08-26 06:49:24 +0100

    add search results and scroll to selected founded item and fix input not allowing more chars + visibility toggles

--- web_ui/src/helpers/SidebarSearch.jsx
+++ web_ui/src/helpers/SidebarSearch.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
 import Modal from "react-modal";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,20 +11,44 @@ class SidebarSearch extends Component {
     constructor(props) {
         super(props);
 
-        this.onChange = this.onChange.bind(this);
-        this.toggleFocus = this.toggleFocus.bind(this);
+        this.searchBox = React.createRef();
+        this.inputBox = React.createRef();
+
         this.selectOption = this.selectOption.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.toggleShowOptions = this.toggleShowOptions.bind(this);
+        this.toggleShowSearchFor = this.toggleShowSearchFor.bind(this);
 
         this.state = {
             query: "",
             showSearchFor: false,
             showOptions: false,
             options: [],
-            selectedOptions: []
+            selectedOptions: [],
+            results: []
         };
     }
 
     componentDidMount() {
+        // HIDES POPUPS IF CLICKED OUTSIDE COMPONENT
+        document.addEventListener("click", (e) => {
+            if (!this.searchBox.current.contains(e.target)) {
+                if (this.state.showOptions) {
+                    this.setState({
+                        showOptions: false
+                    });
+                }
+
+                if (this.state.showSearchFor) {
+                    this.setState({
+                        showSearchFor: false
+                    });
+                }
+            }
+        });
+
+        this.inputBox.current.addEventListener("input", this.onChange.bind(this));
+
         const json = [
             "name",
             "genre",
@@ -46,23 +70,37 @@ class SidebarSearch extends Component {
         });
     }
 
+    componentWillUnmount() {
+        document.removeEventListener("click");
+        this.inputBox.current.removeEventListener("input");
+    }
+
     selectOption(option) {
         if (this.state.selectedOptions.includes(option)) return;
 
+        this.setState(prevState => ({
+            selectedOptions: [...prevState.selectedOptions, option]
+        }));
+    }
+
+    onChange = async (e) => {
         this.setState({
-            selectedOptions: [this.state.selectedOptions, option]
+            query: e.target.textContent.substr(0, 20),
+            showSearchFor: e.target.textContent.length !== 0,
+            showOptions: e.target.textContent.length === 0
+        });
+
+        if (this.state.query.length === 0) return;
+
+        const reqResults = await fetch(`http://86.21.150.167:8000/api/v1/search?query=${this.state.query}&quick=true`);
+        const results = await reqResults.json();
+
+        this.setState({
+            results
         });
     }
 
-    onChange(event) {
-        this.setState({
-            query: event.target.value.substr(0, 20),
-            showSearchFor: event.target.value.length !== 0,
-            showOptions: event.target.value.length === 0
-        });
-    }
-
-    toggleFocus() {
+    toggleShowOptions() {
         if (this.state.query.length > 0) return;
 
         this.setState({
@@ -70,24 +108,63 @@ class SidebarSearch extends Component {
         });
     }
 
+    toggleShowSearchFor() {
+        if (this.state.query.length === 0) return;
+
+        this.setState({
+            showSearchFor: !this.state.showSearchFor
+        });
+    }
+
+    // prevents multi-line (on enter)
+    handleOnKeyDown = (e) => {
+        if (e.which === 13) {
+            e.preventDefault();
+        }
+    }
+
     render() {
         const { options } = this.state;
 
+        const results = (
+            <div className="results">
+                <p>{this.state.results.length} RESULTS</p>
+                <div className="result-list">
+                    {this.state.results.map((
+                        { name, library_id, id }, i
+                    ) => {
+                        return (
+                            <HashLink
+                                to={`/library/${library_id}#${id}`}
+                                scroll={el => {
+                                    el.scrollIntoView({ behavior: "smooth", block: "center" });
+                                    el.style.animation = "cardGlow 3s ease-in-out infinite";
+                                }}
+                                onClick={this.toggleShowSearchFor}
+                                key={i}>{name}</HashLink>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+
         return (
-            <div className="search-box">
+            <div className="search-box" ref={this.searchBox}>
                 <div className="search-box-wrapper">
-                    <input
-                        type="text"
-                        placeholder="SEARCH"
+                    <div className="input"
+                        ref={this.inputBox}
+                        onKeyDown={this.handleOnKeyDown}
+                        contentEditable="true"
                         value={this.state.query}
                         onChange={this.onChange}
-                        onFocus={this.toggleFocus}/>
-                    <button type="submit">
+                        onFocus={this.toggleShowOptions}>
+                    </div>
+                    <button type="submit" onClick={this.onSubmit}>
                         <FontAwesomeIcon icon="search"/>
                     </button>
                 </div>
                 {this.state.showOptions &&
-                    <div className="search-box-options" onBlur={this.toggleFocus}>
+                    <div className="search-box-options">
                         <header>
                             <p>SEARCH OPTIONS</p>
                         </header>
@@ -99,6 +176,7 @@ class SidebarSearch extends Component {
                     <div className="search-box-search-for">
                         <p>SEARCH FOR: <span id="query">{this.state.query}</span></p>
                         { this.state.selectedOptions.map((option, i) => <p key={i}>{option}</p>)}
+                        { results }
                     </div>}
             </div>
         );
