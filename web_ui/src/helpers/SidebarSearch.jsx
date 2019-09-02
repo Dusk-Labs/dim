03238a50d51bbb9937b171675edca8f6f1464b35
EukalyptenHD <eukalypteneuka@gmail.com>
2019-09-02 19:14:53 +0100

    add error handling for search reqs and add missing placeholder
    Former-commit-id: 13f814962be42a75a34bece0503e5d53e36d4107

--- web_ui/src/helpers/SidebarSearch.jsx
+++ web_ui/src/helpers/SidebarSearch.jsx
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
-import { HashLink } from "react-router-hash-link";
-import Modal from "react-modal";
-import ContentEditable from "react-contenteditable";
-import sanitizeHtml from "sanitize-html";
+import { HashLink } from 'react-router-hash-link';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./SidebarSearch.scss";
-
-Modal.setAppElement("body");
 
 class SidebarSearch extends Component {
     constructor(props) {
@@ -16,49 +11,21 @@ class SidebarSearch extends Component {
         this.searchBox = React.createRef();
         this.inputBox = React.createRef();
 
-        this.selectOption = this.selectOption.bind(this);
         this.onChange = this.onChange.bind(this);
-        this.toggleShowOptions = this.toggleShowOptions.bind(this);
         this.toggleShowSearchFor = this.toggleShowSearchFor.bind(this);
+        this.handleDocumentClick = this.handleDocumentClick.bind(this);
 
         this.state = {
             query: "",
             showSearchFor: false,
-            showOptions: false,
-            options: [],
-            selectedOptions: [],
-            results: []
+            results: null,
+            showPlaceholder: true
         };
     }
 
     componentDidMount() {
-        // HIDES POPUPS IF CLICKED OUTSIDE COMPONENT
-        // ! THIS NEEDS TO BE MOVED (CREATE REF ETC).
-        document.addEventListener("click", (e) => {
-            if (!this.searchBox.current.contains(e.target)) {
-                if (this.state.showOptions) {
-                    this.setState({
-                        showOptions: false
-                    });
-                }
-
-                if (this.state.showSearchFor) {
-                    this.setState({
-                        showSearchFor: false
-                    });
-                }
-            }
-        });
-
+        document.addEventListener("click", this.handleDocumentClick);
         this.inputBox.current.addEventListener("input", this.onChange.bind(this));
-
-        const json = [
-            "genre"
-        ];
-
-        this.setState({
-            options: json
-        });
     }
 
     componentWillUnmount() {
@@ -66,89 +33,82 @@ class SidebarSearch extends Component {
         this.inputBox.current.removeEventListener("input");
     }
 
-    // ! DOES NOT INSERT TAG INTO INPUT
-    selectOption(option) {
-        if (this.state.selectedOptions.includes(option)) return;
-
-        let { options } = this.state;
-        const index = options.indexOf(option);
-
-        if (index !== -1) {
-            options.splice(index, 1);
-        }
-
-        this.setState(prevState => ({
-            selectedOptions: [...prevState.selectedOptions, option]
-        }));
-    }
-
-    // ! DOES NOT REMOVE TAG INTO INPUT
-    unselectOption(option) {
-        if (this.state.options.includes(option)) return;
-
-        let { selectedOptions } = this.state;
-        const index = selectedOptions.indexOf(option);
-
-        if (index !== -1) {
-            selectedOptions.splice(index, 1);
-        }
-
-        this.setState(prevState => ({
-            options: [...prevState.options, option]
-        }));
-    }
-
-    // ! MISSING MANUAL TAG DELETION
-    // ! STYLING GETS LOST ON UNFOCUS.
-    onChange = async (e) => {
+    async handle_req(promise) {
         try {
-            let query = e.target.value
+            return await (await promise).json();
+        } catch (err) {
+            return { err: err };
+        }
+    }
 
-            // tag: (for styling and adding/removing from selectedOptions)
-            const findTag = RegExp(/([A-z0-9]{1,}:)/gi);
-
-            // tag: value
-            const findTagValue = RegExp(/([A-z0-9]{1,}:)[ ]?([A-z0-9 ]{1,})/gi);
-
-            if (findTag.test(query)) {
-                const regex = /(<([^>]+)>)/ig;
-                query = query.replace(regex, "");
+    handleDocumentClick(e) {
+        // hides popups if clicked outside component
+        if (!this.searchBox.current.contains(e.target)) {
+            if (this.state.showSearchFor) {
+                this.setState({
+                    showSearchFor: false
+                });
             }
+        }
+    }
 
-            if (query.match(findTag)) {
-                const [ tag ] = query.match(findTag);
-                const tag_name = tag.replace(":", "");
-                const available = this.state.options.includes(tag_name) || this.state.selectedOptions.includes(tag_name);
+    onChange = (e) => {
+        this.setState({
+            query: e.target.textContent.substr(0, 20),
+            showSearchFor: e.target.textContent.length !== 0,
+            results: <p id="response">LOADING</p>
+        });
 
-                if (available) {
-                    query = query.replace(findTag, `<span>${tag}</span>`);
-                    this.selectOption(tag_name);
-                }
-            }
+        if (this.state.query.length >= 1) {
+            this.getResults();
 
             this.setState({
-                query,
-                showSearchFor: e.target.value.length !== 0,
-                showOptions: e.target.value.length === 0
+                showPlaceholder: false
             });
-
-            if (this.state.query.length === 0) return;
-
-            // ! UN-COMMENT WHEN FUNCTIONING
-            // const reqResults = await fetch(`http://86.21.150.167:8000/api/v1/search?query=${this.state.query}&quick=true`);
-            // const results = await reqResults.json();
-
-            // this.setState({
-            //     results
-            // });
-        } catch {}
+        } else {
+            this.setState({
+                showPlaceholder: true
+            })
+        }
     }
 
-    toggleShowOptions() {
-        if (this.state.query.length > 0) return;
+    togglePlaceholder() {
+        this.setState({
+            showPlaceholder: !this.state.showPlaceholder
+        });
+    }
+
+    async getResults() {
+        const reqResults = fetch(`http://86.21.150.167:8000/api/v1/search?query=${this.state.query}&quick=true`);
+        const results = await this.handle_req(reqResults);
+
+        if (results.err) {
+            return this.setState({
+                results: <p id="response">FAILED TO LOAD</p>
+            });
+        }
+
+        const list = results.map((
+            { name, library_id, id }, i
+        ) => <HashLink
+                to={`/library/${library_id}#${id}`}
+                scroll={elm => {
+                    elm.scrollIntoView({ behavior: "smooth", block: "center" });
+                    elm.style.animation = "cardGlow 3s ease-in-out infinite";
+                }}
+                onClick={this.toggleShowSearchFor}
+                key={i}>
+                {name}
+            </HashLink>
+        );
 
         this.setState({
-            showOptions: !this.state.showOptions
+            results: (
+                <div className="results">
+                    <p>{list.length} RESULTS</p>
+                    <div className="result-list">{list}</div>
+                </div>
+            )
         });
     }
 
@@ -167,84 +127,28 @@ class SidebarSearch extends Component {
         }
     }
 
-    sanitize = () => {
-        this.setState({ query: sanitizeHtml(this.state.query)});
-    };
-
     render() {
-        const availableOptions = this.state.options.length > 0
-            ? (this.state.options.map((option, i) => {
-                return (
-                    <div key={i} className="option" onClick={() => this.selectOption(option)}>
-                        <p>{option}:</p>
-                        <FontAwesomeIcon icon="plus"/>
-                    </div>)
-                })
-            ) : <p id="response">NO MORE OPTIONS</p>;
-
-        const selectedOptions = this.state.selectedOptions.length > 0
-            ? (this.state.selectedOptions.map((option, i) => {
-                return (
-                    <div key={i} className="option" onClick={() => this.unselectOption(option)}>
-                        <p>{option}:</p>
-                        <FontAwesomeIcon icon="minus"/>
-                    </div>)
-                })
-            ) : <p id="response">NONE ADDED</p>;
-
-        const results = (
-            <div className="results">
-                <p>{this.state.results.length} RESULTS</p>
-                <div className="result-list">
-                    {this.state.results.map((
-                        { name, library_id, id }, i
-                    ) => {
-                        return (
-                            <HashLink
-                                to={`/library/${library_id}#${id}`}
-                                scroll={el => {
-                                    el.scrollIntoView({ behavior: "smooth", block: "center" });
-                                    el.style.animation = "cardGlow 3s ease-in-out infinite";
-                                }}
-                                onClick={this.toggleShowSearchFor}
-                                key={i}>
-                                {name}
-                            </HashLink>
-                        );
-                    })}
-                </div>
-            </div>
-        );
-
         return (
             <div className="search-box" ref={this.searchBox}>
                 <div className="search-box-wrapper">
-                    <ContentEditable
-                        className="input"
-                        innerRef={this.inputBox}
-                        html={this.state.query}
-                        onChange={this.onChange}
-                        onBlur={this.sanitize}/>
+                    <div className="input"
+                        ref={this.inputBox}
+                        onKeyDown={this.handleOnKeyDown}
+                        contentEditable="true"
+                        value={this.state.query}
+                        onChange={this.onChange}>
+                    </div>
+                    {this.state.showPlaceholder &&
+                        <span id="placeholder">SEARCH</span>
+                    }
                     <button type="submit" onClick={this.onSubmit}>
                         <FontAwesomeIcon icon="search"/>
                     </button>
                 </div>
-                {this.state.showOptions &&
-                    <div className="search-box-options">
-                        <header>
-                            <p>SEARCH OPTIONS</p>
-                        </header>
-                        <div className="options">
-                            { availableOptions }
-                        </div>
-                        <div className="options">
-                            { selectedOptions }
-                        </div>
-                    </div>}
                 {this.state.showSearchFor &&
                     <div className="search-box-search-for">
                         <p>SEARCH FOR: <span id="query">{this.state.query}</span></p>
-                        { results }
+                        { this.state.results }
                     </div>}
             </div>
         );
