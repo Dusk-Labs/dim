b853ad27613e7d81266c82a561daab302945fe6c
HinamoreCh4n <motherrussia711@gmail.com>
2019-10-03 17:07:25 +0000

    Added scrollbar to sidebar elems

--- web_ui/src/layouts/Sidebar.jsx
+++ web_ui/src/layouts/Sidebar.jsx
@@ -7,6 +7,7 @@ import SidebarSearch from "../helpers/SidebarSearch.jsx";
 import SidebarIcon from "../helpers/SidebarIcon.jsx";
 import LazyImage from "../helpers/LazyImage.jsx";
 import ProfileIcon from "../assets/profile_icon.jpg";
+import { Scrollbar } from "react-scrollbars-custom";
 import "./Sidebar.scss";
 
 Modal.setAppElement("body");
@@ -188,7 +189,9 @@ class Sidebar extends Component {
                         <h4>CONNECTED HOSTS</h4>
                     </header>
                     <div className="list">
+                        <Scrollbar>
                         {this.state.connectedHosts}
+                            </Scrollbar>
                     </div>
                 </section>
 
@@ -211,6 +214,7 @@ class Sidebar extends Component {
                         </Modal>
                     </header>
                     <div className="list">
+                        <Scrollbar>
                         <div className="item-wrapper">
                             <NavLink to="/" exact>
                                 <FontAwesomeIcon icon="home"/>
@@ -218,6 +222,7 @@ class Sidebar extends Component {
                             </NavLink>
                         </div>
                         {this.state.libraries}
+                    </Scrollbar>
                     </div>
                 </section>
 
