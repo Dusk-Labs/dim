2f063cd8847a2786e34ec4e443d65887748ffc2f
Eray Chumak <eray_chumak@protonmail.com>
2019-10-19 02:39:40 +0100

    add newLibary and delLibrary action cases

--- web_ui/src/reducers/libraryReducer.js
+++ web_ui/src/reducers/libraryReducer.js
@@ -4,15 +4,37 @@ import {
     FETCH_LIBRARIES_ERR,
     FETCH_LIBRARY_INFO,
     FETCH_LIBRARY_MEDIA,
-    NEW_LIBRARY,
-    DEL_LIBRARY
+    NEW_LIBRARY_START,
+    NEW_LIBRARY_OK,
+    NEW_LIBRARY_ERR,
+    DEL_LIBRARY_START,
+    DEL_LIBRARY_OK,
+    DEL_LIBRARY_ERR,
 } from "../actions/types.js";
 
-const initialState = {
+const fetch_libraries = {
     items: [],
     fetching: false,
     fetched: false,
     error: null
+};
+
+const new_library = {
+    creating: false,
+    created: false,
+    error: null
+};
+
+const del_library = {
+    deleting: false,
+    deleted: false,
+    error: null
+};
+
+const initialState = {
+    fetch_libraries,
+    new_library,
+    del_library
 };
 
 export default function(state = initialState, action) {
@@ -20,30 +42,87 @@ export default function(state = initialState, action) {
         case FETCH_LIBRARIES_START:
             return {
                 ...state,
-                fetching: true
+                fetch_libraries: {
+                    ...fetch_libraries,
+                    fetching: true
+                }
             }
         case FETCH_LIBRARIES_OK:
             return {
                 ...state,
-                fetching: false,
-                fetched: true,
-                items: action.payload
+                fetch_libraries: {
+                    ...fetch_libraries,
+                    fetching: false,
+                    fetched: true,
+                    items: action.payload
+                }
             }
         case FETCH_LIBRARIES_ERR:
             return {
                 ...state,
-                fetching: false,
-                fetched: true,
-                error: action.payload
+                fetch_libraries: {
+                    ...fetch_libraries,
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
             }
         case FETCH_LIBRARY_INFO:
             return state;
         case FETCH_LIBRARY_MEDIA:
             return state;
-        case NEW_LIBRARY:
-            return state;
-        case DEL_LIBRARY:
-            return state;
+        case NEW_LIBRARY_START:
+            return {
+                ...state,
+                new_library: {
+                    ...new_library,
+                    creating: true,
+                }
+            }
+        case NEW_LIBRARY_OK:
+            return {
+                ...state,
+                new_library: {
+                    ...new_library,
+                    creating: false,
+                    created: true
+                }
+            }
+        case NEW_LIBRARY_ERR:
+            return {
+                ...state,
+                new_library: {
+                    creating: false,
+                    created: false,
+                    error: action.payload
+                }
+            }
+        case DEL_LIBRARY_START:
+            return {
+                ...state,
+                del_library: {
+                    ...del_library,
+                    deleting: true,
+                }
+            }
+        case DEL_LIBRARY_OK:
+            return {
+                ...state,
+                del_library: {
+                    ...del_library,
+                    deleting: false,
+                    deleted: true
+                }
+            }
+        case DEL_LIBRARY_ERR:
+            return {
+                ...state,
+                del_library: {
+                    deleting: false,
+                    deleted: false,
+                    error: action.payload
+                }
+            }
         default:
             return state;
     }
