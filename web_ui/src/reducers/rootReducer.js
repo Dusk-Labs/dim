f3fc55824ce707eb8bf4f8a651dd1d68fbbc89d5
Eray Chumak <eray_chumak@protonmail.com>
2019-10-19 02:39:08 +0100

    add fileBrowserReducer

--- web_ui/src/reducers/rootReducer.js
+++ web_ui/src/reducers/rootReducer.js
@@ -5,11 +5,13 @@ import hostReducer from "./hostReducer.js";
 import userReducer from "./userReducer.js";
 import dashboardReducer from "./dashboardReducer.js";
 import bannerReducer from "./bannerReducer.js";
+import fileBrowserReducer from "./fileBrowserReducer.js";
 
 export default combineReducers({
     user: userReducer,
     hosts: hostReducer,
-    libraries: libraryReducer,
+    libraryReducer,
+    fileBrowserReducer,
     dashboard: dashboardReducer,
     banners: bannerReducer
 });
