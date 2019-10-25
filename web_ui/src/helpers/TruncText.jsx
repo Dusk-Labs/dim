3be9d9391879a397b911f2652b6d72c18b7e9752
Eray Chumak <eray_chumak@protonmail.com>
2019-10-25 02:53:49 +0100

    make TruncText component return Fragment

--- web_ui/src/helpers/TruncText.jsx
+++ web_ui/src/helpers/TruncText.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class TruncText extends Component {
     render() {
         const words = this.props.content.split(" ");
 
         if (words.length < this.props.max) {
-            return <p>{words.join(" ")}</p>
+            return <Fragment>{words.join(" ")}</Fragment>
         }
 
         words.length = this.props.max;
-        return <p>{words.join(" ") + "..."}</p>
+        return <Fragment>{words.join(" ") + "..."}</Fragment>
     }
 }
 
