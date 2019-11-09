df73e860964d5f143c72e479a2d2badd64c5a79c
HinamoreCh4n <motherrussia711@gmail.com>
2019-11-09 17:54:44 +0000

    Added some docstrings to some major models, including examples and unit tests

--- src/database/src/lib.rs
+++ src/database/src/lib.rs
@@ -3,10 +3,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use]
-extern crate diesel;
-extern crate diesel_migrations;
-#[macro_use]
 extern crate serde;
+#[macro_use]
+extern crate diesel;
 
 pub mod episode;
 pub mod genre;
@@ -19,14 +18,27 @@ pub mod season;
 pub mod streamablemedia;
 pub mod tv;
 
+/// Function which returns a Result<T, E> where T is a new connection session or E is a connection
+/// error.
+///
+/// # Example
+/// ```
+/// use dim_database::get_conn;
+///
+/// let conn = get_conn().unwrap(); // panics if connection failed.
+/// ```
 pub fn get_conn() -> Result<diesel::PgConnection, diesel::result::ConnectionError> {
     use diesel::connection::Connection;
     use diesel::pg::PgConnection;
+
+    // This is the URL for the database inside a docker container
     let conn = PgConnection::establish("postgres://postgres:dimpostgres@postgres/dim");
 
     if conn.is_ok() {
         conn
     } else {
+        // If we cant connect to the docker URL, assume we are not running inside docker and
+        // connect to localhost instead.
         PgConnection::establish("postgres://postgres:dimpostgres@127.0.0.1/dim")
     }
 }
