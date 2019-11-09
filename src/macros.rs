82ddf9e8602aedb48562eb3ca27a790a1089d149
HinamoreCh4n <motherrussia711@gmail.com>
2019-11-09 17:55:16 +0000

    Fixed some deadcode
    Former-commit-id: 8e2681910b8ebf01a00daef270e1ee4e396ea040

--- src/macros.rs
+++ src/macros.rs
@@ -5,15 +5,6 @@ use parking_lot::Mutex;
 pub static DB_LOCK: Mutex<()> = Mutex::new(());
 
 #[macro_export]
-macro_rules! insert {
-    ($conn:expr, $table:expr, $aggregate:expr) => {
-        diesel::insert_into($table)
-            .values(&$aggregate)
-            .execute($conn);
-    };
-}
-
-#[macro_export]
 macro_rules! run_test {
     (|$client:ident| $block:expr) => {{
         let _lock = DB_LOCK.lock();
