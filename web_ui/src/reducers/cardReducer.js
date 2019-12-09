05aa16988fc5c716e4357f1d01a051474f48bba0
Eray Chumak <eray_chumak@protonmail.com>
2019-12-09 20:27:36 +0000

    add fetch card method
    Former-commit-id: bf521e582c4e6e4ab8895e1d5131138a5a41784c

--- web_ui/src/reducers/cardReducer.js
+++ web_ui/src/reducers/cardReducer.js
@@ -1,38 +1,93 @@
 import {
     FETCH_CARDS_START,
     FETCH_CARDS_OK,
-    FETCH_CARDS_ERR
- } from "../actions/types.js";
+    FETCH_CARDS_ERR,
+    FETCH_CARD_START,
+    FETCH_CARD_OK,
+    FETCH_CARD_ERR
+} from "../actions/types.js";
 
-const initialState = {
+const fetch_cards = {
     items: [],
     fetching: false,
     fetched: false,
     error: null
 };
 
+const fetch_card = {
+    info: {},
+    fetching: false,
+    fetched: false,
+    error: null
+};
+
+
+const initialState = {
+    fetch_cards,
+    fetch_card
+};
+
 export default function(state = initialState, action) {
     switch(action.type) {
         case FETCH_CARDS_START:
             return {
-                items: [],
-                fetching: true,
-                fetched: false,
-                error: null
+                ...state,
+                fetch_cards: {
+                    items: [],
+                    fetching: true,
+                    fetched: false,
+                    error: null
+                }
             }
         case FETCH_CARDS_OK:
             return {
-                items: action.payload,
-                fetching: false,
-                fetched: true,
-                error: null
+                ...state,
+                fetch_cards: {
+                    items: action.payload,
+                    fetching: false,
+                    fetched: true,
+                    error: null
+                }
             }
         case FETCH_CARDS_ERR:
             return {
-                items: [],
-                fetching: false,
-                fetched: true,
-                error: action.payload
+                ...state,
+                fetch_cards: {
+                    items: [],
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
+            }
+        case FETCH_CARD_START:
+            return {
+                ...state,
+                fetch_card: {
+                    info: {},
+                    fetching: true,
+                    fetched: false,
+                    error: null
+                }
+            }
+        case FETCH_CARD_OK:
+            return {
+                ...state,
+                fetch_card: {
+                    info: action.payload,
+                    fetching: false,
+                    fetched: true,
+                    error: null
+                }
+            }
+        case FETCH_CARD_ERR:
+            return {
+                ...state,
+                fetch_card: {
+                    info: {},
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
             }
         default:
             return state;
