142dba3e3ea8c532fad3dcf3bf79de9ad8924d9f
mental <m3nta1@yahoo.com>
2019-12-10 09:27:15 +0000

    Check presence of ffmpeg and ffprobe

--- src/streaming/src/lib.rs
+++ src/streaming/src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(result_map_or_else)]
 extern crate database;
+extern crate lazy_static;
 extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
@@ -8,5 +9,35 @@ extern crate uuid;
 pub mod ffmpeg;
 pub mod ffprobe;
 
-pub static FFMPEG_BIN: &str = "/usr/bin/ffmpeg";
-pub static FFPROBE_BIN: &str = "/usr/bin/ffprobe";
+use lazy_static::lazy_static;
+
+macro_rules! which {
+    ($prog:expr) => {
+        String::from_utf8(Command::new("which").arg($prog).output().unwrap().stdout)
+            .expect("Failed to decode `wich $prog`.")
+            .into_boxed_str()
+    };
+}
+
+lazy_static! {
+    pub static ref FFMPEG_BIN: Box<str> = { which!("ffmpeg") };
+    pub static ref FFPROBE_BIN: Box<str> = { which!("ffprobe") };
+}
+
+use std::process::Command;
+
+pub fn ffcheck<'a>(bucket: &'a mut Vec<Box<str>>) -> Result<(), Box<&str>> {
+    for program in ["ffmpeg", "ffprobe"].iter() {
+        if let Ok(output) = Command::new(program).arg("-version").output() {
+            let stdout = dbg!(String::from_utf8(output.stdout)
+                .expect("Failed to decode subprocess stdout.")
+                .into_boxed_str());
+
+            bucket.push(stdout);
+        } else {
+            return Err(Box::new(program));
+        }
+    }
+
+    Ok(())
+}
