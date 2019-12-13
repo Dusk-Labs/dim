80e5a599bd06f00feef5fab31cd83bea56c9ef11
Eray Chumak <eray_chumak@protonmail.com>
2019-12-13 02:53:22 +0000

    add del transcode method
    Former-commit-id: 827f4225bd479fb87db2192261c5fd8fcc4efb26

--- web_ui/src/actions/videoPlayerActions.js
+++ web_ui/src/actions/videoPlayerActions.js
@@ -2,16 +2,16 @@ import {
     TRANSCODE_START,
     TRANSCODE_OK,
     TRANSCODE_ERR,
-    FETCH_FILE_START,
-    FETCH_FILE_OK,
-    FETCH_FILE_ERR
+    DEL_TRANSCODE_START,
+    DEL_TRANSCODE_OK,
+    DEL_TRANSCODE_ERR
 } from "./types.js";
 
-export const startTranscode = (id) => async (dispatch) => {
+export const startTranscode = (id, params = "") => async (dispatch) => {
     dispatch({ type: TRANSCODE_START });
 
     try {
-        const res = await fetch(`http://86.21.150.167:8000/api/v1/stream/start/${id}`);
+        const res = await fetch(`http://${window.host}:8000/api/v1/stream/start/${id}${params}`);
 
         if (res.status !== 200) {
             return dispatch({
@@ -34,29 +34,27 @@ export const startTranscode = (id) => async (dispatch) => {
     }
 };
 
-export const fetchFile = (uuid, file) => async (dispatch) => {
-    dispatch({ type: FETCH_FILE_START });
+export const delTranscode = (uuid) => async (dispatch) => {
+    dispatch({ type: DEL_TRANSCODE_START });
+
+    const config = {
+        method: "DELETE"
+    };
 
     try {
-        const res = await fetch(`http://86.21.150.167:8000/api/v1/stream/static/${uuid}/${file}`);
+        const res = await fetch(`http://${window.host}:8000/api/v1/stream/${uuid}`, config);
 
         if (res.status !== 200) {
             return dispatch({
-                type: FETCH_FILE_ERR,
+                type: DEL_TRANSCODE_ERR,
                 payload: res.statusText
             });
         }
 
-        const payload = await res.json();
-
-        dispatch({
-            type: FETCH_FILE_OK,
-            payload
-        });
+        dispatch({ type: DEL_TRANSCODE_OK });
     } catch(err) {
         dispatch({
-            type: FETCH_FILE_ERR,
-            payload: err
+            type: DEL_TRANSCODE_ERR
         });
     }
 };
