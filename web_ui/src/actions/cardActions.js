d898da6423f41dd114c719c6c1a3ce16cb51f8f5
Eray Chumak <eray_chumak@protonmail.com>
2019-12-20 03:27:40 +0000

    add fetch media info, extra info, season and season episodes actions

--- web_ui/src/actions/cardActions.js
+++ web_ui/src/actions/cardActions.js
@@ -1,18 +1,36 @@
 import {
     FETCH_CARDS_START,
     FETCH_CARDS_OK,
-    FETCH_CARDS_ERR
+    FETCH_CARDS_ERR,
+    FETCH_MEDIA_INFO_START,
+    FETCH_MEDIA_INFO_OK,
+    FETCH_MEDIA_INFO_ERR,
+    FETCH_EXTRA_MEDIA_INFO_START,
+    FETCH_EXTRA_MEDIA_INFO_OK,
+    FETCH_EXTRA_MEDIA_INFO_ERR,
+    FETCH_MEDIA_SEASONS_START,
+    FETCH_MEDIA_SEASONS_OK,
+    FETCH_MEDIA_SEASONS_ERR,
+    FETCH_MEDIA_SEASON_EPISODES_START,
+    FETCH_MEDIA_SEASON_EPISODES_OK,
+    FETCH_MEDIA_SEASON_EPISODES_ERR,
 } from "./types.js";
 
 export const fetchCards = (path) => async (dispatch) => {
     dispatch({ type: FETCH_CARDS_START });
 
     try {
-        const res = await fetch(path);
+        const config = {
+            headers: {
+                Authorization: window.token
+            }
+        };
+
+        const res = await fetch(path, config);
 
         if (res.status !== 200) {
             return dispatch({
-                type: FETCH_DIRECTORIES_ERR,
+                type: FETCH_CARDS_ERR,
                 payload: res.statusText
             });
         }
@@ -29,4 +47,147 @@ export const fetchCards = (path) => async (dispatch) => {
             payload: err
         });
     }
-}
+};
+
+export const fetchMediaInfo = (id) => async (dispatch) => {
+    dispatch({ type: FETCH_MEDIA_INFO_START });
+
+    // await new Promise(_ => setTimeout(_, 3000));
+
+    try {
+        const config = {
+            headers: {
+                Authorization: window.token
+            }
+        };
+
+        const res = await fetch(`http://${window.host}:8000/api/v1/media/${id}`, config);
+
+        if (res.status !== 200) {
+            return dispatch({
+                type: FETCH_MEDIA_INFO_ERR,
+                payload: res.statusText
+            });
+        }
+
+        const payload = await res.json();
+
+        // await new Promise(_ => setTimeout(_, 3000));
+
+        dispatch({
+            type: FETCH_MEDIA_INFO_OK,
+            payload
+        });
+    } catch(err) {
+        dispatch({
+            type: FETCH_MEDIA_INFO_ERR,
+            payload: err
+        });
+    }
+};
+
+export const fetchExtraMediaInfo = (id) => async (dispatch) => {
+    dispatch({ type: FETCH_EXTRA_MEDIA_INFO_START });
+
+    try {
+        const config = {
+            headers: {
+                Authorization: window.token
+            }
+        };
+
+        const res = await fetch(`http://${window.host}:8000/api/v1/media/${id}/info`, config);
+
+        if (res.status !== 200) {
+            return dispatch({
+                type: FETCH_EXTRA_MEDIA_INFO_ERR,
+                payload: res.statusText
+            });
+        }
+
+        const payload = await res.json();
+
+        if (payload.error) {
+            return dispatch({
+                type: FETCH_EXTRA_MEDIA_INFO_ERR,
+                payload: payload.error
+            });
+        }
+
+        dispatch({
+            type: FETCH_EXTRA_MEDIA_INFO_OK,
+            payload
+        });
+    } catch(err) {
+        dispatch({
+            type: FETCH_EXTRA_MEDIA_INFO_ERR,
+            payload: err
+        });
+    }
+};
+
+export const fetchMediaSeasons = (id) => async (dispatch) => {
+    dispatch({ type: FETCH_MEDIA_SEASONS_START });
+
+    try {
+        const config = {
+            headers: {
+                Authorization: window.token
+            }
+        };
+
+        const res = await fetch(`http://${window.host}:8000/api/v1/tv/${id}/season`, config);
+
+        if (res.status !== 200) {
+            return dispatch({
+                type: FETCH_MEDIA_SEASONS_ERR,
+                payload: res.statusText
+            });
+        }
+
+        const payload = await res.json();
+
+        dispatch({
+            type: FETCH_MEDIA_SEASONS_OK,
+            payload
+        });
+    } catch(err) {
+        dispatch({
+            type: FETCH_MEDIA_SEASONS_ERR,
+            payload: err
+        });
+    }
+};
+
+export const fetchMediaSeasonEpisodes = (id, season) => async (dispatch) => {
+    dispatch({ type: FETCH_MEDIA_SEASON_EPISODES_START });
+
+    try {
+        const config = {
+            headers: {
+                Authorization: window.token
+            }
+        };
+
+        const res = await fetch(`http://${window.host}:8000/api/v1/tv/${id}/season/${season}/episode`, config);
+
+        if (res.status !== 200) {
+            return dispatch({
+                type: FETCH_MEDIA_SEASON_EPISODES_ERR,
+                payload: res.statusText
+            });
+        }
+
+        const payload = await res.json();
+
+        dispatch({
+            type: FETCH_MEDIA_SEASON_EPISODES_OK,
+            payload
+        });
+    } catch(err) {
+        dispatch({
+            type: FETCH_MEDIA_SEASON_EPISODES_ERR,
+            payload: err
+        });
+    }
+};
--- web_ui/src/layouts/MediaPage.jsx
+++ web_ui/src/layouts/MediaPage.jsx
@@ -1,48 +1,292 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import LazyImage from "../helpers/LazyImage.jsx";
 
+import {
+    fetchMediaInfo,
+    fetchExtraMediaInfo,
+    fetchMediaSeasons,
+    fetchMediaSeasonEpisodes
+} from "../actions/cardActions.js";
+
+import "./MediaPage.scss";
 class MediaPage extends Component {
     constructor(props) {
         super(props);
+
+        this.showSeason = this.showSeason.bind(this);
+
         this.state = {
-            mediaid: this.props.match.params.id,
-            data: {},
+            season: undefined
         }
     }
 
     async componentDidMount() {
-        await this.getData();
+        const { id } = this.props.match.params;
+
+        this.props.fetchMediaInfo(id);
+        this.props.fetchExtraMediaInfo(id);
     }
 
-    async getData() {
-        const { mediaid } = this.state;
-        const extra_info = await (await fetch(`http://${window.host}:8000/api/v1/media/${mediaid}/info`)).json();
-        let info = await (await fetch(`http://${window.host}:8000/api/v1/media/${mediaid}`)).json();
-
-        if(info.media_type === "tv") {
-            const seasons = await (await fetch(`http://${window.host}:8000/api/v1/tv/${mediaid}/season`)).json();
-            const full_data = seasons.map(async x => { return {...x, episodes: (await (await fetch(`http://${window.host}:8000/api/v1/tv/${mediaid}/season/${x.season_number}/episode`)).json())}});
-            info = {...info, seasons: full_data}
+    componentDidUpdate() {
+        // FETCH_MEDIA_INFO_OK
+        if (this.props.fetch_media_info.fetched && !this.props.fetch_media_info.error) {
+            document.title = `Dim - ${this.props.fetch_media_info.info.name}`;
         }
+    }
 
-        await this.setState({data: {...info, extra_info}});
+    showSeason(number) {
+        this.setState({
+            season: number
+        });
     }
 
     render() {
-        const { backdrop_path, description, duration, genres, name, poster_path, rating, year } = this.state.data;
+        let backdrop;
+        let metaContent;
+
+        // FETCH_MEDIA_INFO_START
+        if (this.props.fetch_media_info.fetching) {
+            backdrop = <div className="placeholder"/>;
+
+            metaContent = (
+                <div className="meta-content">
+                    <div className="cover">
+                        <div className="placeholder"/>
+                    </div>
+                    <div className="overview">
+                        <h1><div className="placeholder-text"/></h1>
+                        <div className="genres">
+                            <div className="placeholder-text"/>
+                            <div className="placeholder-text"/>
+                            <div className="placeholder-text"/>
+                            <div className="placeholder-text"/>
+                        </div>
+                        <div className="description">
+                            <div className="placeholder-text"/>
+                        </div>
+                        <div className="meta-info">
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        // FETCH_MEDIA_INFO_ERR
+        if (this.props.fetch_media_info.fetched && this.props.fetch_media_info.error) {
+            console.log("[FETCH MEDIA INFO] ERR", this.props.fetch_media_info);
+
+            backdrop = <div className="placeholder"/>;
+
+            metaContent = (
+                <div className="meta-content">
+                    <div className="cover">
+                        <LazyImage/>
+                    </div>
+                    <div className="overview">
+                        <div className="horizontal-err">
+                            <FontAwesomeIcon icon="times-circle"/>
+                            <p>FAILED TO LOAD</p>
+                        </div>
+                        <div className="genres">
+                            <div className="placeholder-text"/>
+                            <div className="placeholder-text"/>
+                            <div className="placeholder-text"/>
+                            <div className="placeholder-text"/>
+                        </div>
+                        <div className="description">
+                            <div className="placeholder-text"/>
+                        </div>
+                        <div className="meta-info">
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                            <div className="info">
+                                <h4><div className="placeholder-text"/></h4>
+                                <div className="placeholder-text"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        // FETCH_MEDIA_INFO_OK
+        if (this.props.fetch_media_info.fetched && !this.props.fetch_media_info.error) {
+            console.log("[FETCH MEDIA INFO] OK", this.props.fetch_media_info.info);
+
+            const {
+                backdrop_path,
+                poster_path,
+                description,
+                genres,
+                name,
+                duration,
+                rating,
+                year,
+                media_type,
+                id
+            } = this.props.fetch_media_info.info;
+
+            backdrop = <LazyImage src={backdrop_path}/>;
+
+            metaContent = (
+                <div className="meta-content">
+                    <div className="cover">
+                        <LazyImage src={poster_path}/>
+                    </div>
+                    <div className="overview">
+                        <h1>{name}</h1>
+                        <div className="genres">
+                            <Link to={`/search?year=${year}`}>{year}</Link>
+                            <FontAwesomeIcon icon="circle"/>
+                            {genres &&
+                                genres.map((genre, i) => <Link to={`/search/genre=${genre}`} key={i}>{genre}</Link>)
+                            }
+                        </div>
+                        <p className="description">{description}</p>
+                        <div className="meta-info">
+                            <div className="info">
+                                <h4>ID</h4>
+                                <p>{id}</p>
+                            </div>
+                            <div className="info">
+                                <h4>MEDIA TYPE</h4>
+                                <p>{media_type}</p>
+                            </div>
+                            <div className="info">
+                                <h4>DURATION</h4>
+                                <p>{Math.round(duration / 60)} min</p>
+                            </div>
+                            <div className="info">
+                                <h4>RATING</h4>
+                                <p>{rating}/10</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        let mediaSeasons;
+        let mediaEpisodes = {};
+
+        // FETCH_EXTRA_MEDIA_INFO_START
+        if (this.props.fetch_extra_media_info.fetching) {
+            console.log("[FETCH EXTRA MEDIA INFO] FETCHING");
+        }
+
+        // FETCH_EXTRA_MEDIA_INFO_ERR
+        if (this.props.fetch_extra_media_info.fetched && this.props.fetch_extra_media_info.error) {
+            console.table("[FETCH EXTRA MEDIA INFO] ERR", this.props.fetch_extra_media_info);
+        }
+
+        // FETCH_EXTRA_MEDIA_INFO_OK
+        if (this.props.fetch_extra_media_info.fetched && !this.props.fetch_extra_media_info.error) {
+            console.log("[FETCH EXTRA MEDIA INFO] OK", this.props.fetch_extra_media_info);
+
+            const { seasons } = this.props.fetch_extra_media_info.info;
+
+            seasons.sort((a, b) => {
+                return a.season_number - b.season_number;
+            });
+
+            console.log("SEASONS", seasons);
+
+            mediaSeasons = seasons.map((season, si) => {
+                return (
+                    <div className="season" key={si} onClick={() => this.showSeason(season.season_number)}>
+                        <LazyImage src={season.poster}/>
+                        <p>SEASON {season.season_number}</p>
+                    </div>
+                );
+            });
+
+            for (let x = 0; x < seasons.length; x++) {
+                seasons[x].episodes.sort((a, b) => {
+                    return a.episode - b.episode;
+                });
+
+                mediaEpisodes[seasons[x].season_number] = seasons[x].episodes.map((episode, i) => {
+                    return (
+                        <Link to={`/play/${episode.versions[0].id}`} className="episode" key={i}>
+                            <LazyImage src={episode.backdrop}/>
+                            <p>EPISODE {episode.episode}</p>
+                        </Link>
+                    );
+                });
+            }
+        }
+
         return (
-            <div>
-                <LazyImage src={backdrop_path} />
-                <LazyImage src={poster_path} />
-                <div className="name">{name}</div>
-                <div className="description">{description}</div>
-                <div className="duration">{duration}</div>
-                <div className="rating">{rating}</div>
-                <div className="year">{year}</div>
-                {genres !== undefined ? genres.map(x => <div key={x} className="genre">{x}</div>) : <div/>}
+            <div className="media-page">
+                <div className="backdrop">
+                    {backdrop}
+                </div>
+                {metaContent}
+                <div className="content">
+                    <div className="se-ep">
+                        <div className="seasons">
+                            <h2>SEASONS</h2>
+                            <div className="list">
+                                {mediaSeasons}
+                            </div>
+                        </div>
+                        {this.state.season !== undefined &&
+                            <div className="episodes">
+                                <h2>SEASON {this.state.season} EPISODES</h2>
+                                <div className="list">
+                                    {mediaEpisodes[this.state.season]}
+                                </div>
+                            </div>
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default MediaPage;
+const mapStateToProps = (state) => ({
+    fetch_media_info: state.cardReducer.fetch_media_info,
+    fetch_extra_media_info: state.cardReducer.fetch_extra_media_info,
+    fetch_media_seasons_info: state.cardReducer.fetch_media_seasons_info,
+    fetch_media_season_episodes_info: state.cardReducer.fetch_media_season_episodes_info,
+});
+
+const mapActionsToProps = {
+    fetchMediaInfo,
+    fetchExtraMediaInfo,
+    fetchMediaSeasons,
+    fetchMediaSeasonEpisodes
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(MediaPage);
