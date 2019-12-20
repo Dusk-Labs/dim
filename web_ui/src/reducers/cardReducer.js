2d2c40952d7daf9a89a979c5a108932c9b804c98
Eray Chumak <eray_chumak@protonmail.com>
2019-12-20 03:30:40 +0000

    add fech media info, extra info, seasons and season episodes reducer

--- web_ui/src/reducers/cardReducer.js
+++ web_ui/src/reducers/cardReducer.js
@@ -1,38 +1,230 @@
 import {
     FETCH_CARDS_START,
     FETCH_CARDS_OK,
-    FETCH_CARDS_ERR
- } from "../actions/types.js";
+    FETCH_CARDS_ERR,
+    FETCH_MEDIA_INFO_START,
+    FETCH_MEDIA_INFO_OK,
+    FETCH_MEDIA_INFO_ERR,
+    FETCH_EXTRA_MEDIA_INFO_START,
+    FETCH_EXTRA_MEDIA_INFO_OK,
+    FETCH_EXTRA_MEDIA_INFO_ERR,
+    FETCH_MEDIA_SEASONS_START,
+    FETCH_MEDIA_SEASONS_OK,
+    FETCH_MEDIA_SEASONS_ERR,
+    FETCH_MEDIA_SEASON_EPISODES_START,
+    FETCH_MEDIA_SEASON_EPISODES_OK,
+    FETCH_MEDIA_SEASON_EPISODES_ERR,
+} from "../actions/types.js";
 
-const initialState = {
+const fetch_cards = {
     items: [],
     fetching: false,
     fetched: false,
     error: null
 };
 
+const fetch_media_info = {
+    info: {},
+    fetching: false,
+    fetched: false,
+    error: null
+};
+
+const fetch_extra_media_info = {
+    info: {},
+    fetching: false,
+    fetched: false,
+    error: null
+};
+
+const fetch_media_seasons = {
+    items: [],
+    fetching: false,
+    fetched: false,
+    error: null
+};
+
+const fetch_media_season_episodes = {
+    items: [],
+    fetching: false,
+    fetched: false,
+    error: null
+};
+
+const initialState = {
+    fetch_cards,
+    fetch_media_info,
+    fetch_extra_media_info,
+    fetch_media_seasons,
+    fetch_media_season_episodes
+};
+
 export default function(state = initialState, action) {
     switch(action.type) {
         case FETCH_CARDS_START:
             return {
-                items: [],
-                fetching: true,
-                fetched: false,
-                error: null
+                ...state,
+                fetch_cards: {
+                    ...fetch_cards,
+                    items: [],
+                    fetching: true,
+                    fetched: false,
+                    error: null
+                }
             }
         case FETCH_CARDS_OK:
             return {
-                items: action.payload,
-                fetching: false,
-                fetched: true,
-                error: null
+                ...state,
+                fetch_cards: {
+                    ...fetch_cards,
+                    items: action.payload,
+                    fetching: false,
+                    fetched: true,
+                    error: null
+                }
             }
         case FETCH_CARDS_ERR:
             return {
-                items: [],
-                fetching: false,
-                fetched: true,
-                error: action.payload
+                ...state,
+                fetch_cards: {
+                    ...fetch_cards,
+                    items: [],
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
+            }
+        case FETCH_MEDIA_INFO_START:
+            return {
+                ...state,
+                fetch_media_info: {
+                    ...fetch_media_info,
+                    info: {},
+                    fetching: true,
+                    fetched: false,
+                    error: null
+                }
+            }
+        case FETCH_MEDIA_INFO_OK:
+            return {
+                ...state,
+                fetch_media_info: {
+                    ...fetch_media_info,
+                    info: action.payload,
+                    fetching: false,
+                    fetched: true,
+                    error: null
+                }
+            }
+        case FETCH_MEDIA_INFO_ERR:
+            return {
+                ...state,
+                fetch_media_info: {
+                    ...fetch_media_info,
+                    info: {},
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
+            }
+        case FETCH_EXTRA_MEDIA_INFO_START:
+            return {
+                ...state,
+                fetch_extra_media_info: {
+                    ...fetch_extra_media_info,
+                    info: {},
+                    fetching: true,
+                    fetched: false,
+                    error: null
+                }
+            }
+        case FETCH_EXTRA_MEDIA_INFO_OK:
+            return {
+                ...state,
+                fetch_extra_media_info: {
+                    ...fetch_extra_media_info,
+                    info: action.payload,
+                    fetching: false,
+                    fetched: true,
+                    error: null
+                }
+            }
+        case FETCH_EXTRA_MEDIA_INFO_ERR:
+            return {
+                ...state,
+                fetch_extra_media_info: {
+                    ...fetch_extra_media_info,
+                    info: {},
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
+            }
+        case FETCH_MEDIA_SEASONS_START:
+            return {
+                ...state,
+                fetch_media_seasons: {
+                    ...fetch_media_seasons,
+                    items: [],
+                    fetching: true,
+                    fetched: false,
+                    error: null
+                }
+            }
+        case FETCH_MEDIA_SEASONS_OK:
+            return {
+                ...state,
+                fetch_media_seasons: {
+                    ...fetch_media_seasons,
+                    items: action.payload,
+                    fetching: false,
+                    fetched: true,
+                    error: null
+                }
+            }
+        case FETCH_MEDIA_SEASONS_ERR:
+            return {
+                ...state,
+                fetch_media_seasons: {
+                    ...fetch_media_seasons,
+                    items: [],
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
+            }
+        case FETCH_MEDIA_SEASON_EPISODES_START:
+            return {
+                ...state,
+                fetch_media_season_episodes: {
+                    ...fetch_media_season_episodes,
+                    items: [],
+                    fetching: true,
+                    fetched: false,
+                    error: null
+                }
+            }
+        case FETCH_MEDIA_SEASON_EPISODES_OK:
+            return {
+                ...state,
+                fetch_media_season_episodes: {
+                    ...fetch_media_seasons,
+                    items: action.payload,
+                    fetching: false,
+                    fetched: true,
+                    error: null
+                }
+            }
+        case FETCH_MEDIA_SEASON_EPISODES_ERR:
+            return {
+                ...state,
+                fetch_media_season_episodes: {
+                    ...fetch_media_season_episodes,
+                    items: [],
+                    fetching: false,
+                    fetched: true,
+                    error: action.payload
+                }
             }
         default:
             return state;
