c3f590631639d093d569e84c1f15311a4f82b30d
Valerian Garleanu <valerian.garleanu@protonmail.com>
2020-01-21 02:09:46 +0000

    Added register forms, added error checking for invalid tokens, and taken usernames, added routing when not logged in, undefined routes now redirect to /login when not logged in or / when logged in
    Former-commit-id: 4ec6200393420f386ee6ad8d8c1708d095c7a0a2

--- web_ui/src/reducers/authReducer.js
+++ web_ui/src/reducers/authReducer.js
@@ -3,7 +3,10 @@ import {
     AUTH_LOGIN_OK,
     AUTH_LOGIN_ERR,
     AUTH_UPDATE_TOKEN,
-    AUTH_LOGOUT
+    AUTH_LOGOUT,
+    AUTH_REGISTER_ERR,
+    AUTH_REGISTER_OK,
+    AUTH_REGISTER_START
 } from "../actions/types.js";
 
 const initialState = {
@@ -45,6 +48,20 @@ export default function(state = initialState, action) {
             }
         case AUTH_LOGOUT:
             return initialState;
+        case AUTH_REGISTER_OK:
+            return initialState;
+        case AUTH_REGISTER_START:
+            return {
+                ...initialState,
+                logging_in: true,
+            };
+        case AUTH_REGISTER_ERR:
+            return {
+                ...state,
+                logging_in: false,
+                logged_in: false,
+                error: action.payload,
+            };
         default:
             return state;
     }
