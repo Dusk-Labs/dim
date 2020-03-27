ac0747cbaa5b207f3d0726433675586c54af3686
Valerian Garleanu <valerian.garleanu@protonmail.com>
2020-03-27 23:22:06 +0000

    Migrated to a older version of notify

--- src/scanners/parser_daemon.rs
+++ src/scanners/parser_daemon.rs
@@ -1,12 +1,8 @@
 use super::{iterative_parser::IterativeScanner, EventTx};
-use crossbeam_channel::unbounded;
 use database::{get_conn, library::Library};
-use notify::{
-    event::{EventKind::*, ModifyKind::*},
-    RecommendedWatcher, RecursiveMode, Result as nResult, Watcher,
-};
+use notify::{DebouncedEvent::*, RecommendedWatcher, RecursiveMode, Result as nResult, Watcher};
 use slog::{debug, error, Logger};
-use std::{path::PathBuf, time::Duration};
+use std::{path::PathBuf, time::Duration, sync::mpsc};
 
 pub struct ParserDaemon {
     lib: Library,
@@ -26,59 +22,55 @@ impl ParserDaemon {
     }
 
     pub fn start_daemon(&self) -> nResult<()> {
-        let (tx, rx) = unbounded();
-        let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(1))?;
+        let (tx, rx) = mpsc::channel();
+        let mut watcher = <RecommendedWatcher as Watcher>::new(tx, Duration::from_secs(1))?;
 
         watcher.watch(&self.lib.location, RecursiveMode::Recursive)?;
 
         loop {
             match rx.recv() {
-                Ok(event) => self.handle_event(event.unwrap()),
+                Ok(event) => self.handle_event(event),
                 Err(err) => error!(self.log, "Received error: {:?}", err),
             };
         }
     }
 
-    fn handle_event(&self, event: notify::Event) {
+    fn handle_event(&self, event: notify::DebouncedEvent) {
         debug!(self.log, "Handling event: {:?}", event);
-        match event.kind {
-            Create(_) => self.handle_create(event.paths),
-            Modify(kind) => {
-                if let Name(_) = kind {
-                    self.handle_rename(event.paths)
-                }
-            }
-            Remove(_) => self.handle_remove(event.paths),
+        match event {
+            Create(path) => self.handle_create(path),
+            Rename(from, to) => self.handle_rename(from, to),
+            Remove(path) => self.handle_remove(path),
             _ => {}
         }
     }
 
-    fn handle_create(&self, paths: Vec<PathBuf>) {
-        for path in paths {
-            let parser =
-                IterativeScanner::new(self.lib.id, self.log.clone(), self.event_tx.clone())
-                    .unwrap();
+    fn handle_create(&self, path: PathBuf) {
+        let parser =
+            IterativeScanner::new(self.lib.id, self.log.clone(), self.event_tx.clone()).unwrap();
 
-            debug!(self.log, "Received handle_create event type: {:?}", path);
+        debug!(self.log, "Received handle_create event type: {:?}", path);
 
-            if path.is_file()
-                && [".avi", ".mkv", ".mp4"].contains(&path.extension().unwrap().to_str().unwrap())
-            {
-                parser.mount_file(path.to_string_lossy()).unwrap();
-            } else if path.is_dir() {
-                parser.start(path.to_str());
-            }
-
-            parser.fix_orphans();
+        if path.is_file()
+            && [".avi", ".mkv", ".mp4"].contains(&path.extension().unwrap().to_str().unwrap())
+        {
+            parser.mount_file(path.to_string_lossy()).unwrap();
+        } else if path.is_dir() {
+            parser.start(path.to_str());
         }
+
+        parser.fix_orphans();
     }
 
     // Fuck. rename events get first handled as fucking remove events. What the fuck.
-    fn handle_remove(&self, paths: Vec<PathBuf>) {
-        debug!(self.log, "Received handle_remove event type: {:?}", paths);
+    fn handle_remove(&self, path: PathBuf) {
+        debug!(self.log, "Received handle_remove event type: {:?}", path);
     }
 
-    fn handle_rename(&self, paths: Vec<PathBuf>) {
-        debug!(self.log, "Received handle_rename event type: {:?}", paths);
+    fn handle_rename(&self, from: PathBuf, to: PathBuf) {
+        debug!(
+            self.log,
+            "Received handle_rename for file: {:?} to {:?}", from, to
+        );
     }
 }
