671cf13878cd5b21bed62bab47baab736c741e4e
Valerian Garleanu <valerian.garleanu@protonmail.com>
2020-03-29 16:22:45 +0000

    Linted progress.rs
    Former-commit-id: 40b29f790c6e002588c11175dc6382aa8d2dccd8

--- src/database/src/progress.rs
+++ src/database/src/progress.rs
@@ -1,8 +1,8 @@
+use crate::schema::progress;
+use crate::{media::Media, user::User};
 use diesel::prelude::*;
 use diesel::result::Error as DieselError;
-use crate::{media::Media, user::User};
-use serde::{ Serialize};
-use crate::schema::progress;
+use serde::Serialize;
 
 #[derive(Queryable, Debug, Identifiable, Associations, Serialize)]
 #[belongs_to(User, foreign_key = "user_id")]
@@ -16,22 +16,40 @@ pub struct Progress {
 }
 
 impl Progress {
-    pub fn set(conn: &diesel::PgConnection, delta: i32, uid: String, mid: i32) -> Result<usize, DieselError> {
+    pub fn set(
+        conn: &diesel::PgConnection,
+        delta: i32,
+        uid: String,
+        mid: i32,
+    ) -> Result<usize, DieselError> {
         diesel::insert_into(progress::table)
-            .values((progress::delta.eq(delta), progress::media_id.eq(mid), progress::user_id.eq(uid)))
+            .values((
+                progress::delta.eq(delta),
+                progress::media_id.eq(mid),
+                progress::user_id.eq(uid),
+            ))
             .on_conflict((progress::media_id, progress::user_id))
             .do_update()
             .set(progress::delta.eq(delta))
             .execute(conn)
     }
 
-    pub fn get_for_media_user(conn: &diesel::PgConnection, uid: String, mid: i32) -> Result<i32, DieselError> {
+    pub fn get_for_media_user(
+        conn: &diesel::PgConnection,
+        uid: String,
+        mid: i32,
+    ) -> Result<i32, DieselError> {
         use crate::schema::progress::dsl::*;
 
-        match progress.filter(media_id.eq(mid)).filter(user_id.eq(uid)).select(delta).first::<i32>(conn) {
+        match progress
+            .filter(media_id.eq(mid))
+            .filter(user_id.eq(uid))
+            .select(delta)
+            .first::<i32>(conn)
+        {
             Ok(x) => Ok(x),
             Err(DieselError::NotFound) => Ok(0),
-            Err(e) => Err(e)
+            Err(e) => Err(e),
         }
     }
 }
