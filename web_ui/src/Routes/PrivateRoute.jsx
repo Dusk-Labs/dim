e1145d4a3963af3ebe4c973317e6391d6c0a5a58
Eray Chumak <eray_chumak@protonmail.com>
2020-03-29 22:01:26 +0100

    create PrivateRoute component
    Former-commit-id: a93bd37f6e78bca21348aafe4a32e5cd543dca60

---
+++ web_ui/src/Routes/PrivateRoute.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { updateAuthToken } from "../actions/authActions.js";
+
+class PrivateRoute extends Component {
+    componentDidMount() {
+        const token = document.cookie.split("=")[1];
+
+		if (token) {
+			this.props.updateAuthToken(token);
+		}
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.auth.logged_in !== this.props.auth.logged_in) {
+            const token = document.cookie.split("=")[1];
+
+			if (!this.props.auth.error && !token) {
+                const dateExpires = new Date();
+
+                dateExpires.setTime(dateExpires.getTime() + 604800000);
+				document.cookie = `token=${this.props.auth.token};expires=${dateExpires.toGMTString()};`;
+			}
+		}
+	}
+
+    render() {
+        let route;
+
+        // LOGGED IN
+		if (this.props.auth.logged_in && this.props.auth.token && !this.props.auth.error) {
+            const { exact, path, component } = this.props;
+
+            route = <Route exact={exact} path={path} component={component}/>;
+        }
+
+        // NOT LOGGED IN
+        if (!this.props.auth.logged_in || !this.props.auth.token || this.props.auth.error) {
+            route = <Redirect to="/login"/>
+        }
+
+        return route;
+    }
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.authReducer
+});
+
+const mapActionsToProps = ({
+    updateAuthToken
+});
+
+export default connect(mapStateToProps, mapActionsToProps)(PrivateRoute);
