5ba3c57931ae72d24cf8f7419af30037dc7db003
Eray Chumak <eray_chumak@protonmail.com>
2020-04-01 21:18:28 +0100

    add invite list and createNewInvite button
    Former-commit-id: d4d10c24ca270b1e614a164cf7f82b218764bf40

--- web_ui/src/Pages/Preferences.jsx
+++ web_ui/src/Pages/Preferences.jsx
@@ -1,5 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { fetchInvites, createNewInvite } from "../actions/auth.js";
+
+import Profile from "../Components/User/Profile.jsx";
 
 import "./Preferences.scss";
 
@@ -15,7 +20,11 @@ class Preferences extends Component {
     }
 
     componentDidMount() {
-        document.title = `Dim - Preferences`;
+        document.title = "Dim - Preferences";
+
+        if (this.props.user.info.owner) {
+            this.props.fetchInvites(this.props.auth.token);
+        }
     }
 
     componentDidUpdate() {}
@@ -27,11 +36,68 @@ class Preferences extends Component {
     }
 
     // TODO: improve nav switch - just added somethin' super basic to make it work.
+    // TODO: track newly created issues better .
     render() {
-        const fields = [
-            "Account",
-            "Invites"
-        ];
+        let invitesList = <p>You do not have permission to manage invites.</p>;
+
+        if (!this.props.user.owner) {
+            const { fetchInvites, createNewInvite } = this.props.auth;
+
+            const invites = [
+                ...fetchInvites.items,
+                createNewInvite.code
+            ];
+
+            // FETCH_INVITES_START
+            if (fetchInvites.fetching) {
+                invitesList = (
+                    <div className="invitesList">
+                        <div className="item-wrapper">
+                            <div className="placeholder"/>
+                        </div>
+                        <div className="item-wrapper">
+                            <div className="placeholder"/>
+                        </div>
+                        <div className="item-wrapper">
+                            <div className="placeholder"/>
+                        </div>
+                    </div>
+                );
+            }
+
+            // FETCH_INVITES_ERR
+            if (!fetchInvites.fetching && fetchInvites.error) {
+                invitesList = (
+                    <div className="item-wrapper">
+                        <div className="horizontal-err">
+                            <FontAwesomeIcon icon="times-circle"/>
+                            <p>FAILED TO FETCH</p>
+                        </div>
+                    </div>
+                );
+            }
+
+            // FETCH_INVITES_OK
+            if (!fetchInvites.fetching && fetchInvites.fetched && !fetchInvites.error) {
+                const invitesElement = invites.map((invite, i) => <p key={i}>{invite}</p>);
+
+                invitesList = (
+                    invites.length === 0
+                        ? <p>You don't have any invite codes.</p>
+                        : <div className="invitesList">{invitesElement}</div>
+                );
+            }
+
+            // CREATE_NEW_INVITE_ERR
+            if (!createNewInvite.creating && createNewInvite.error) {
+                console.log(createNewInvite.error);
+            }
+
+            // CREATE_NEW_INVITE_OK
+            if (!createNewInvite.creating && createNewInvite.created && !createNewInvite.error) {
+                this.props.fetchInvites(this.props.auth.token);
+            }
+        }
 
         return (
             <div className="preferencesPage">
@@ -40,15 +106,12 @@ class Preferences extends Component {
                         <section>
                             <p>Preferences</p>
                             <div className="fields">
-                                {fields.map((field, i) => {
-                                    const classes = this.state.active === i ? "field active" : "field";
-
-                                    return (
-                                        <div key={i} className={classes} onClick={() => this.switchTo(i)}>
-                                            <p>{field}</p>
-                                        </div>
-                                    );
-                                })}
+                                <div className={this.state.active === 0 ? "field active" : "field"} onClick={() => this.switchTo(0)}>
+                                    <p>Account</p>
+                                </div>
+                                <div className={this.state.active === 1 ? "field active" : "field"} onClick={() => this.switchTo(1)}>
+                                    <p>Invites</p>
+                                </div>
                             </div>
                         </section>
                     </nav>
@@ -56,11 +119,19 @@ class Preferences extends Component {
                         {this.state.active === 0 &&
                             <section>
                                 <p>My account</p>
+                                <div className="account">
+                                    <Profile name={true}/>
+                                    <FontAwesomeIcon className="edit" icon="edit"/>
+                                </div>
                             </section>
                         }
                         {this.state.active === 1 &&
-                            <section>
-                                <p>Invites</p>
+                            <section className="invites">
+                                <p>Invite Codes</p>
+                                {this.props.user.info.owner &&
+                                    <button onClick={() => this.props.createNewInvite(this.props.auth.token)}>Create new invite code</button>
+                                }
+                                {invitesList}
                             </section>
                         }
                     </div>
@@ -71,9 +142,13 @@ class Preferences extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    auth: state.auth
+    auth: state.auth,
+    user: state.user
 });
 
-const mapActionsToProps = {};
+const mapActionsToProps = {
+    fetchInvites,
+    createNewInvite
+};
 
 export default connect(mapStateToProps, mapActionsToProps)(Preferences);
