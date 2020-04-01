af6a6d23b87b0b9cb16842c6d169cc745fea429f
Eray Chumak <eray_chumak@protonmail.com>
2020-04-01 21:16:33 +0100

    create Profile component

--- web_ui/src/components/Sidebar.jsx
+++ web_ui/src/components/Sidebar.jsx
@@ -6,12 +6,11 @@ import { Scrollbar } from "react-scrollbars-custom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { fetchLibraries, delLibrary, handleWsNewLibrary, handleWsDelLibrary } from "../actions/library.js";
-import { fetchUser } from "../actions/user.js";
 import { logout } from "../actions/auth.js";
 
+import Profile from "./User/Profile.jsx";
 import SidebarSearch from "../Helpers/SidebarSearch.jsx";
 import SidebarIcon from "../Helpers/SidebarIcon.jsx";
-import LazyImage from "../Helpers/LazyImage.jsx";
 import NewLibraryModal from "../Helpers/NewLibraryModal.jsx";
 import ConfirmationBox from "../Helpers/ConfirmationBox.jsx";
 
@@ -51,7 +50,6 @@ class Sidebar extends Component {
     };
 
     componentDidMount() {
-        this.props.fetchUser(this.props.auth.token);
         this.props.fetchLibraries(this.props.auth.token);
     }
 
@@ -75,61 +73,7 @@ class Sidebar extends Component {
     }
 
     render() {
-        let user;
         let libraries;
-
-        /*
-            * == USER ==
-        */
-
-        // FETCH_USER_START
-        if (this.props.user.fetching) {
-            user = (
-                <div className="profile">
-                    <div className="profile-icon">
-                        <div className="default-icon"></div>
-                    </div>
-                    <div className="placeholder-text"/>
-                </div>
-            );
-        }
-
-        // FETCH_USER_ERR
-        if (this.props.user.fetched && this.props.user.error) {
-            user = (
-                <div className="profile">
-                    <div className="profile-icon">
-                        <div className="default-icon"></div>
-                    </div>
-                    <div className="horizontal-err">
-                        <FontAwesomeIcon icon="times-circle"/>
-                        <p>FAILED TO FETCH</p>
-                    </div>
-                </div>
-            );
-        }
-
-        // FETCH_USER_OK
-        if (this.props.user.fetched && !this.props.user.error) {
-            const loading = <div className="default-icon"/>;
-            const { username, picture, spentWatching } = this.props.user.info;
-
-            user = (
-                <div className="profile">
-                    <div className="profile-icon">
-                        <LazyImage
-                            alt=""
-                            src={picture}
-                            loading={loading}
-                        />
-                    </div>
-                    <div className="info">
-                        <h4>{username}</h4>
-                        <h6>{spentWatching}h spent watching</h6>
-                    </div>
-                </div>
-            );
-        }
 
         /*
             * == LIBRARIES ==
@@ -204,7 +148,7 @@ class Sidebar extends Component {
                     <FontAwesomeIcon icon="angle-left"/>
                 </div>
                 <section className="main-part">
-                    {user}
+                    <Profile name={true} hoursSpentWatching={true}/>
                     <div className="separator"/>
                     <SidebarSearch/>
                 </section>
@@ -251,14 +195,12 @@ class Sidebar extends Component {
 
 const mapStateToProps = (state) => ({
     auth: state.auth,
-    user: state.user,
     libraries: state.library.fetch_libraries
 });
 
 const mapActionsToProps = {
     logout,
     fetchLibraries,
-    fetchUser,
     delLibrary,
     handleWsDelLibrary,
     handleWsNewLibrary,
