29a00857b432a576d001da7de880bc416a563f24
Eray Chumak <eray_chumak@protonmail.com>
2020-11-01 23:39:15 +0000

    Refactor Register page component
    Former-commit-id: c9c6dbc4af0a56ad670b55a613a8e55521ffcea9

--- web_ui/src/Pages/Register.jsx
+++ web_ui/src/Pages/Register.jsx
@@ -1,204 +1,158 @@
-import React, { Component } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { authenticate, register, checkAdminExists } from "../actions/auth.js";
 
 import "./AuthForm.scss";
 
-class Register extends Component {
-    constructor(props) {
-        super(props);
+function Register(props) {
+  const history = useHistory();
 
-        this.updateField = this.updateField.bind(this);
-        this.authorize = this.authorize.bind(this);
-        this.onKeyDown = this.onKeyDown.bind(this);
+  const [username, setUsername] = useState("");
+  const [usernameErr, setUsernameErr] = useState("");
 
-        this.state = {
-            username: {
-                value: "",
-                err: ""
-            },
-            password: {
-                value: "",
-                err: ""
-            },
-            confirm_password: {
-                value: "",
-                err: ""
-            },
-            invite: {
-                value: "",
-                err: ""
-            },
-        };
-    }
+  const [password1, setPassword1] = useState("");
+  const [passwordErr1, setPassword1Err] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [passwordErr2, setPassword2Err] = useState("");
 
-    componentDidMount() {
-		document.title = "Dim - Register";
-        this.props.checkAdminExists();
-        document.addEventListener("keydown", this.onKeyDown);
-    }
+  const [invite, setInvite] = useState("");
 
-    componentWillUnmount() {
-        document.removeEventListener("keydown", this.onKeyDown);
-    }
+  // CHECK AUTH STATE
+  useEffect(() => {
+    const token = document.cookie.split("=")[1];
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.auth.error !== this.props.auth.register.error) {
-            if (this.props.auth.register.error === "NoTokenError") {
-                this.warn("invite", "Wrong invite token");
-            }
-            if (this.props.auth.register.error === "UsernameTaken") {
-                this.warn("username", "Username is already taken");
-            }
-        }
-    }
+		if (props.auth.login.logged_in && props.auth.token && !props.auth.login.error || token) {
+      props.fetchUser(props.auth.token);
 
-    onKeyDown(e) {
-        if (e.keyCode !== 13) return;
-        this.authorize();
-    }
+      if (!token) {
+        const dateExpires = new Date();
 
-    updateField(e) {
-        const { name, value } = e.target;
+        dateExpires.setTime(dateExpires.getTime() + 604800000);
 
-        this.setState({
-            [name]: {
-                value,
-                err: ""
-            }
-        });
-    }
-
-    warn(field, err) {
-        this.setState({
-            [field]: {
-                ...this.state[field],
-                err
-            }
-        });
-    }
-
-    async authorize() {
-        const { username, password, confirm_password, invite } = this.state;
-
-        if (password.value !== confirm_password.value )
-            this.warn("confirm_password", "Passwords must match");
-
-        if (username.value.length <= 3 || password.value.length <= 3 || (this.props.auth.admin_exists && invite.value.length !== 36)) {
-            if (username.value.length <= 3) {
-                this.warn("username", "Too short, min. 4 chars.");
-            }
-
-            if (password.value.length <= 3) {
-                this.warn("password", "Too short, min. 4 chars.");
-            }
-
-            if (this.props.auth.admin_exists && invite.value.length !== 36) {
-                this.warn("invite", "Should be 36 chars.");
-            }
-        } else {
-            await this.props.register(username.value, password.value, invite.value);
-            await this.props.authenticate(username.value, password.value);
-        }
-    }
-
-    render() {
-        const token = document.cookie.split("=")[1];
-
-        // LOGGED IN
-		if (this.props.auth.login.logged_in && this.props.auth.token && !this.props.auth.login.error || token) {
-            if (!token) {
-                const dateExpires = new Date();
-
-                dateExpires.setTime(dateExpires.getTime() + 604800000);
-                document.cookie = `token=${this.props.auth.token};expires=${dateExpires.toGMTString()};`;
-            }
-
-            return <Redirect to="/"/>;
-        }
-
-        const { admin_exists } = this.props.auth;
-
-        // AUTH_LOGIN_ERR
-        if (this.props.auth.register.error) {
-            console.log("[AUTH] REGISTER ERROR", this.props.auth);
-        }
-
-        return (
-            <div className="auth-form">
-                <header>
-                    <h1>Welcome to Dim</h1>
-                    {admin_exists
-                        ? <h3>A media manager fueled by dark forces</h3>
-                        : <h3>Warning: You are making a admin account! </h3>
-                    }
-                </header>
-                <div className="fields">
-                    <div className="field">
-                        <label>
-                            <FontAwesomeIcon icon="user"/>
-                            <p>USERNAME</p>
-                            {this.state.username.err.length > 0 &&
-                                <div className="horizontal-err">
-                                    <FontAwesomeIcon icon="times-circle"/>
-                                    <p>{this.state.username.err}</p>
-                                </div>
-                            }
-                        </label>
-                        <input type="text" name="username" onChange={this.updateField} spellCheck="false"/>
-                    </div>
-                    <div className="field">
-                        <label>
-                            <FontAwesomeIcon icon="key"/>
-                            <p>PASSWORD</p>
-                            {this.state.password.err.length > 0 &&
-                                <div className="horizontal-err">
-                                    <FontAwesomeIcon icon="times-circle"/>
-                                    <p>{this.state.password.err}</p>
-                                </div>
-                            }
-                        </label>
-                        <input type="password" name="password" onChange={this.updateField}/>
-                    </div>
-                    <div className="field">
-                        <label>
-                            <FontAwesomeIcon icon="key"/>
-                            <p>CONFIRM YOUR PASSWORD</p>
-                            {this.state.confirm_password.err.length > 0 &&
-                                <div className="horizontal-err">
-                                    <FontAwesomeIcon icon="times-circle"/>
-                                    <p>{this.state.confirm_password.err}</p>
-                                </div>
-                            }
-                        </label>
-                        <input type="password" name="confirm_password" onChange={this.updateField}/>
-                    </div>
-                    {admin_exists ?
-                    <div className="field">
-                        <label>
-                            <FontAwesomeIcon icon="tag"/>
-                            <p>INVITE TOKEN</p>
-                            {this.state.invite.err.length > 0 &&
-                                <div className="horizontal-err">
-                                    <FontAwesomeIcon icon="times-circle"/>
-                                    <p>{this.state.invite.err}</p>
-                                </div>
-                            }
-                        </label>
-                        <input type="invite" name="invite" onChange={this.updateField}/>
-                    </div> : <div/>
-                    }
-                </div>
-                <footer>
-                    <button onClick={this.authorize}>Register</button>
-                    <Link to="/login">Already have an account?</Link>
-                </footer>
-            </div>
+        document.cookie = (
+          `token=${props.auth.token};expires=${dateExpires.toGMTString()};`
         );
+      }
+
+      return history.push("/");
     }
+
+    // AUTH_LOGIN_ERR
+    if (props.auth.login.error) {
+      console.log("[AUTH] ERROR", props.auth.login);
+    }
+  }, []);
+
+  useEffect(() => { props.checkAdminExists() }, []);
+  useEffect(() => setUsernameErr(""), [username])
+  useEffect(() => setPassword1Err(""), [password1])
+  useEffect(() => setPassword2Err(""), [password2])
+
+  const authorize = useCallback(async () => {
+    if (password1 !== password2) {
+      setPassword2Err("Passwords must match")
+    }
+
+    if (username.length <= 3 || password1.length <= 3 || (props.auth.admin_exists && invite.length !== 36)) {
+        if (username.length <= 3) {
+          setUsernameErr("Too short, min. 4 chars.")
+        }
+
+        if (password1.length <= 3) {
+          setPassword1Err("Too short, min. 4 chars.")
+        }
+
+        if (props.auth.admin_exists && invite.value.length !== 36) {
+          setPassword2Err("Should be 36 chars.")
+        }
+    } else {
+        await props.register(username.value, password.value, invite.value);
+        await props.authenticate(username.value, password.value);
+    }
+  }, [username, password1, password2, invite])
+
+  return (
+    <div className="authForm">
+      <header>
+        <h1>Welcome to Dim</h1>
+        {props.auth.admin_exists
+          ? <h3>A media manager fueled by dark forces</h3>
+          : <h3>You are making an admin account</h3>
+        }
+      </header>
+      <div className="fields">
+        <label>
+          <div className="name">
+            <FontAwesomeIcon icon="user"/>
+            <p>USERNAME</p>
+            {usernameErr.length > 0 && (
+              <div className="horizontal-err">
+                <FontAwesomeIcon icon="times-circle"/>
+                <p>{usernameErr}</p>
+              </div>
+            )}
+          </div>
+          <input type="text" name="username" onChange={e => setUsername(e.target.value)} spellCheck="false"/>
+        </label>
+        <label>
+          <div className="name">
+            <FontAwesomeIcon icon="key"/>
+            <p>PASSWORD</p>
+            {passwordErr1.length > 0 && (
+              <div className="horizontal-err">
+                <FontAwesomeIcon icon="times-circle"/>
+                <p>{passwordErr1}</p>
+              </div>
+            )}
+          </div>
+          <input type="password" name="password" onChange={e => setPassword1(e.target.value)}/>
+        </label>
+        <label>
+          <div className="name">
+            <FontAwesomeIcon icon="key"/>
+            <p>CONFIRM YOUR PASSWORD</p>
+            {passwordErr2.length > 0 && (
+              <div className="horizontal-err">
+                <FontAwesomeIcon icon="times-circle"/>
+                <p>{passwordErr2}</p>
+              </div>
+            )}
+          </div>
+          <input type="password" name="confirm_password" onChange={e => setPassword2(e.target.value)}/>
+        </label>
+        {props.auth.admin_exists && (
+          <label>
+            <div className="name">
+              <FontAwesomeIcon icon="tag"/>
+              <p>INVITE TOKEN</p>
+              {inviteErr.length > 0 && (
+                <div className="horizontal-err">
+                  <FontAwesomeIcon icon="times-circle"/>
+                  <p>{inviteErr}</p>
+                </div>
+              )}
+            </div>
+            <input type="invite" name="invite" onChange={e => setInvite(e.target.value)}/>
+          </label>
+        )}
+      </div>
+      <footer>
+        <button onClick={authorize}>Register</button>
+        <Link to="/login">I have an account</Link>
+      </footer>
+      <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 202.832 143.542">
+        <g transform="translate(-397.21 -251.229)">
+          <path d="M6712.87,5823.906l34.876,34.876a9.96,9.96,0,0,1,0,14.063l-34.876,34.9a9.989,9.989,0,0,1-14.088,0l-34.876-34.9a9.96,9.96,0,0,1,0-14.063l34.876-34.876a9.989,9.989,0,0,1,14.088,0Zm33.989,35.763-34.876-34.876a8.741,8.741,0,0,0-12.314,0l-34.876,34.876a8.741,8.741,0,0,0,0,12.314l34.876,34.876a8.741,8.741,0,0,0,12.314,0l34.876-34.876a8.741,8.741,0,0,0,0-12.314Z" transform="translate(-6205.073 -5569.771)" fill="#fff"/>
+          <path d="M8974.543,8036.733l34.875,34.876a9.35,9.35,0,0,1,0,13.177l-13.743,13.768V8082.89l-3.694-5.616v24.975l-17.437,17.438a9.381,9.381,0,0,1-13.2,0l-13.743-13.768v-28.645l-3.694,5.616v19.359l-17.438-17.462a9.352,9.352,0,0,1,0-13.177l34.876-34.876A9.337,9.337,0,0,1,8974.543,8036.733Z" transform="translate(-8412.092 -7728.265)" fill="#fff" fill-rule="evenodd"/>
+          <path d="M4328.87,8011.906l34.875,34.876a9.959,9.959,0,0,1,0,14.063l-34.875,34.9a9.989,9.989,0,0,1-14.088,0l-34.876-34.9a9.96,9.96,0,0,1,0-14.063l34.876-34.876a9.989,9.989,0,0,1,14.088,0Zm-9.95,85v-86.154a8.858,8.858,0,0,0-3.251,2.02l-34.875,34.9a8.71,8.71,0,0,0,0,12.29l34.875,34.9A8.946,8.946,0,0,0,4318.92,8096.9Z" transform="translate(-3879.79 -7703.881)" fill="#fff"/>
+          <rect width="14" height="88" rx="7" transform="translate(494 252)" fill="#fff"/>
+        </g>
+      </svg>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({
