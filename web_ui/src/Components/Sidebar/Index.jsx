6445d3ac9cde2bbb9cc4ebc8bd3456ee38491354
Eray Chumak <eray_chumak@protonmail.com>
2020-11-11 17:39:32 +0000

    Remove toggle logic+styling (moved into own component)
    Former-commit-id: d14cddb962e8d2e8d91c956dda62bc6d08657b5e

--- web_ui/src/Components/Sidebar/Index.jsx
+++ web_ui/src/Components/Sidebar/Index.jsx
@@ -1,7 +1,6 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { logout } from "../../actions/auth.js";
 import { fetchUser } from "../../actions/user.js";
@@ -10,63 +9,47 @@ import Profile from "./Profile.jsx";
 import Search from "./Search.jsx";
 import Icon from "./Icon.jsx";
 import Libraries from "./Libraries";
+import Toggle from "./Toggle";
 
 import "./Index.scss";
 
 function Sidebar(props) {
   const divContainer = useRef(null);
-  const [visible, setVisible] = useState(true);
-
-  const toggleSidebar = useCallback(() => {
-    setVisible(state => !state);
-
-    const main = document.querySelectorAll("main")[0];
-
-    divContainer.current.classList.toggle("hide", visible);
-    divContainer.current.classList.toggle("show", !visible);
-
-    main.classList.toggle("full", visible);
-    main.classList.toggle("shrunk", !visible);
-  }, [visible]);
 
   useEffect(() => {
-    if (window.innerWidth < 800) {
-      toggleSidebar();
-    }
-
     props.fetchUser(props.auth.token);
   }, []);
 
   return (
     <nav className="sidebar" ref={divContainer}>
-      <div className="toggle" onClick={toggleSidebar}>
-        <FontAwesomeIcon icon="angle-left"/>
+      <div className="sectionsWrapper">
+        <section className="main-part">
+          <Profile name={true} hoursSpentWatching={true}/>
+          <div className="separator"/>
+          <Search/>
+        </section>
+        <Libraries/>
+        <section className="your-account">
+          <header>
+            <h4>Account</h4>
+          </header>
+          <div className="list">
+            <div className="item-wrapper">
+              <NavLink to="/preferences">
+                <Icon icon="preferences"/>
+                <p className="item-wrapper-name">Preferences</p>
+              </NavLink>
+            </div>
+            <div className="item-wrapper">
+              <NavLink to="/login" onClick={props.logout}>
+                <Icon icon="logout"/>
+                <p className="item-wrapper-name">Logout</p>
+              </NavLink>
+            </div>
+          </div>
+        </section>
       </div>
-      <section className="main-part">
-        <Profile name={true} hoursSpentWatching={true}/>
-        <div className="separator"/>
-        <Search/>
-      </section>
-      <Libraries/>
-      <section className="your-account">
-        <header>
-          <h4>Account</h4>
-        </header>
-        <div className="list">
-          <div className="item-wrapper">
-            <NavLink to="/preferences">
-              <Icon icon="preferences"/>
-              <p className="item-wrapper-name">Preferences</p>
-            </NavLink>
-          </div>
-          <div className="item-wrapper">
-            <NavLink to="/login" onClick={props.logout}>
-              <Icon icon="logout"/>
-              <p className="item-wrapper-name">Logout</p>
-            </NavLink>
-          </div>
-        </div>
-      </section>
+      <Toggle sidebar={divContainer}/>
     </nav>
   );
 }
