83675dde2102c7d23f9d8e4cd20b40acb1640d4c
Eray Chumak <eray_chumak@protonmail.com>
2020-11-20 02:39:22 +0000

    Refactor to use react hooks (to be deleted later)

--- web_ui/src/Helpers/LazyImage.jsx
+++ web_ui/src/Helpers/LazyImage.jsx
@@ -1,116 +1,70 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback } from "react";
 
-class LazyImage extends Component {
-    constructor(props) {
-        super(props);
+function LazyImage(props) {
+  const [currentSrc, setCurrentSrc] = useState();
+  const [show, setShow] = useState();
 
-        this._isMounted = false;
+  const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setErr] = useState(false);
 
-        this.state = {
-            fetching: false,
-            fetched: false,
-            error: false
-        };
+  useEffect(() => {
+    if (!currentSrc) {
+      setCurrentSrc(props.src);
     }
 
-    componentDidMount() {
-        this._isMounted = true;
-        this.renderBlob();
+    if ((props.src !== currentSrc) && currentSrc) {
+      setLoading(true);
+      setLoaded(false);
+      setErr(false);
+      setShow(false);
     }
+  }, [props.src]);
 
-    componentWillUnmount() {
-        this._isMounted = false;
-        if (this.state.objectUrl) {
-            URL.revokeObjectURL(this.state.objectUrl);
-        }
+  const swapSrc = useCallback((e) => {
+    if ((props.src !== currentSrc) && (e.animationName === props.hideAnimationName)) {
+      setCurrentSrc(props.src);
+      setShow(true);
     }
+  }, [props.src]);
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.src !== this.props.src) {
-            this.renderBlob();
-        }
-    }
-
-    async renderBlob() {
-        this.setState({
-            fetching: true,
-            fetched: false,
-            error: false
-        });
-
-        const res = await fetch(this.props.src);
-
-        if ((!res.headers.get("content-type") === "image/jpeg" || !this.props.src) && this._isMounted) {
-            return this.setState({
-                fetching: false,
-                fetched: true,
-                error: true
-            });
-        }
-
-        const blob = await res.blob();
-        const objectUrl = URL.createObjectURL(blob);
-
-        if (this._isMounted) {
-            this.setState({
-                fetching: false,
-                fetched: true,
-                blob,
-                objectUrl
-            });
-
-            if (this.props.onLoad) {
-                await this.props.onLoad(blob);
-            }
-        }
-    }
-
-    render() {
-        // FETCHING
-        if (this.state.fetching) {
-            if (!this.props.loading) {
-                return (
-                    <div className="placeholder">
-                        <div className="spinner"/>
-                    </div>
-                );
-            } else return this.props.loading;
-        }
-
-        // ERR
-        if (this.state.fetched && this.state.error) {
-            if (!this.props.onFail) {
-                if (this.props.type === "SMALL") {
-                    return (
-                        <div className="error-icon">
-                            <FontAwesomeIcon icon="times-circle"/>
-                        </div>
-                    );
-                }
-
-                return (
-                    <div className="placeholder">
-                        <div className="vertical-err">
-                            <FontAwesomeIcon icon="question-circle"/>
-                            <p>FAILED TO LOAD</p>
-                        </div>
-                    </div>
-                );
-            } else return this.props.onFail();
-        }
-
-        // OK
-        if (this.state.fetched && !this.state.error) {
-            return (
-                <div className="image-wrapper" ref={this.props.imageWrapperRef}>
-                    <img src={this.state.objectUrl} alt={this.props.alt}/>
-                </div>
-            );
-        }
-
-        return <div className="placeholder"/>;
-    }
+  // OK
+  return (
+    <div
+      className={`image-wrapper show-${show && loaded}`}
+      ref={props.imageWrapperRef}
+      onAnimationEnd={swapSrc}
+    >
+      {loading && (
+        <div className="placeholder">
+          <div className="spinner"/>
+        </div>
+      )}
+      {(loaded && error && !props.onFail) && (
+        <div className="placeholder"/>
+      )}
+      {(loaded && error && !props.onFail && props.type === "SMALL") && (
+        <div className="error-icon">
+          <FontAwesomeIcon icon="times-circle"/>
+        </div>
+      )}
+      <img
+        key={props.src}
+        src={currentSrc}
+        alt={props.alt}
+        onLoad={() => {
+          setLoaded(true)
+          setLoading(false)
+        }}
+        onError={() => {
+          setLoaded(true);
+          setLoading(false)
+          setErr(true);
+        }}/>
+    </div>
+  )
 }
 
 export default LazyImage;
