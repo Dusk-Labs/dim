b45ba6553fbc59dbfe0a109f62089b2ab8516c86
Eray Chumak <eray_chumak@protonmail.com>
2020-11-29 21:18:59 +0000

    Refactor ProgressBar component to use react hooks
    Former-commit-id: a1a7901c509c37e164f3bfde843efaf19a601fac

--- web_ui/src/Components/Banners/ProgressBar.jsx
+++ web_ui/src/Components/Banners/ProgressBar.jsx
@@ -1,49 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./ProgressBar.scss";
 
-class ProgressBar extends Component {
-    render() {
-        const {
-            backgroundColor,
-            season,
-            episode
-        } = this.props.data;
+function ProgressBar(props) {
+  const {
+    backgroundColor,
+    season,
+    episode
+  } = props.data;
 
-        let { delta, duration } = this.props.data;
+  let { delta, duration } = props.data;
 
-        delta = !delta ? 0 : delta;
-        duration = Math.round(duration / 60);
+  delta = !delta ? 0 : delta;
+  duration = Math.round(duration / 60);
 
-        const current = Math.round(delta / 60);
-        const width = current / duration * 100 + "%";
+  const current = Math.round(delta / 60);
+  const width = current / duration * 100 + "%";
 
-        return (
-            <div className="banner-progress-bar">
-                {season && episode &&
-                    <div className="s-e">
-                        <p>S{season}</p>
-                        <FontAwesomeIcon icon="circle" style={{ color: backgroundColor }}/>
-                        <p>E{episode}</p>
-                    </div>
-                }
-                <div className="progress">
-                    <div className="current">
-                        <p>{current}</p>
-                        <p>min</p>
-                    </div>
-                    <div className="bar">
-                        <span className="progress-fill" style={{ width: width, background: backgroundColor }}/>
-                    </div>
-                    <div className="duration">
-                        <p>{duration}</p>
-                        <p>min</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  return (
+    <div className="banner-progress-bar">
+      {(season && episode) && (
+        <div className="s-e">
+          <p>S{season}</p>
+          <FontAwesomeIcon icon="circle" style={{ color: backgroundColor }}/>
+          <p>E{episode}</p>
+        </div>
+      )}
+      <div className="progress">
+        <div className="current">
+          <p>{current}</p>
+          <p>min</p>
+        </div>
+        <div className="bar">
+          <span className="progress-fill" style={{ width: width, background: backgroundColor }}/>
+        </div>
+        <div className="duration">
+          <p>{duration}</p>
+          <p>min</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProgressBar;
