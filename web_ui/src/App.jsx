6580175ad1d95c5e86826d06a50111ba83b40e1c
Eray Chumak <eray_chumak@protonmail.com>
2020-11-30 12:10:06 +0000

    Remove WithoutSidebarLayout component
    Former-commit-id: d06ceba46a22bd4582e8a11fbe1e52487c94cc9c

--- web_ui/src/App.jsx
+++ web_ui/src/App.jsx
@@ -63,9 +63,7 @@ const routes = (
       </MainLayout>
     )}/>
     {/* <PrivateRoute path="/play/:id" render={(props) => (
-      <WithOutSidebarLayout>
-        <VideoPlayer {...props}/>
-      </WithOutSidebarLayout>
+      <VideoPlayer {...props}/>
     )}/> */}
   </Switch>
 );
