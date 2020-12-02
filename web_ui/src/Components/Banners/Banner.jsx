9812d4d5146b13572d1f1a4249cabaf74aa57f4f
Eray Chumak <eray_chumak@protonmail.com>
2020-12-02 02:24:36 +0000

    Don't show 'Empty' when error

--- web_ui/src/Components/Banners/Banner.jsx
+++ web_ui/src/Components/Banners/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { connect } from "react-redux";
 
 import ProgressBar from "./ProgressBar.jsx";
 import Image from "./Image.jsx";
@@ -13,75 +14,92 @@ function Banner(props) {
   const [backgroundColor, setBackgroundColor] = useState("#f7931e");
   const [textColor, setTextColor] = useState("#fff");
 
-  if (!props.data) {
+  // FETCH_BANNERS_FETCHING or FETCH_BANNERS_ERROR
+  if (props.banners.fetching || (props.banners.fetched && props.banners.error)) {
     return (
       <div className="banner">
-        <div className="placeholder">
-          <p>Empty</p>
-        </div>
+        <div className="placeholder"/>
       </div>
     );
   }
 
-  const {
-    id,
-    title,
-    year,
-    synopsis,
-    backdrop,
-    delta,
-    duration,
-    genres,
-    season,
-    episode
-  } = props.data;
+  // FETCH_BANNERS_FETCHED
+  if (props.banners.fetched && !props.banners.error) {
+    if (!props.data) {
+      return (
+        <div className="banner">
+          <div className="placeholder">
+            <p>Empty</p>
+          </div>
+        </div>
+      );
+    }
 
-  if (genres.length > 3) {
-    genres.length = 3;
+    const {
+      id,
+      title,
+      year,
+      synopsis,
+      backdrop,
+      delta,
+      duration,
+      genres,
+      season,
+      episode
+    } = props.data;
+
+    const progressBarData = {
+      backgroundColor,
+      season,
+      episode,
+      duration,
+      delta
+    };
+
+    return (
+      <div className="banner">
+        <Image
+          setText={setTextColor}
+          setBG={setBackgroundColor}
+          src={backdrop}
+          hideAnimationName="onHideBannerImage"
+        />
+        <div className="extras">
+          <Link to={`/search?year=${year}`}>{year}</Link>
+          <FontAwesomeIcon icon="circle" style={{ color: backgroundColor }}/>
+          {genres.map((genre, i) => (
+            <Link
+              to={`/search?genre=${genre}`}
+              key={i}
+            >
+              {genre}
+            </Link>
+          ))}
+        </div>
+        <div className="info">
+          <h1>{title}</h1>
+          <p className="description">
+            <TruncText content={synopsis} max={35}/>
+          </p>
+          <PlayButton
+            id={id}
+            bgColor={backgroundColor}
+            textColor={textColor}
+          />
+        </div>
+        <ProgressBar data={progressBarData}/>
+      </div>
+    );
   }
 
-  const progressBarData = {
-    backgroundColor,
-    season,
-    episode,
-    duration,
-    delta
-  };
-
-  return (
-    <div className="banner">
-      <Image
-        setText={setTextColor}
-        setBG={setBackgroundColor}
-        src={backdrop}
-        hideAnimationName="onHideBannerImage"
-      />
-      <div className="extras">
-        <Link to={`/search?year=${year}`}>{year}</Link>
-        <FontAwesomeIcon icon="circle" style={{ color: backgroundColor }}/>
-        {genres.map((genre, i) => (
-          <Link
-            to={`/search?genre=${genre}`}
-            key={i}
-          >
-            {genre}
-          </Link>
-        ))}
-      </div>
-      <div className="info">
-        <h1>{title}</h1>
-        <p className="description">
-          <TruncText content={synopsis} max={35}/>
-        </p>
-        <PlayButton
-          id={id}
-          bgColor={backgroundColor}
-          textColor={textColor}
-        />
-      </div>
-      <ProgressBar data={progressBarData}/>
-    </div>
-  );
+  return <div className="banner"/>
 }
 
-export default Banner;
+const mapStateToProps = (state) => ({
+  banners: state.banner
+});
+
+const mapActionstoProps = {};
+
+export default connect(mapStateToProps, mapActionstoProps)(Banner);
+
