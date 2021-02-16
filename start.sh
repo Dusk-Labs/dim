f091fa8a2c6140fdba2eb5276f1fd8b0c9f169de
Valerian G <valerian.garleanu@pm.me>
2021-02-16 06:28:19 +0000

    change DIM_ENABLE_SSL flag from "enable" to 1

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo "host all  all    127.0.0.1/0  md5" > /var/lib/postgresql/10/main/pg_hba.co
 echo "listen_addresses='*'" > /var/lib/postgresql/10/main/postgresql.conf
 sudo -u postgres /usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main start 
 
-if [[ $DIM_ENABLE_SSL == "enable" ]];
+if [[ $DIM_ENABLE_SSL == "1" ]];
 then
     cd /opt/dim && ./dim --priv-key /opt/dim/config/priv_key.pem --ssl-cert /opt/dim/config/cert.pem;
 else
