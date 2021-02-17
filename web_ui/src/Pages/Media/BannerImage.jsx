be01a5129e0c8526670e8d503bf309c82d9e4d17
Valerian G <valerian.garleanu@pm.me>
2021-02-17 04:16:11 +0000

    All Image components now check whether the img src url supplied is absolute or not, if its relative then it appends the url to window.host

--- web_ui/src/Pages/Media/BannerImage.jsx
+++ web_ui/src/Pages/Media/BannerImage.jsx
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from "react";
 function BannerImage(props) {
   const [loaded, setLoaded] = useState(false);
   const [error, setErr] = useState(false);
+  const [imgSrc, setImgSrc] = useState();
 
   useEffect(() => {
     const img = new Image();
 
     img.onload = () => {
+      setImgSrc(img.src);
       setLoaded(true);
       setErr(false);
     };
@@ -17,13 +19,14 @@ function BannerImage(props) {
       setErr(true);
     };
 
-    img.src = props.src;
+    img.src = new RegExp("/^(?:\/|[a-z]+:\/\/)/").test(props.src)
+      ? props.src : `//${window.host}:${window.backend_port}/${props.src}`;
   }, [props.src]);
 
   return (
     <div className="bannerImageWrapper">
       {(loaded && !error) && (
-        <img src={props.src} alt="cover"/>
+        <img src={imgSrc} alt="cover"/>
       )}
       {error && (
         <div className="placeholder"/>
--- web_ui/src/Pages/Media/CardImage.jsx
+++ web_ui/src/Pages/Media/CardImage.jsx
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from "react";
 function CardImage(props) {
   const [loaded, setLoaded] = useState(false);
   const [error, setErr] = useState(false);
+  const [imgSrc, setImgSrc] = useState();
 
   useEffect(() => {
     const img = new Image();
 
     img.onload = () => {
+      setImgSrc(img.src);
       setLoaded(true);
       setErr(false);
     };
@@ -17,13 +19,14 @@ function CardImage(props) {
       setErr(true);
     };
 
-    img.src = props.src;
+    img.src = new RegExp("/^(?:\/|[a-z]+:\/\/)/").test(props.src)
+      ? props.src : `//${window.host}:${window.backend_port}/${props.src}`;
   }, [props.src]);
 
   return (
     <div className="cardImageWrapper">
       {(loaded && !error) && (
-        <img src={props.src} alt="cover"/>
+        <img src={imgSrc} alt="cover"/>
       )}
       {loaded && error && (
         <div className="placeholder"/>
