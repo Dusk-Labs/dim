ff03593eb6da328c345739bf411dbe976021ccb4
Eray Chumak <eray_chumak@protonmail.com>
2021-02-18 15:38:08 +0000

    Fix duplicate redirection + add comment with reasons for some decisions

--- web_ui/src/Routes/Private.jsx
+++ web_ui/src/Routes/Private.jsx
@@ -33,10 +33,27 @@ function PrivateRoute(props) {
 
   // auto logout when logged out in another tab
   useEffect(() => {
+    /*
+      BroadcastChannel API doesn't provide any way to determine
+      if a message came from the same instance. This just makes
+      sure it doesn't e.g. head to /login twice.
+    */
+    let valid = false;
+
+    setTimeout(() => {
+      valid = true;
+    }, 1000);
+
     const bc = new BroadcastChannel("dim");
 
     bc.onmessage = (e) => {
+      if (!valid) return;
+
       if (e.data === "logout") {
+        /*
+          cannot use history.push, throws an error when
+          tab is not active and it tries to redirect.
+        */
         window.location.replace("/login");
       }
     };
