db0035c80b7eb1eef0187a0addbdab09766e68a7
Eray Chumak <eray_chumak@protonmail.com>
2021-02-20 23:13:07 +0000

    Remove old ghost components

--- web_ui/src/Pages/Media/MetaContent.jsx
+++ web_ui/src/Pages/Media/MetaContent.jsx
@@ -4,7 +4,6 @@ import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { fetchMediaInfo } from "../../actions/card.js";
-import CardImage from "./CardImage.jsx";
 
 import "./MetaContent.scss";
 
@@ -24,101 +23,11 @@ function MetaContent(props) {
     }
   }, [props.media_info]);
 
-  let metaContent;
-
-  // FETCH_MEDIA_INFO_START
-  // if (props.media_info.fetching) {
-  if (false) {
-    metaContent = (
-      <div className="meta-content">
-        <div className="cover">
-          <div className="placeholder"/>
-        </div>
-        <div className="overview">
-          <h1><div className="placeholder-text"/></h1>
-          <div className="genres">
-            <div className="placeholder-text"/>
-            <div className="placeholder-text"/>
-            <div className="placeholder-text"/>
-          </div>
-          <div className="description">
-            <div className="placeholder-text"/>
-          </div>
-          <div className="meta-info">
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  // FETCH_MEDIA_INFO_ERR
-  // if (props.media_info.fetched && props.media_info.error) {
-  if (false) {
-    console.log("[FETCH MEDIA INFO] ERR", props.media_info);
-
-    metaContent = (
-      <div className="meta-content">
-        <div className="cover">
-          <LazyImage/>
-        </div>
-        <div className="overview">
-          <div className="horizontal-err">
-            <FontAwesomeIcon icon="times-circle"/>
-            <p>FAILED TO LOAD</p>
-          </div>
-          <div className="genres">
-            <div className="placeholder-text"/>
-            <div className="placeholder-text"/>
-            <div className="placeholder-text"/>
-            <div className="placeholder-text"/>
-          </div>
-          <div className="description">
-            <div className="placeholder-text"/>
-          </div>
-          <div className="meta-info">
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-            <div className="info">
-              <h4><div className="placeholder-text"/></h4>
-              <div className="placeholder-text"/>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  let metaContent = <></>;
 
   // FETCH_MEDIA_INFO_OK
-  // if (props.media_info.fetched && !props.media_info.error) {
-  if (true) {
+  if (props.media_info.fetched && !props.media_info.error) {
     const {
-      poster_path,
       description,
       genres,
       name,
@@ -138,51 +47,46 @@ function MetaContent(props) {
 
     metaContent = (
       <div className="metaContent">
-        <CardImage src={poster_path}/>
-        <div className="overview">
-          <h1>{name}</h1>
-          <div className="genres">
-            <Link to={`/search?year=${year}`}>{year}</Link>
-            <FontAwesomeIcon icon="circle"/>
-            {genres &&
-              genres.map((genre, i) => <Link to={`/search/genre=${genre}`} key={i}>{genre}</Link>)
-            }
+        <h1>{name}</h1>
+        <div className="genres">
+          <Link to={`/search?year=${year}`}>{year}</Link>
+          <FontAwesomeIcon icon="circle"/>
+          {genres &&
+            genres.map((genre, i) => <Link to={`/search/genre=${genre}`} key={i}>{genre}</Link>)
+          }
+        </div>
+        <p className="description">{description}</p>
+        <div className="meta-info">
+          <div className="info">
+            <h4>ID</h4>
+            <p>{id}</p>
           </div>
-          <p className="description">{description}</p>
-          <div className="separator"/>
-          <div className="meta-info">
-            <div className="info">
-              <h4>ID</h4>
-              <p>{id}</p>
-            </div>
-            <div className="info">
-              <h4>Type</h4>
-              <p>{media_type}</p>
-            </div>
-            {!seasons && (
-              <div className="info">
-                <h4>Duration</h4>
-                <p>{length.hh}:{length.mm}:{length.ss}</p>
-              </div>
-            )}
-            {seasons && (
-              <div className="info">
-                <h4>Seasons</h4>
-                <p>{seasons}</p>
-              </div>
-            )}
-            <div className="info">
-              <h4>Rating</h4>
-              <p>{rating}/10</p>
-            </div>
+          <div className="info">
+            <h4>Type</h4>
+            <p>{media_type}</p>
           </div>
-          <div className="separator"/>
-          <div>
-              <Link to={`/play/${id}`} className="playBtn">
-                <p>Play media</p>
-                <FontAwesomeIcon icon="play"/>
-              </Link>
+          {!seasons && (
+            <div className="info">
+              <h4>Duration</h4>
+              <p>{length.hh}:{length.mm}:{length.ss}</p>
             </div>
+          )}
+          {seasons && (
+            <div className="info">
+              <h4>Seasons</h4>
+              <p>{seasons}</p>
+            </div>
+          )}
+          <div className="info">
+            <h4>Rating</h4>
+            <p>{rating}/10</p>
+          </div>
+        </div>
+        <div>
+          <Link to={`/play/${id}`} className="playBtn">
+            <p>Play media</p>
+            <FontAwesomeIcon icon="play"/>
+          </Link>
         </div>
       </div>
     );
