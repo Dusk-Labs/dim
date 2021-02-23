5acb8e74d9906053a1e2240cf94c9a9b4fb9247f
Eray Chumak <eray_chumak@protonmail.com>
2021-02-23 21:59:14 +0000

    Move 'empty' logic to individual components

--- web_ui/src/Pages/Dashboard.jsx
+++ web_ui/src/Pages/Dashboard.jsx
@@ -1,51 +1,19 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
 
 import Banners from "../Components/Banners/Index";
-import GhostCards from "../Components/CardList/Ghost";
 import CardList from "../Components/CardList/Index";
-import NewLibraryModal from "../Modals/NewLibrary/Index";
 
-import "./Dashboard.scss";
-
-function Dashboard(props) {
+function Dashboard() {
   useEffect(() => {
     document.title = "Dim - Dashboard";
   }, [])
 
   return (
     <div className="dashboard">
-      {Object.values(props.cards.items).flat().length > 0 && (
-        <>
-          <Banners/>
-          <CardList path={`//${window.host}:8000/api/v1/dashboard`}/>
-        </>
-      )}
-      {Object.values(props.cards.items).flat().length === 0 && (
-        <>
-          <div className="emptyDashboard">
-            <h2>Add your first library</h2>
-            <p>
-              You will be able to see all the media from your
-              libraries here, organized for quick and easy access.
-            </p>
-            <NewLibraryModal>
-              <button>Add library</button>
-            </NewLibraryModal>
-          </div>
-          <div className="card_list">
-            <GhostCards/>
-          </div>
-        </>
-      )}
+      <Banners/>
+      <CardList path={`//${window.host}:8000/api/v1/dashboard`}/>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  cards: state.card.cards
-});
-
-const mapActionsToProps = {};
-
-export default connect(mapStateToProps, mapActionsToProps)(Dashboard);
+export default Dashboard;
