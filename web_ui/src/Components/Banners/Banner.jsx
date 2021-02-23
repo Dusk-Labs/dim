c8fd068e73fbca724b751d9c4f05f05c7c3cc798
Eray Chumak <eray_chumak@protonmail.com>
2021-02-23 21:59:34 +0000

    Show 'add your first library' message if empty

--- web_ui/src/Components/Banners/Banner.jsx
+++ web_ui/src/Components/Banners/Banner.jsx
@@ -7,6 +7,7 @@ import ProgressBar from "./ProgressBar.jsx";
 import Image from "./Image.jsx";
 import PlayButton from "./PlayButton.jsx";
 import TruncText from "../../Helpers/TruncText.jsx";
+import NewLibraryModal from "../../Modals/NewLibrary/Index";
 
 import "./Banner.scss";
 
@@ -26,7 +27,14 @@ function Banner(props) {
       return (
         <div className="banner">
           <div className="placeholder">
-            <p>Empty</p>
+            <h2>Add your first library</h2>
+            <p>
+              You will be able to see all the media from your
+              libraries here, organized for quick and easy access.
+            </p>
+            <NewLibraryModal>
+              <button>Add library</button>
+            </NewLibraryModal>
           </div>
         </div>
       );
