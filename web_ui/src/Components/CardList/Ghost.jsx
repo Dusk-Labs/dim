c6890e8b4bb1b0a11a46d97d52f2fde1798eecc0
Eray Chumak <eray_chumak@protonmail.com>
2021-02-23 21:18:27 +0000

    Add card list ghost component

---
+++ web_ui/src/Components/CardList/Ghost.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function GhostCards() {
+  const sectionRef = useRef(null);
+
+  const [ count, setCount ] = useState(0);
+
+  useEffect(() => {
+    if (!sectionRef.current) return;
+
+    setCount(
+      Math.floor(sectionRef.current.offsetWidth / 230) * 2
+    );
+  }, [sectionRef.current]);
+
+  const cards = [];
+
+  for (let x = 0; x < count; x++) {
+    cards.push(
+      <div key={x} className="card-wrapper">
+        <div className="card">
+          <div className="placeholder"/>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <section className="showAfter100ms" ref={sectionRef}>
+      <div className="placeholderText"/>
+      <div className="cards">{cards}</div>
+    </section>
+  );
+}
+
+export default GhostCards;
