dcc75aa4cda0faef446966b45a345d45c9b2aac7
Eray Chumak <eray_chumak@protonmail.com>
2021-02-26 10:39:50 +0000

    Remove unused variable

--- ui/src/Pages/Preferences.jsx
+++ ui/src/Pages/Preferences.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
