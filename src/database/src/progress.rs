939339d836d81f7f7a771c2607392fc0fdb822d6
Valerian G <valerian.garleanu@pm.me>
2021-03-31 03:46:13 +0000

    Added new column to the progress table containing the timestamp of when the progress report was submited

--- src/database/src/progress.rs
+++ src/database/src/progress.rs
@@ -1,8 +1,14 @@
+use crate::episode::Episode;
+use crate::library::MediaType;
+use crate::media::Media;
 use crate::schema::progress;
-use crate::{media::Media, user::User};
+use crate::user::User;
+
 use diesel::prelude::*;
 use diesel::result::Error as DieselError;
 use serde::Serialize;
+
+use std::time::SystemTime;
 
 #[derive(Queryable, Debug, Identifiable, Associations, Serialize)]
 #[belongs_to(User, foreign_key = "user_id")]
@@ -13,6 +19,7 @@ pub struct Progress {
     pub delta: i32,
     pub media_id: i32,
     pub user_id: String,
+    pub populated: i32,
 }
 
 impl Progress {
@@ -22,6 +29,10 @@ impl Progress {
         uid: String,
         mid: i32,
     ) -> Result<usize, DieselError> {
+        let timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
         // NOTE: We could use `on_conflict` here but the diesel backend for sqlite doesnt support
         // this yet.
         if diesel::insert_into(progress::table)
@@ -29,6 +40,7 @@ impl Progress {
                 progress::delta.eq(delta),
                 progress::media_id.eq(mid),
                 progress::user_id.eq(uid.clone()),
+                progress::populated.eq(timestamp as i32),
             ))
             .execute(conn)
             .is_err()
@@ -38,7 +50,10 @@ impl Progress {
                     .filter(progress::media_id.eq(mid))
                     .filter(progress::user_id.eq(uid)),
             )
-            .set(progress::delta.eq(delta))
+            .set((
+                progress::delta.eq(delta),
+                progress::populated.eq(timestamp as i32),
+            ))
             .execute(conn)
         } else {
             Ok(1)
@@ -49,17 +64,22 @@ impl Progress {
         conn: &crate::DbConnection,
         uid: String,
         mid: i32,
-    ) -> Result<i32, DieselError> {
+    ) -> Result<Self, DieselError> {
         use crate::schema::progress::dsl::*;
 
         match progress
             .filter(media_id.eq(mid))
-            .filter(user_id.eq(uid))
-            .select(delta)
-            .first::<i32>(conn)
+            .filter(user_id.eq(uid.clone()))
+            .first::<Self>(conn)
         {
             Ok(x) => Ok(x),
-            Err(DieselError::NotFound) => Ok(0),
+            Err(DieselError::NotFound) => Ok(Self {
+                id: 0,
+                delta: 0,
+                media_id: mid,
+                user_id: uid,
+                populated: 0,
+            }),
             Err(e) => Err(e),
         }
     }
@@ -75,6 +95,31 @@ impl Progress {
             .select(delta)
             .load::<i32>(conn)?
             .iter()
+            .sum())
+    }
+
+    pub fn get_total_for_media(
+        conn: &crate::DbConnection,
+        media: &Media,
+        uid: String,
+    ) -> Result<i32, DieselError> {
+        match media.media_type {
+            Some(MediaType::Tv) => Self::get_total_for_tv(conn, uid, media),
+            _ => Self::get_for_media_user(conn, uid, media.id).map(|x| x.delta),
+        }
+    }
+
+    pub fn get_total_for_tv(
+        conn: &crate::DbConnection,
+        uid: String,
+        media: &Media,
+    ) -> Result<i32, DieselError> {
+        let episodes = Episode::get_all_of_tv(conn, media)?;
+
+        Ok(episodes
+            .iter()
+            .filter_map(|x| Self::get_for_media_user(conn, uid.clone(), x.media.id).ok())
+            .map(|x| x.delta)
             .sum())
     }
 }
