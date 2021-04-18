d4e6441b2da647cec31f4f21f47c17c9965cf5a3
Eray Chumak <eray_chumak@protonmail.com>
2021-04-18 19:26:11 +0100

    Remove intersection observer, redundant transition

--- ui/src/Components/CardList/UnmatchedCardList.jsx
+++ ui/src/Components/CardList/UnmatchedCardList.jsx
@@ -1,26 +1,6 @@
-import { useCallback, useEffect, useRef } from "react";
 import Card from "./Card.jsx";
 
 function UnmatchedCardList(props) {
-  const cardList = useRef(null);
-
-  const handleIntersect = useCallback((entries) => {
-    if (!cardList.current) return;
-    cardList.current.style.opacity = entries[0].isIntersecting ? "1" : ".2";
-  }, []);
-
-  useEffect(() => {
-    if (!cardList.current) return;
-
-    let options = {
-      threshold: .5,
-      rootMargin: "0px"
-    };
-
-    const observer = new IntersectionObserver(handleIntersect, options);
-    observer.observe(cardList.current);
-  }, [handleIntersect]);
-
   const { fetched, error } = props.cards;
 
   let sections = [];
@@ -42,7 +22,7 @@ function UnmatchedCardList(props) {
   }
 
   return (
-    <div className="card_list unmatched" ref={cardList}>
+    <div className="card_list unmatched">
       <section>
         <h1>Unmatched</h1>
         <p className="sectionDesc">Could not find an accurate match for these files.</p>
