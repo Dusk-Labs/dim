08878c66a6c8a7b295d0024dd6459378a2928fb7
Eray Chumak <eray_chumak@protonmail.com>
2021-04-18 15:18:36 +0100

    Add UnmatchedCardList component

---
+++ ui/src/Components/CardList/UnmatchedCardList.jsx
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useRef } from "react";
+import Card from "./Card.jsx";
+
+function UnmatchedCardList(props) {
+  const cardList = useRef(null);
+
+  const handleIntersect = useCallback((entries) => {
+    cardList.current.style.opacity = entries[0].isIntersecting ? "1" : ".2";
+  }, []);
+
+  useEffect(() => {
+    if (!cardList.current) return;
+
+    let options = {
+      threshold: 1,
+      rootMargin: "0px"
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(cardList.current);
+  }, [handleIntersect]);
+
+  let card_list;
+
+  const { fetched, error } = props.cards;
+
+  // ERR
+  if (fetched && error) {
+    card_list = (
+      <section>
+        <h1>Unmatched media</h1>
+        <p>Could not load unmatched media</p>
+      </section>
+    );
+  }
+
+  // OK
+  if (fetched && !error) {
+    const { items } = props.cards;
+
+    card_list = (
+      <section>
+        <h1>Unmatched media</h1>
+        <div className="cards">
+          {items.map((card, i) => <Card key={i} data={card}/>)}
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <div className="card_list unmatched" ref={cardList}>
+      {card_list}
+    </div>
+  );
+}
+
+export default UnmatchedCardList;
--- ui/src/Pages/Library/Index.jsx
+++ ui/src/Pages/Library/Index.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
 
+import { fetchLibraryUnmatched } from "../../actions/library.js";
 import Actions from "./Actions";
 import CardList from "../../Components/CardList/Index";
+import UnmatchedCardList from "../../Components/CardList/UnmatchedCardList.jsx";
 
-const Library = () => (
-  <div className="library">
-    <CardList path={`//${window.host}:8000/api/v1/library/${useParams().id}/media`}/>
-    <Actions id={useParams().id}/>
-  </div>
-);
+const Library = (props) => {
+  const params = useParams();
 
-export default Library;
+  const { fetchLibraryUnmatched, auth } = props;
+  const { token } = auth;
+
+  useEffect(() => {
+    fetchLibraryUnmatched(token, params.id);
+  }, [fetchLibraryUnmatched, params.id, token]);
+
+  const { fetched, items } = props.unmatched;
+
+  return (
+    <div className="library">
+      <CardList path={`//${window.host}:8000/api/v1/library/${params.id}/media`}/>
+      {(fetched && items.length > 0) && (
+        <UnmatchedCardList cards={props.unmatched}/>
+      )}
+      <Actions id={useParams().id}/>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  unmatched: state.library.fetch_library_unmatched
+});
+
+const mapActionsToProps = {
+  fetchLibraryUnmatched
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(Library);
