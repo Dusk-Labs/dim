8955502993f63160614a57f7c9b272f298a1379c
Valerian G <valerian.garleanu@pm.me>
2021-04-19 17:48:13 +0000

    fix bug where a future wasnt being awaited causing ffmpeg procs to pile up

--- src/stream_tracking.rs
+++ src/stream_tracking.rs
@@ -1,27 +1,12 @@
-use rocket::fairing::Fairing;
-use rocket::fairing::Info;
-use rocket::fairing::Kind;
-
-use rocket::http::ContentType;
-use rocket::http::Method;
-use rocket::http::Status;
-use rocket::request::FromRequest;
-
-use rocket::Data;
-use rocket::Outcome;
-use rocket::Request;
-use rocket::Response;
-use rocket::State;
-
 use crate::core::StateManager;
-
 use auth::Wrapper as Auth;
+use rocket::State;
 
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::RwLock;
 
-const PATHS_TO_FILTER: &'static [&'static str] = &["/api/v1/stream", "api/v1/stream"];
+use tokio::runtime::Handle;
 
 pub struct StreamTracking {
     streaming_sessions: Arc<RwLock<HashMap<u128, Vec<String>>>>,
@@ -33,13 +18,13 @@ impl StreamTracking {
         lock.entry(id).or_default().push(stream_id);
     }
 
-    pub fn kill_all(&self, state: &State<StateManager>, id: u128) {
+    pub fn kill_all(&self, tokio_rt: &State<Handle>, state: &State<StateManager>, id: u128) {
         let mut lock = self.streaming_sessions.write().unwrap();
 
         if let Some(v) = lock.get_mut(&id) {
             if !v.is_empty() {
                 for id in v.drain(..) {
-                    let _ = state.die(id);
+                    let _ = tokio_rt.block_on(state.die(id));
                 }
             }
         }
@@ -48,38 +33,6 @@ impl StreamTracking {
     pub fn get_for_gid(&self, gid: u128) -> Option<Vec<String>> {
         let lock = self.streaming_sessions.read().unwrap();
         lock.get(&gid).cloned()
-    }
-}
-
-impl Fairing for StreamTracking {
-    fn info(&self) -> Info {
-        Info {
-            name: "StreamTracker",
-            kind: Kind::Request,
-        }
-    }
-
-    fn on_request(&self, request: &mut Request, data: &Data) {
-        let token = match request.guard::<Auth>() {
-            Outcome::Success(x) => x,
-            _ => return,
-        };
-
-        let route = request
-            .uri()
-            .segments()
-            .take(3)
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join("/");
-
-        if PATHS_TO_FILTER.contains(&route.as_str()) {
-            return;
-        }
-
-        if let Outcome::Success(sessions) = request.guard::<State<StateManager>>() {
-            self.kill_all(&sessions, token.0.claims.id);
-        }
     }
 }
 
