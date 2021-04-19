192eb41fd0f9bae478fa8b5e1292929dd29ab357
Eray Chumak <eray_chumak@protonmail.com>
2021-04-20 00:49:51 +0100

    Change indentation to 2 spaces

--- ui/src/actions/library.js
+++ ui/src/actions/library.js
@@ -1,187 +1,183 @@
 import {
-    FETCH_LIBRARIES_START,
-    FETCH_LIBRARIES_OK,
-    FETCH_LIBRARIES_ERR,
-    FETCH_LIBRARY_INFO,
-    FETCH_LIBRARY_MEDIA,
-    NEW_LIBRARY_START,
-    NEW_LIBRARY_OK,
-    NEW_LIBRARY_ERR,
-    DEL_LIBRARY_START,
-    DEL_LIBRARY_OK,
-    DEL_LIBRARY_ERR,
-    RM_LIBRARY,
-    ADD_LIBRARY,
-    FETCH_LIBRARY_UNMATCHED_START,
-    FETCH_LIBRARY_UNMATCHED_ERR,
-    FETCH_LIBRARY_UNMATCHED_OK
+  FETCH_LIBRARIES_START,
+  FETCH_LIBRARIES_OK,
+  FETCH_LIBRARIES_ERR,
+  FETCH_LIBRARY_INFO,
+  FETCH_LIBRARY_MEDIA,
+  NEW_LIBRARY_START,
+  NEW_LIBRARY_OK,
+  NEW_LIBRARY_ERR,
+  DEL_LIBRARY_START,
+  DEL_LIBRARY_OK,
+  DEL_LIBRARY_ERR,
+  RM_LIBRARY,
+  ADD_LIBRARY,
+  FETCH_LIBRARY_UNMATCHED_START,
+  FETCH_LIBRARY_UNMATCHED_ERR,
+  FETCH_LIBRARY_UNMATCHED_OK
 } from "./types.js";
 
 export const fetchLibraries = (token) => async (dispatch) => {
-    dispatch({ type: FETCH_LIBRARIES_START });
+  dispatch({ type: FETCH_LIBRARIES_START });
 
-    try {
-        const config = {
-            headers: {
-                "authorization": token
-            }
-        };
-        const res = await fetch(`//${window.host}:8000/api/v1/library`, config);
+  try {
+    const config = {
+      headers: {
+        "authorization": token
+      }
+    };
 
-        if (res.status !== 200) {
-            return dispatch({
-                type: FETCH_LIBRARIES_ERR,
-                payload: res.statusText
-            });
-        }
+    const res = await fetch(`//${window.host}:8000/api/v1/library`, config);
 
-        const payload = await res.json();
-
-        dispatch({
-            type: FETCH_LIBRARIES_OK,
-            payload
-        });
-    } catch(err) {
-        dispatch({
-            type: FETCH_LIBRARIES_ERR,
-            payload: err
-        });
+    if (res.status !== 200) {
+      return dispatch({
+        type: FETCH_LIBRARIES_ERR,
+        payload: res.statusText
+      });
     }
+
+    const payload = await res.json();
+
+    dispatch({
+      type: FETCH_LIBRARIES_OK,
+      payload
+    });
+  } catch(err) {
+    dispatch({
+      type: FETCH_LIBRARIES_ERR,
+      payload: err
+    });
+  }
 };
 
 export const fetchLibraryUnmatched = (token, id) => async (dispatch) => {
-    dispatch({ type: FETCH_LIBRARY_UNMATCHED_START });
+  dispatch({ type: FETCH_LIBRARY_UNMATCHED_START });
 
-    try {
-        const config = {
-            headers: {
-                "authorization": token
-            }
-        };
+  try {
+    const config = {
+      headers: {
+        "authorization": token
+      }
+    };
 
-        const res = await fetch(`//${window.host}:8000/api/v1/library/${id}/unmatched`, config);
+    const res = await fetch(`//${window.host}:8000/api/v1/library/${id}/unmatched`, config);
 
-        if (res.status !== 200) {
-            return dispatch({
-                type: FETCH_LIBRARY_UNMATCHED_ERR,
-                payload: res.statusText
-            });
-        }
-
-        const payload = await res.json();
-
-        dispatch({
-            type: FETCH_LIBRARY_UNMATCHED_OK,
-            payload
-        });
-    } catch(err) {
-        dispatch({
-            type: FETCH_LIBRARY_UNMATCHED_ERR,
-            payload: err
-        });
+    if (res.status !== 200) {
+      return dispatch({
+        type: FETCH_LIBRARY_UNMATCHED_ERR,
+        payload: res.statusText
+      });
     }
+
+    const payload = await res.json();
+
+    dispatch({
+      type: FETCH_LIBRARY_UNMATCHED_OK,
+      payload
+    });
+  } catch(err) {
+    dispatch({
+      type: FETCH_LIBRARY_UNMATCHED_ERR,
+      payload: err
+    });
+  }
 };
 
 export const newLibrary = (token, data) => async (dispatch) => {
-    dispatch({ type: NEW_LIBRARY_START });
+  dispatch({ type: NEW_LIBRARY_START });
 
-    const options = {
-        method: "POST",
-        headers: {
-            "Authorization": token,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    };
+  const options = {
+    method: "POST",
+    headers: {
+      "Authorization": token,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
 
-    try {
-        const res = await fetch(`//${window.host}:8000/api/v1/library`, options);
+  try {
+    const res = await fetch(`//${window.host}:8000/api/v1/library`, options);
 
-        if (res.status !== 201) {
-            return dispatch({
-                type: NEW_LIBRARY_ERR,
-                payload: res.statusText
-            });
-        }
-
-        dispatch({ type: NEW_LIBRARY_OK });
-    } catch(err) {
-        dispatch({
-            type: NEW_LIBRARY_ERR,
-            payload: err
-        });
+    if (res.status !== 201) {
+      return dispatch({
+        type: NEW_LIBRARY_ERR,
+        payload: res.statusText
+      });
     }
+
+    dispatch({ type: NEW_LIBRARY_OK });
+  } catch(err) {
+    dispatch({
+      type: NEW_LIBRARY_ERR,
+      payload: err
+    });
+  }
 };
 
 export const delLibrary = (token, id) => async (dispatch) => {
-    dispatch({ type: DEL_LIBRARY_START });
+  dispatch({ type: DEL_LIBRARY_START });
 
-    const options = {
-        headers: {
-            "authorization": token
-        },
-        method: "DELETE"
-    };
+  const options = {
+    headers: {
+      "authorization": token
+    },
+    method: "DELETE"
+  };
 
-    try {
-        const res = await fetch(`//${window.host}:8000/api/v1/library/${id}`, options);
+  try {
+    const res = await fetch(`//${window.host}:8000/api/v1/library/${id}`, options);
 
-        if (res.status !== 204) {
-            return dispatch({
-                type: DEL_LIBRARY_ERR,
-                payload: res.statusText
-            });
-        }
-
-        dispatch({ type: DEL_LIBRARY_OK });
-    } catch(err) {
-        dispatch({
-            type: DEL_LIBRARY_ERR,
-            payload: err
-        });
+    if (res.status !== 204) {
+      return dispatch({
+        type: DEL_LIBRARY_ERR,
+        payload: res.statusText
+      });
     }
+
+    dispatch({ type: DEL_LIBRARY_OK });
+  } catch(err) {
+    dispatch({
+      type: DEL_LIBRARY_ERR,
+      payload: err
+    });
+  }
 };
 
 export const fetchLibraryInfo = () => async (dispatch) => {
-    dispatch({
-        type: FETCH_LIBRARY_INFO
-    });
+  dispatch({ type: FETCH_LIBRARY_INFO });
 };
 
 export const fetchLibraryMedia = () => async (dispatch) => {
-    dispatch({
-        type: FETCH_LIBRARY_MEDIA
-    });
+  dispatch({ type: FETCH_LIBRARY_MEDIA });
 };
 
 export const handleWsDelLibrary = (id) => async (dispatch) => {
-    dispatch({
-        type: RM_LIBRARY,
-        id: id,
-    });
+  dispatch({
+    type: RM_LIBRARY,
+    id: id,
+  });
 }
 
 export const handleWsNewLibrary = (token, id) => async (dispatch) => {
-    const options = {
-        headers: {
-            "Authorization": token
-        },
-        methid: "GET"
-    };
+  const options = {
+    headers: {
+      "Authorization": token
+    },
+    methid: "GET"
+  };
 
-    try {
-        const res = await fetch(`//${window.host}:8000/api/v1/library/${id}`, options);
+  try {
+    const res = await fetch(`//${window.host}:8000/api/v1/library/${id}`, options);
 
-        if (res.status !== 200) {
-            return;
-        }
-
-        const info = await res.json();
-
-        dispatch({
-            type: ADD_LIBRARY,
-            payload: info
-        });
-    } catch(err) {
+    if (res.status !== 200) {
+      return;
     }
+
+    const info = await res.json();
+
+    dispatch({
+      type: ADD_LIBRARY,
+      payload: info
+    });
+  } catch(err) {}
 }
--- ui/src/reducers/library.js
+++ ui/src/reducers/library.js
@@ -1,192 +1,192 @@
 import {
-    FETCH_LIBRARIES_START,
-    FETCH_LIBRARIES_OK,
-    FETCH_LIBRARIES_ERR,
-    FETCH_LIBRARY_INFO,
-    FETCH_LIBRARY_MEDIA,
-    NEW_LIBRARY_START,
-    NEW_LIBRARY_OK,
-    NEW_LIBRARY_ERR,
-    DEL_LIBRARY_START,
-    DEL_LIBRARY_OK,
-    DEL_LIBRARY_ERR,
-    RM_LIBRARY,
-    ADD_LIBRARY,
-    FETCH_LIBRARY_UNMATCHED_START,
-    FETCH_LIBRARY_UNMATCHED_OK,
-    FETCH_LIBRARY_UNMATCHED_ERR,
+  FETCH_LIBRARIES_START,
+  FETCH_LIBRARIES_OK,
+  FETCH_LIBRARIES_ERR,
+  FETCH_LIBRARY_INFO,
+  FETCH_LIBRARY_MEDIA,
+  NEW_LIBRARY_START,
+  NEW_LIBRARY_OK,
+  NEW_LIBRARY_ERR,
+  DEL_LIBRARY_START,
+  DEL_LIBRARY_OK,
+  DEL_LIBRARY_ERR,
+  RM_LIBRARY,
+  ADD_LIBRARY,
+  FETCH_LIBRARY_UNMATCHED_START,
+  FETCH_LIBRARY_UNMATCHED_OK,
+  FETCH_LIBRARY_UNMATCHED_ERR,
 } from "../actions/types.js";
 
 const fetch_libraries = {
-    items: [],
-    fetching: false,
-    fetched: false,
-    error: null
+  items: [],
+  fetching: false,
+  fetched: false,
+  error: null
 };
 
 const fetch_library_unmatched = {
-    items: {},
-    fetching: false,
-    fetched: false,
-    error: null
+  items: {},
+  fetching: false,
+  fetched: false,
+  error: null
 };
 
 const new_library = {
-    creating: false,
-    created: false,
-    error: null
+  creating: false,
+  created: false,
+  error: null
 };
 
 const del_library = {
-    deleting: false,
-    deleted: false,
-    error: null
+  deleting: false,
+  deleted: false,
+  error: null
 };
 
 const initialState = {
-    fetch_libraries,
-    fetch_library_unmatched,
-    new_library,
-    del_library
+  fetch_libraries,
+  fetch_library_unmatched,
+  new_library,
+  del_library
 };
 
 export default function libraryReducer(state = initialState, action) {
-    switch(action.type) {
-        case FETCH_LIBRARIES_START:
-            return {
-                ...state,
-                fetch_libraries: {
-                    items: [],
-                    fetching: true,
-                    fetched: false,
-                    error: null
-                }
-            }
-        case FETCH_LIBRARIES_OK:
-            return {
-                ...state,
-                fetch_libraries: {
-                    ...fetch_libraries,
-                    fetching: false,
-                    fetched: true,
-                    items: action.payload
-                }
-            }
-        case FETCH_LIBRARIES_ERR:
-            return {
-                ...state,
-                fetch_libraries: {
-                    ...fetch_libraries,
-                    fetching: false,
-                    fetched: true,
-                    error: action.payload
-                }
-            }
-        case FETCH_LIBRARY_UNMATCHED_START:
-            return {
-                ...state,
-                fetch_library_unmatched: {
-                    items: {},
-                    fetching: true,
-                    fetched: false,
-                    error: null
-                }
-            }
-        case FETCH_LIBRARY_UNMATCHED_OK:
-            return {
-                ...state,
-                fetch_library_unmatched: {
-                    ...fetch_library_unmatched,
-                    fetching: false,
-                    fetched: true,
-                    items: action.payload
-                }
-            }
-        case FETCH_LIBRARY_UNMATCHED_ERR:
-            return {
-                ...state,
-                fetch_library_unmatched: {
-                    ...fetch_library_unmatched,
-                    fetching: false,
-                    fetched: true,
-                    error: action.payload
-                }
-            }
-        case FETCH_LIBRARY_INFO:
-            return state;
-        case FETCH_LIBRARY_MEDIA:
-            return state;
-        case NEW_LIBRARY_START:
-            return {
-                ...state,
-                new_library: {
-                    creating: true,
-                    created: false,
-                    error: null
-                }
-            }
-        case NEW_LIBRARY_OK:
-            return {
-                ...state,
-                new_library: {
-                    ...new_library,
-                    creating: false,
-                    created: true
-                }
-            }
-        case NEW_LIBRARY_ERR:
-            return {
-                ...state,
-                new_library: {
-                    creating: false,
-                    created: false,
-                    error: action.payload
-                }
-            }
-        case DEL_LIBRARY_START:
-            return {
-                ...state,
-                del_library: {
-                    deleting: true,
-                    deleted: false,
-                    error: null
-                }
-            }
-        case DEL_LIBRARY_OK:
-            return {
-                ...state,
-                del_library: {
-                    ...del_library,
-                    deleting: false,
-                    deleted: true
-                }
-            }
-        case DEL_LIBRARY_ERR:
-            return {
-                ...state,
-                del_library: {
-                    deleting: false,
-                    deleted: false,
-                    error: action.payload
-                }
-            }
-        case RM_LIBRARY:
-            return {
-                ...state,
-                fetch_libraries: {
-                    ...state.fetch_libraries,
-                    items: state.fetch_libraries.items.filter(item => item.id !== action.id)
-                }
-            }
-        case ADD_LIBRARY:
-            return {
-                ...state,
-                fetch_libraries: {
-                    ...state.fetch_libraries,
-                    items: [...state.fetch_libraries.items, action.payload]
-                }
-            }
-        default:
-            return state;
-    }
+  switch(action.type) {
+    case FETCH_LIBRARIES_START:
+      return {
+        ...state,
+        fetch_libraries: {
+          items: [],
+          fetching: true,
+          fetched: false,
+          error: null
+        }
+      }
+    case FETCH_LIBRARIES_OK:
+      return {
+        ...state,
+        fetch_libraries: {
+          ...fetch_libraries,
+          fetching: false,
+          fetched: true,
+          items: action.payload
+        }
+      }
+    case FETCH_LIBRARIES_ERR:
+      return {
+        ...state,
+        fetch_libraries: {
+          ...fetch_libraries,
+          fetching: false,
+          fetched: true,
+          error: action.payload
+        }
+      }
+    case FETCH_LIBRARY_UNMATCHED_START:
+      return {
+        ...state,
+        fetch_library_unmatched: {
+          items: {},
+          fetching: true,
+          fetched: false,
+          error: null
+        }
+      }
+    case FETCH_LIBRARY_UNMATCHED_OK:
+      return {
+        ...state,
+        fetch_library_unmatched: {
+          ...fetch_library_unmatched,
+          fetching: false,
+          fetched: true,
+          items: action.payload
+        }
+      }
+    case FETCH_LIBRARY_UNMATCHED_ERR:
+      return {
+        ...state,
+        fetch_library_unmatched: {
+          ...fetch_library_unmatched,
+          fetching: false,
+          fetched: true,
+          error: action.payload
+        }
+      }
+    case FETCH_LIBRARY_INFO:
+      return state;
+    case FETCH_LIBRARY_MEDIA:
+      return state;
+    case NEW_LIBRARY_START:
+      return {
+        ...state,
+        new_library: {
+          creating: true,
+          created: false,
+          error: null
+        }
+      }
+    case NEW_LIBRARY_OK:
+      return {
+        ...state,
+        new_library: {
+          ...new_library,
+          creating: false,
+          created: true
+        }
+      }
+    case NEW_LIBRARY_ERR:
+      return {
+        ...state,
+        new_library: {
+          creating: false,
+          created: false,
+          error: action.payload
+        }
+      }
+    case DEL_LIBRARY_START:
+      return {
+        ...state,
+        del_library: {
+          deleting: true,
+          deleted: false,
+          error: null
+        }
+      }
+    case DEL_LIBRARY_OK:
+      return {
+        ...state,
+        del_library: {
+          ...del_library,
+          deleting: false,
+          deleted: true
+        }
+      }
+    case DEL_LIBRARY_ERR:
+      return {
+        ...state,
+        del_library: {
+          deleting: false,
+          deleted: false,
+          error: action.payload
+        }
+      }
+    case RM_LIBRARY:
+      return {
+        ...state,
+        fetch_libraries: {
+          ...state.fetch_libraries,
+          items: state.fetch_libraries.items.filter(item => item.id !== action.id)
+        }
+      }
+    case ADD_LIBRARY:
+      return {
+        ...state,
+        fetch_libraries: {
+          ...state.fetch_libraries,
+          items: [...state.fetch_libraries.items, action.payload]
+        }
+      }
+    default:
+      return state;
+  }
 }
