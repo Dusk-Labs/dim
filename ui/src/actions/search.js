0a4a0995ac4b986a943acfc4bdf42cb4e22586d7
Eray Chumak <eray_chumak@protonmail.com>
2021-04-20 00:49:51 +0100

    Change indentation to 2 spaces

--- ui/src/actions/search.js
+++ ui/src/actions/search.js
@@ -1,78 +1,78 @@
 import {
-    QUICK_SEARCH_OK,
-    QUICK_SEARCH_ERR,
-    SEARCH_START,
-    SEARCH_OK,
-    SEARCH_ERR
+  QUICK_SEARCH_OK,
+  QUICK_SEARCH_ERR,
+  SEARCH_START,
+  SEARCH_OK,
+  SEARCH_ERR
 } from "./types.js";
 
 export const search = (params, token) => async (dispatch) => {
-    dispatch({ type: SEARCH_START });
+  dispatch({ type: SEARCH_START });
 
-    try {
-        const config = {
-            headers: {
-                "authorization": token,
-            }
-        };
+  try {
+    const config = {
+      headers: {
+        "authorization": token,
+      }
+    };
 
-        const res = await fetch(`//${window.host}:8000/api/v1/search${params}`, config);
+    const res = await fetch(`//${window.host}:8000/api/v1/search${params}`, config);
 
-        if (res.status === 404) {
-            return dispatch({
-                type: SEARCH_OK,
-                payload: []
-            });
-        }
-
-        if (res.status !== 200) {
-            return dispatch({
-                type: SEARCH_ERR,
-                payload: res.statusText
-            });
-        }
-
-        const payload = await res.json();
-
-        dispatch({
-            type: SEARCH_OK,
-            payload
-        });
-    } catch(err) {
-        dispatch({
-            type: SEARCH_ERR,
-            payload: err
-        });
+    if (res.status === 404) {
+      return dispatch({
+        type: SEARCH_OK,
+        payload: []
+      });
     }
+
+    if (res.status !== 200) {
+      return dispatch({
+        type: SEARCH_ERR,
+        payload: res.statusText
+      });
+    }
+
+    const payload = await res.json();
+
+    dispatch({
+      type: SEARCH_OK,
+      payload
+    });
+  } catch(err) {
+    dispatch({
+      type: SEARCH_ERR,
+      payload: err
+    });
+  }
 };
 
 export const quickSearch = (query, token) => async (dispatch) => {
-    try {
-        const config = {
-            headers: {
-                "authorization": token,
-            }
-        };
+  try {
+    const config = {
+      headers: {
+        "authorization": token,
+      }
+    };
 
-        const res = await fetch(`//${window.host}:8000/api/v1/search?query=${query}&quick=true`, config);
+    const res = await fetch(`//${window.host}:8000/api/v1/search?query=${query}&quick=true`, config);
 
-        if (res.status !== 200) {
-            return dispatch({
-                type: QUICK_SEARCH_ERR,
-                payload: res.statusText
-            });
-        }
-
-        const payload = await res.json();
-
-        dispatch({
-            type: QUICK_SEARCH_OK,
-            payload
-        });
-    } catch(err) {
-        dispatch({
-            type: QUICK_SEARCH_ERR,
-            payload: err
-        });
+    if (res.status !== 200) {
+      return dispatch({
+        type: QUICK_SEARCH_ERR,
+        payload: res.statusText
+      });
     }
+
+    const payload = await res.json();
+
+    dispatch({
+      type: QUICK_SEARCH_OK,
+      payload
+    });
+  } catch(err) {
+    dispatch({
+      type: QUICK_SEARCH_ERR,
+      payload: err
+    });
+  }
 };
--- ui/src/reducers/user.js
+++ ui/src/reducers/user.js
@@ -1,40 +1,40 @@
 import {
-    FETCH_USER_START,
-    FETCH_USER_OK,
-    FETCH_USER_ERR
+  FETCH_USER_START,
+  FETCH_USER_OK,
+  FETCH_USER_ERR
 } from "../actions/types.js";
 
 const initialState = {
-    info: {},
-    fetching: false,
-    fetched: false,
-    error: null
+  info: {},
+  fetching: false,
+  fetched: false,
+  error: null
 };
 
 export default function userReducer(state = initialState, action) {
-    switch(action.type) {
-        case FETCH_USER_START:
-            return {
-                info: {},
-                fetching: true,
-                fetched: false,
-                error: null
-            }
-        case FETCH_USER_OK:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                info: action.payload
-            }
-        case FETCH_USER_ERR:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                error: action.payload
-            }
-        default:
-            return state;
-    }
+  switch(action.type) {
+    case FETCH_USER_START:
+      return {
+        info: {},
+        fetching: true,
+        fetched: false,
+        error: null
+      }
+    case FETCH_USER_OK:
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        info: action.payload
+      }
+    case FETCH_USER_ERR:
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        error: action.payload
+      }
+    default:
+      return state;
+  }
 }
