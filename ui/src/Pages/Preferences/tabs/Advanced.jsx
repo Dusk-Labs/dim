3b0a84f8d702d501d92bcb9430d04e8960ce213a
liamdebellada <liam.debell@ada.ac.uk>
2021-04-22 20:22:32 +0100

    Changed tabbing system to use indexes, created profile layout and form component

---
+++ ui/src/Pages/Preferences/tabs/Advanced.jsx
@@ -0,0 +1,5 @@
+export default function AdvancedTab() {
+    return (
+        <div>advanced</div>
+    )
+}
---
+++ ui/src/Pages/Preferences/tabs/Appearance.jsx
@@ -0,0 +1,5 @@
+export default function AppearanceTab () {
+     return (
+        <div>hi</div>   
+    )
+}
