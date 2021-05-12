cccbcb1d1a3988a0d7ba7acbfe4cbab385a12859
Eray Chumak <eray_chumak@protonmail.com>
2021-05-12 05:26:27 +0100

    Add preferences appearance section component

---
+++ ui/src/Pages/Preferences/Appearance.jsx
@@ -0,0 +1,12 @@
+function Appearance() {
+  return (
+    <section>
+      <h3>This section will house the following features:</h3>
+      <p>Change theme</p>
+      <p>Show/hide media card names</p>
+      <p>Keep sidebar always compact</p>
+    </section>
+  );
+}
+
+export default Appearance;
