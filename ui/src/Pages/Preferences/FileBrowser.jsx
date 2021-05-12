a4dd8e0f563af40ed9a8729b66e3920e6f54693f
Eray Chumak <eray_chumak@protonmail.com>
2021-05-12 05:26:17 +0100

    Add preferences file browser section component

---
+++ ui/src/Pages/Preferences/FileBrowser.jsx
@@ -0,0 +1,11 @@
+function FileBrowser() {
+  return (
+    <section>
+      <h3>This section will house the following features:</h3>
+      <p>Change default path</p>
+      <p>Change to list or grid view</p>
+    </section>
+  );
+}
+
+export default FileBrowser;
