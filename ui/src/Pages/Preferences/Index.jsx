db7db1cfb0bb5c2e5e287da6fa35225b5f82f29d
Eray Chumak <eray_chumak@protonmail.com>
2021-05-12 05:27:12 +0100

    Refactor preferences to function component

---
+++ ui/src/Pages/Preferences/Index.jsx
@@ -0,0 +1,83 @@
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { checkAdminExists } from "../../actions/auth.js";
+
+import Account from "./Account";
+import Invites from "./Invites";
+import FileBrowser from "./FileBrowser";
+import Appearance from "./Appearance";
+import Advanced from "./Advanced";
+
+import "./Index.scss";
+
+function Preferences() {
+  const dispatch = useDispatch();
+
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    document.title = "Dim - Preferences";
+  }, []);
+
+  useEffect(() => {
+    dispatch(checkAdminExists());
+  }, [dispatch]);
+
+  const switchTo = useCallback((i) => {
+    setActive(i);
+  }, []);
+
+  return (
+    <div className="preferencesPage">
+      <div className="preferences">
+        <nav>
+          <section>
+            <p>Preferences</p>
+            <div className="fields">
+              <p
+                className={`field ${active === 0 ? "active" : ""}`}
+                onClick={() => switchTo(0)}
+              >
+                Account
+              </p>
+              <p
+                className={`field ${active === 1 ? "active" : ""}`}
+                onClick={() => switchTo(1)}
+              >
+                Invites
+              </p>
+              <p
+                className={`field ${active === 2 ? "active" : ""}`}
+                onClick={() => switchTo(2)}
+              >
+                File Browser
+              </p>
+              <p
+                className={`field ${active === 3 ? "active" : ""}`}
+                onClick={() => switchTo(3)}
+              >
+                Appearance
+              </p>
+              <p
+                className={`field ${active === 4 ? "active" : ""}`}
+                onClick={() => switchTo(4)}
+              >
+                Advanced
+              </p>
+            </div>
+          </section>
+        </nav>
+        <div className="content">
+          {active === 0 && <Account/>}
+          {active === 1 && <Invites/>}
+          {active === 2 && <FileBrowser/>}
+          {active === 3 && <Appearance/>}
+          {active === 4 && <Advanced/>}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Preferences;
