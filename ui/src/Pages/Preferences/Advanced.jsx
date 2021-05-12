44d117af8803eb50918fdb778e7ef8db9b367c8f
Eray Chumak <eray_chumak@protonmail.com>
2021-05-12 05:26:34 +0100

    Add preferences advanced section component

---
+++ ui/src/Pages/Preferences/Advanced.jsx
@@ -0,0 +1,12 @@
+function Appearance() {
+  return (
+    <section>
+      <h3>This section will house the following features:</h3>
+      <p>Change port Dim runs on</p>
+      <p>Limit active transcoding sessions</p>
+      <p>Enable hardware acceleration</p>
+    </section>
+  );
+}
+
+export default Appearance;
