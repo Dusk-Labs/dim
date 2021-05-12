4e4e965bb2923ee2e412b015bfbc09e61f377372
Eray Chumak <eray_chumak@protonmail.com>
2021-05-12 05:25:42 +0100

    Add preferences account section component

---
+++ ui/src/Pages/Preferences/Account.jsx
@@ -0,0 +1,11 @@
+function Account() {
+  return (
+    <section>
+      <h3>This section will house the following features:</h3>
+      <p>Change username and profile picture</p>
+      <p>Manage password</p>
+    </section>
+  );
+}
+
+export default Account;
