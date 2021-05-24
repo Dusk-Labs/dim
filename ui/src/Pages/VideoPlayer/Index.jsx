cfc24e5fda525549534767cad181fc10b8a9f80a
Eray Chumak <eray_chumak@protonmail.com>
2021-05-24 01:07:19 +0100

    Add subtitles track switcher menu

--- ui/src/Pages/VideoPlayer/Index.jsx
+++ ui/src/Pages/VideoPlayer/Index.jsx
@@ -5,6 +5,7 @@ import { MediaPlayer } from "dashjs";
 import { clearMediaInfo, fetchExtraMediaInfo, fetchMediaInfo } from "../../actions/card";
 import { VideoPlayerContext } from "./Context";
 import RingLoad from "../../Components/Load/Ring";
+import Menus from "./Menus";
 import VideoControls from "./Controls/Index";
 import ErrorBox from "./ErrorBox";
 import ContinueProgress from "./ContinueProgress";
@@ -37,12 +38,17 @@ function VideoPlayer(props) {
   const [ GID, setGID ] = useState();
 
   const [ videoTracks, setVideoTracks ] = useState([]);
+  // eslint-disable-next-line no-unused-vars
   const [ currentVideoTrack, setCurrentVideoTrack ] = useState(0);
   const [ audioTracks, setAudioTracks ] = useState([]);
+  // eslint-disable-next-line no-unused-vars
   const [ currentAudioTrack, setCurrentAudioTrack ] = useState(0);
   const [ subtitleTracks, setSubtitleTracks ] = useState([]);
-  const [ currentSubtitleTrack, setCurrentSubtitleTrack ] = useState(0);
+  const [ currentSubtitleTrack, setCurrentSubtitleTrack ] = useState(-1);
   const [ virtualManifestLoaded, setVirtualManifestLoaded ] = useState(false);
+
+  const [subReady, setSubReady] = useState(false);
+  const [prevSubs, setPrevSubs] = useState();
 
   const [manifestLoading, setManifestLoading] = useState(false);
   const [manifestLoaded, setManifestLoaded] = useState(false);
@@ -52,15 +58,19 @@ function VideoPlayer(props) {
   const [fullscreen, setFullscreen] = useState(false);
   const [muted, setMuted] = useState(false);
   const [error, setError] = useState();
-  const [videoUUID, setVideoUUID] = useState();
   const [paused, setPaused] = useState(false);
-  const [currentTextTrack, setCurrentTextTrack] = useState(0);
   const [textTrackEnabled, setTextTrackEnabled] = useState(false);
   const [episode, setEpisode] = useState();
 
   const [buffer, setBuffer] = useState(true);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [currentCue, setCurrentCue] = useState("");
+
+  const [ prevSubTrack, setPrevSubTrack ] = useState(0);
+  const [ showSubSelection, setShowSubSelection ] = useState(false);
+
+  const [ idleCount, setIdleCount ] = useState(0);
 
   const { match } = props;
   const { params } = match;
@@ -176,7 +186,7 @@ function VideoPlayer(props) {
     setManifestLoaded(false);
     setManifestLoading(true);
 
-    const includes = `${videoTracks[currentVideoTrack].id},${audioTracks[currentAudioTrack].id}${subtitleTracks.length > 0 ? `,${subtitleTracks[currentSubtitleTrack].id}` : ""}`;
+    const includes = `${videoTracks[currentVideoTrack].id},${audioTracks[currentAudioTrack].id}`;
     const url = `/api/v1/stream/${GID}/manifest.mpd?start_num=0&should_kill=false&includes=${includes}`;
     const mediaPlayer = MediaPlayer().create();
 
@@ -223,21 +233,22 @@ function VideoPlayer(props) {
         sessionStorage.clear();
       })();
     };
-  }, [GID, audioTracks, auth.token, currentAudioTrack, currentSubtitleTrack, currentVideoTrack, subtitleTracks, videoTracks, virtualManifestLoaded]);
+  }, [GID, audioTracks, auth.token, currentAudioTrack, currentVideoTrack, videoTracks, virtualManifestLoaded]);
 
   const seekTo = useCallback(async newTime => {
     const newSegment = Math.floor(newTime / 5);
 
+    setCurrentCue("");
     setCurrentTime(newTime);
     setBuffer(0);
 
-    const includes = `${videoTracks[currentVideoTrack].id},${audioTracks[currentAudioTrack].id}${subtitleTracks.length > 0 ? `,${subtitleTracks[currentSubtitleTrack].id}` : ""}`;
+    const includes = `${videoTracks[currentVideoTrack].id},${audioTracks[currentAudioTrack].id}`;
     const url = `/api/v1/stream/${GID}/manifest.mpd?start_num=${newSegment}&should_kill=true&includes=${includes}`;
 
     player.attachSource(url);
 
     setSeeking(false);
-  }, [GID, audioTracks, currentAudioTrack, currentSubtitleTrack, currentVideoTrack, player, subtitleTracks, videoTracks]);
+  }, [GID, audioTracks, currentAudioTrack, currentVideoTrack, player, videoTracks]);
 
   const eManifestLoad = useCallback(() => {
     setManifestLoading(false);
@@ -299,6 +310,11 @@ function VideoPlayer(props) {
     setBuffer(Math.round(player.getBufferLength()));
   }, [player]);
 
+  useEffect(() => {
+    if (showSubSelection) return;
+    setIdleCount(state => state += 1);
+  }, [currentTime, showSubSelection]);
+
   // video events
   useEffect(() => {
     if (!player) return;
@@ -347,11 +363,8 @@ function VideoPlayer(props) {
     buffer,
     paused,
     canPlay,
-    currentTextTrack,
-    setCurrentTextTrack,
     textTrackEnabled,
     setTextTrackEnabled,
-    videoUUID,
     overlay: overlay.current,
     seekTo,
     episode,
@@ -360,15 +373,36 @@ function VideoPlayer(props) {
     audioTracks,
     currentAudioTrack,
     subtitleTracks,
-    currentSubtitleTrack
+    currentSubtitleTrack,
+    setCurrentSubtitleTrack,
+    GID,
+    currentCue,
+    setCurrentCue,
+    showSubSelection,
+    setShowSubSelection,
+    prevSubTrack,
+    setPrevSubTrack,
+    subReady,
+    setSubReady,
+    prevSubs,
+    setPrevSubs,
+    idleCount,
+    setIdleCount
   };
 
   return (
     <VideoPlayerContext.Provider value={initialValue}>
       <div className="videoPlayer" ref={videoPlayer}>
-        <video ref={video}/>
+        <video ref={video}>
+          <track
+            id="videoSubTrack"
+            kind="subtitles"
+            default
+          />
+        </video>
         <VideoSubtitles/>
         <div className="overlay" ref={overlay}>
+          {(!error && (manifestLoaded && canPlay && showSubSelection)) && <Menus/>}
           {(!error && (manifestLoaded && canPlay)) && <VideoControls/>}
           {(!error & (manifestLoading || !canPlay) || waiting) && <RingLoad/>}
           {((!error && (manifestLoaded && canPlay)) && extra_media_info.info.progress > 0) && (
