7c27e1e603d190620e50449f79b89cdeb7f014f6
Eray Chumak <eray_chumak@protonmail.com>
2021-05-24 01:08:24 +0100

    Refactor, fix some logic

--- ui/src/Pages/VideoPlayer/Subtitles.jsx
+++ ui/src/Pages/VideoPlayer/Subtitles.jsx
@@ -5,11 +5,9 @@ import { VideoPlayerContext } from "./Context";
 import "./Subtitles.scss";
 
 function VideoSubtitles() {
-  const { subtitleTracks, currentSubtitleTrack, player, currentTextTrack, textTrackEnabled, video, canPlay } = useContext(VideoPlayerContext);
+  const { prevSubs, setPrevSubs, subReady, setSubReady, currentCue, setCurrentCue, subtitleTracks, currentSubtitleTrack, player, textTrackEnabled, video, canPlay } = useContext(VideoPlayerContext);
 
-  const [currentCue, setCurrentCue] = useState("");
   const [show, setShow] = useState(false);
-  const [subReady, setSubReady] = useState(false);
 
   // relative to window width
   const updateBlackBarHeight = useCallback(() => {
@@ -33,12 +31,12 @@ function VideoSubtitles() {
     } else {
       setShow(false);
     }
-  }, []);
+  }, [setCurrentCue]);
 
   useEffect(() => {
-    if (subtitleTracks.length === 0 || !textTrackEnabled) return;
+    if (subtitleTracks.length === 0 || !textTrackEnabled || !video.current || currentSubtitleTrack === -1 || prevSubs === currentSubtitleTrack) return;
 
-    let prevCount = 0;
+    let prev = 0;
 
     console.log("[Subtitles] preparing subtitle track", subtitleTracks[currentSubtitleTrack]);
 
@@ -46,38 +44,37 @@ function VideoSubtitles() {
       const req = await fetch(`/api/v1/stream/${subtitleTracks[currentSubtitleTrack].id}/data/stream.vtt`);
       const text = await req.text();
 
-      if (!text || (text && text.length === prevCount)) {
+      if (text && text.length === prev.length) {
         console.log("[Subtitles] subtitles fully loaded");
         clearInterval(intervalID);
         setSubReady(true);
+        setPrevSubs(currentSubtitleTrack);
+
+        const videoSubTrack = document.getElementById("videoSubTrack");
+        videoSubTrack.src = `/api/v1/stream/${subtitleTracks[currentSubtitleTrack].id}/data/stream.vtt`;
       } else {
         console.log("[Subtitles] partially loaded, re-fetching again in 3 seconds");
-        prevCount = text.length;
+        prev = text;
       }
     }, 3000);
-  }, [currentSubtitleTrack, subtitleTracks, textTrackEnabled]);
+
+    return () => {
+      console.log("[Subtitles] component unmounted, clearing fetching interval");
+      clearInterval(intervalID);
+    };
+  }, [currentSubtitleTrack, prevSubs, setPrevSubs, setSubReady, subtitleTracks, textTrackEnabled, video]);
 
   useEffect(() => {
     if (!subReady || !player) return;
 
     console.log("[Subtitles] ready to show subtitles", currentSubtitleTrack);
-
-    player.setTextTrack(currentSubtitleTrack);
   }, [currentSubtitleTrack, player, subReady, textTrackEnabled]);
 
   useEffect(() => {
-    if (!player) return;
+    if (video.current) return;
     console.log("[Subtitles] setting player text status to", textTrackEnabled);
-    player.enableText(textTrackEnabled);
-  }, [player, textTrackEnabled]);
-
-  useEffect(() => {
-    if (!player) return;
-
-    setInterval(() => {
-      console.log("[Subtitles] is enabled?", player.isTextEnabled());
-    }, 5000);
-  }, [player]);
+    video.current.textTracks[0].mode = textTrackEnabled ? "showing" : "hidden";
+  }, [textTrackEnabled, video]);
 
   useEffect(() => {
     window.addEventListener("resize", updateBlackBarHeight);
@@ -92,12 +89,14 @@ function VideoSubtitles() {
   useEffect(() => {
     if (!video.current || !canPlay || !subReady) return;
 
-    const track = video.current.textTracks[currentTextTrack];
+    console.log("[Subtitles] html video tracks", video.current.textTracks);
+
+    const track = video.current.textTracks[0];
 
     if (track) {
       track.addEventListener("cuechange", handleCueChange);
     }
-  }, [canPlay, currentTextTrack, handleCueChange, subReady, video]);
+  }, [canPlay, currentSubtitleTrack, handleCueChange, subReady, video]);
 
   return (
     <div className={`videoSubtitles show-${textTrackEnabled && show}`}>
