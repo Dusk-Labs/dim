5480d4900873239ab520f40139cd952645303bc2
Eray Chumak <eray_chumak@protonmail.com>
2021-05-29 14:51:33 +0100

    Add redux video reducer

---
+++ ui/src/reducers/video/track.js
@@ -0,0 +1,34 @@
+import {
+  SET_TRACKS,
+  UPDATE_TRACK
+} from "../../actions/types.js";
+
+export default function trackReducer(state, action) {
+  switch(action.type) {
+    case SET_TRACKS:
+      return {
+        video: {
+          ...state.video,
+          list: action.tracks.video
+        },
+        audio: {
+          ...state.audio,
+          list: action.tracks.audio
+        },
+        subtitle: {
+          ...state.subtitle,
+          list: action.tracks.subtitle
+        }
+      };
+    case UPDATE_TRACK:
+      return {
+        ...state,
+        [action.track]: {
+          ...state[action.track],
+          ...action.data
+        }
+      };
+    default:
+      return state;
+  }
+}
