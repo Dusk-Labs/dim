f456b4c0587f5b23ebe40fe6a38c0e1608333fe8
liamdebellada <liam.debell@ada.ac.uk>
2021-05-29 19:11:50 +0100

    Added new preferences sidebar

--- ui/src/Pages/Preferences/Index.jsx
+++ ui/src/Pages/Preferences/Index.jsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useCallback, useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import { checkAdminExists } from "../../actions/auth.js";
 
@@ -11,10 +11,24 @@ import Advanced from "./Advanced";
 
 import "./Index.scss";
 
-function Preferences() {
+function Preferences(props) {
+  const user = useSelector(store => store.user);
   const dispatch = useDispatch();
 
   const [active, setActive] = useState(1);
+
+  const editBadge = useRef(null);
+  const leftProfilePic = useRef(null);
+  const badge = useRef(null);
+
+  const [tabs, setTabs] = useState([]);
+  const [badgePos, setBagePos] = useState({right: 0, top: 0});
+
+  const tempStats = [
+    {"name" : "Watched", val: "33h"},
+    {"name" : "Users", val: "4"},
+    {"name" : "Tokens", val: "3"}
+  ];
 
   useEffect(() => {
     document.title = "Dim - Preferences";
@@ -28,49 +42,55 @@ function Preferences() {
     setActive(i);
   }, []);
 
+  function computeCirclePos() {
+    let containerHeight = leftProfilePic.current.clientHeight;
+    let containerRadius = leftProfilePic.current.clientWidth / 2;
+    let badgeWidth = editBadge.current.clientWidth;
+    let smallBadgeWidth = badge.current.clientWidth;
+
+    setBagePos({
+      top: (containerHeight / 2) - badgeWidth / 2,
+      left: (containerRadius) + badgeWidth - smallBadgeWidth,
+      width: containerRadius + smallBadgeWidth / 2
+    });
+  }
+
   return (
     <div className="preferencesPage">
-      <nav>
-        <h1>Preferences</h1>
-        <div className="items">
-          <p
-            className={`item ${active === 0 ? "active" : ""}`}
-            onClick={() => switchTo(0)}
-          >
-            Account
-          </p>
-          <p
-            className={`item ${active === 1 ? "active" : ""}`}
-            onClick={() => switchTo(1)}
-          >
-            Invites
-          </p>
-          <p
-            className={`item ${active === 2 ? "active" : ""}`}
-            onClick={() => switchTo(2)}
-          >
-            File Browser
-          </p>
-          <p
-            className={`item ${active === 3 ? "active" : ""}`}
-            onClick={() => switchTo(3)}
-          >
-            Appearance
-          </p>
-          <p
-            className={`item ${active === 4 ? "active" : ""}`}
-            onClick={() => switchTo(4)}
-          >
-            Advanced
-          </p>
+      <div className="preferences">
+        <div className="leftBar">
+          <div className="leftBarImgContainer">
+            <div ref={leftProfilePic} class="leftBarImgParent">
+              <img className="leftBarProfileImg" src={user.info.picture}/>
+              <div className="circle" style={badgePos} ref={editBadge}><div ref={badge} className="leftBarImgEdit"/></div>
+            </div>
+          </div>
+          <div className="leftBarNames">
+            <div className="leftBarUsername">
+              {user.info.username}
+            </div>
+            <div className="leftBarRole">
+              {"Admin"}
+            </div>
+          </div>
+          <div className="leftBarStatistics">
+            {tempStats.map((stat, i) => (
+              <div key={i} className="leftBarStat">
+                <div className="leftBarStatValue">{stat.val}</div>
+                <div className="leftBarStatName">{stat.name}</div>
+              </div>
+            ))}
+          </div>
+          <hr className="leftBarSep"/>
+          <div className="leftBarTabs">
+            <div className={active === 0 && "active"} onClick={() => setActive(0)}>Account</div>
+            <div className={active === 1 && "active"} onClick={() => setActive(1)}>Appearance</div>
+            <div className={active === 2 && "active"} onClick={() => setActive(2)}>Advanced</div>
+          </div>
         </div>
-      </nav>
-      <div className="content">
-        {active === 0 && <Account/>}
-        {active === 1 && <Invites/>}
-        {active === 2 && <FileBrowser/>}
-        {active === 3 && <Appearance/>}
-        {active === 4 && <Advanced/>}
+        <div className="content">
+
+        </div>
       </div>
     </div>
   );
