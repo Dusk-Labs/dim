0c03cf00b1b36791121ecc3445ba4eadfde143c1
Eray Chumak <eray_chumak@protonmail.com>
2021-05-31 17:24:08 +0100

    Add SelectUnmatchedMedia component

--- ui/src/Components/CardList/UnmatchedCardList.jsx
+++ ui/src/Components/CardList/UnmatchedCardList.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useRef, useState } from "react";
+
 import Card from "./Card.jsx";
-import SelectUnmatchedMedia from "../../Modals/SelectUnmatchedMedia";
+import SelectUnmatchedMedia from "./SelectUnmatchedMedia/Index";
 
 import "./UnmatchedCardList.scss";
 
 function UnmatchedCardList(props) {
+  const unmatchedRef = useRef(null);
+  const [manuallyMatch, setManuallyMatch] = useState(false);
+
   const { fetched, error } = props.cards;
+
+  useEffect(() => {
+    unmatchedRef.current.style.height = manuallyMatch ? "100vh" : "auto";
+
+    if (manuallyMatch) {
+      unmatchedRef.current.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  }, [manuallyMatch]);
 
   let sections = [];
 
@@ -24,16 +39,18 @@ function UnmatchedCardList(props) {
     }
   }
 
+  const count = Object.values(props.cards.items).flat().length;
+
   return (
-    <div className="card_list unmatched">
-      <section>
-        <h1>Unmatched</h1>
-        <p className="sectionDesc">Could not find an accurate match for these files.</p>
-        <SelectUnmatchedMedia unmatched={props.cards.items}>
-          <button>Manually match</button>
-        </SelectUnmatchedMedia>
-        {sections}
-      </section>
+    <div className="unmatchedCardList" ref={unmatchedRef}>
+      <h1>Unmatched</h1>
+      <p className="sectionDesc">Could not find an accurate match for {count} {count === 0 ? "file" : "files"} in this library.</p>
+      {!manuallyMatch && (
+        <button className="manuallyMatch" onClick={() => setManuallyMatch(true)}>Manually match</button>
+      )}
+      {manuallyMatch && (
+        <SelectUnmatchedMedia items={props.cards.items} setManuallyMatch={setManuallyMatch}/>
+      )}
     </div>
   );
 }
