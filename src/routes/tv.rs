9a512ca98106933a13a8e4362c58005eb25c0a7b
Valerian G <valerian.garleanu@pm.me>
2021-06-06 21:51:11 +0000

    port tv routes to warp

--- src/routes/tv.rs
+++ src/routes/tv.rs
@@ -8,101 +8,299 @@ use database::media::Media;
 use database::season::{Season, UpdateSeason};
 use database::tv::TVShow;
 
-use rocket::http::Status;
-use rocket::State;
-use rocket_contrib::json::Json;
+use warp::http::status::StatusCode;
+use warp::reply;
+use warp::Filter;
 
-#[get("/<id>")]
+pub fn tv_router(
+    conn: DbConnection,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    filters::get_tv_by_id(conn.clone())
+        .or(filters::get_tv_seasons(conn.clone()))
+        .or(filters::get_season_by_num(conn.clone()))
+        .or(filters::patch_season_by_num(conn.clone()))
+        .or(filters::delete_season_by_num(conn.clone()))
+        .or(filters::get_episode_by_num(conn.clone()))
+        .or(filters::patch_episode_by_num(conn.clone()))
+        .or(filters::delete_episode_by_num(conn.clone()))
+        .recover(super::global_filters::handle_rejection)
+}
+
+mod filters {
+    use warp::reject;
+    use warp::Filter;
+    use warp::Rejection;
+
+    use super::super::global_filters::with_state;
+    use auth::Wrapper as Auth;
+    use database::episode::UpdateEpisode;
+    use database::season::UpdateSeason;
+    use database::DbConnection;
+
+    pub fn get_tv_by_id(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32)
+            .and(warp::get())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(|id: i32, auth: Auth, conn: DbConnection| async move {
+                super::get_tv_by_id(conn, id, auth)
+                    .await
+                    .map_err(|e| reject::custom(e))
+            })
+    }
+
+    pub fn get_tv_seasons(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32 / "season")
+            .and(warp::get())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(|id: i32, auth: Auth, conn: DbConnection| async move {
+                super::get_tv_seasons(conn, id, auth)
+                    .await
+                    .map_err(|e| reject::custom(e))
+            })
+    }
+
+    pub fn get_season_by_num(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32 / "season" / i32)
+            .and(warp::get())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(
+                |id: i32, season_num: i32, auth: Auth, conn: DbConnection| async move {
+                    super::get_season_by_num(conn, id, season_num, auth)
+                        .await
+                        .map_err(|e| reject::custom(e))
+                },
+            )
+    }
+
+    pub fn patch_season_by_num(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32 / "season" / i32)
+            .and(warp::patch())
+            .and(warp::body::json::<UpdateSeason>())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(|id: i32, season_num: i32, data: UpdateSeason, auth: Auth, conn: DbConnection| async move {
+                super::patch_season_by_num(conn, id, season_num, data, auth)
+                    .await
+                    .map_err(|e| reject::custom(e))
+            })
+    }
+
+    pub fn delete_season_by_num(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32 / "season" / i32)
+            .and(warp::delete())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(
+                |id: i32, season_num: i32, auth: Auth, conn: DbConnection| async move {
+                    super::delete_season_by_num(conn, id, season_num, auth)
+                        .await
+                        .map_err(|e| reject::custom(e))
+                },
+            )
+    }
+
+    pub fn get_episode_by_num(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32 / "season" / i32 / "episode" / i32)
+            .and(warp::get())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(
+                |id: i32, season_num: i32, ep_num: i32, auth: Auth, conn: DbConnection| async move {
+                    super::get_episode_by_id(conn, id, season_num, ep_num, auth)
+                        .await
+                        .map_err(|e| reject::custom(e))
+                },
+            )
+    }
+
+    pub fn patch_episode_by_num(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32 / "season" / i32 / "episode" / i32)
+            .and(warp::patch())
+            .and(warp::body::json::<UpdateEpisode>())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(
+                |id: i32,
+                 season_num: i32,
+                 ep_num: i32,
+                 data: UpdateEpisode,
+                 auth: Auth,
+                 conn: DbConnection| async move {
+                    super::patch_episode_by_id(conn, id, season_num, ep_num, data, auth)
+                        .await
+                        .map_err(|e| reject::custom(e))
+                },
+            )
+    }
+
+    pub fn delete_episode_by_num(
+        conn: DbConnection,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+        warp::path!("api" / "v1" / "tv" / i32 / "season" / i32 / "episode" / i32)
+            .and(warp::delete())
+            .and(auth::with_auth())
+            .and(with_state::<DbConnection>(conn))
+            .and_then(
+                |id: i32, season_num: i32, ep_num: i32, auth: Auth, conn: DbConnection| async move {
+                    super::delete_episode_by_id(conn, id, season_num, ep_num, auth)
+                        .await
+                        .map_err(|e| reject::custom(e))
+                },
+            )
+    }
+}
+
+/// Method mapped to `GET /api/v1/tv/<id>` returns info about a a TV Show mapped to the id passed
+/// in.
+///
+/// # Arguments
+/// * `id` - id of the tv show we want info about
 pub async fn get_tv_by_id(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     _user: Auth,
-) -> Result<Json<Media>, errors::DimError> {
-    Ok(Json(TVShow::get(&conn, id).await?))
+) -> Result<impl warp::Reply, errors::DimError> {
+    Ok(reply::json(&TVShow::get(&conn, id).await?))
 }
 
-#[get("/<id>/season")]
+/// Method mapped to `GET /api/v1/tv/<id>/season` returns all seasons for TV Show mapped to the id
+/// passed in.
+///
+/// # Arguments
+/// * `id` - id of the tv show we want info about
 pub async fn get_tv_seasons(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     _user: Auth,
-) -> Result<Json<Vec<Season>>, errors::DimError> {
-    Ok(Json(Season::get_all(&conn, id).await?))
+) -> Result<impl warp::Reply, errors::DimError> {
+    Ok(reply::json(&Season::get_all(&conn, id).await?))
 }
 
-#[get("/<id>/season/<season_num>")]
+/// Method mapped to `GET /api/v1/tv/<id>/season/<season_num>` returns info about the season
+/// <season_num> for tv show by <id>
+///
+/// # Arguments
+/// * `id` - id of the tv show we want info about
+/// * `season_num` - the season we want info about
 pub async fn get_season_by_num(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     season_num: i32,
     _user: Auth,
-) -> Result<Json<Season>, errors::DimError> {
-    Ok(Json(Season::get(&conn, id, season_num).await?))
+) -> Result<impl warp::Reply, errors::DimError> {
+    Ok(reply::json(&Season::get(&conn, id, season_num).await?))
 }
 
-#[patch(
-    "/<id>/season/<season_num>",
-    format = "application/json",
-    data = "<data>"
-)]
+/// Method mapped to `PATCH /api/v1/tv/<id>/season/<season_num>` allows you to patch in info about
+/// the season <season_num>.
+///
+/// # Route Arguments
+/// * `id` - the id of the tv show.
+/// * `season_num` - the season we want to edit.
+///
+/// # Data
+/// This route additionally requires you to pass in a json object by the format of
+/// `database::season::UpdateSeason`.
 pub async fn patch_season_by_num(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     season_num: i32,
-    data: Json<UpdateSeason>,
+    data: UpdateSeason,
     _user: Auth,
-) -> Result<Status, errors::DimError> {
-    data.into_inner().update(&conn, id, season_num).await?;
-    Ok(Status::NoContent)
+) -> Result<impl warp::Reply, errors::DimError> {
+    data.update(&conn, id, season_num).await?;
+    Ok(StatusCode::NO_CONTENT)
 }
 
-#[delete("/<id>/season/<season_num>")]
+/// Method mapped to `DELETE /api/v1/tv/<id>/season/<season_num>` allows you to delete a season for
+/// a particular tv show.
+///
+/// # Arguments
+/// * `id` - id of the tv show.
+/// * `season_num` - the season we want to remove
 pub async fn delete_season_by_num(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     season_num: i32,
     _user: Auth,
-) -> Result<Status, errors::DimError> {
+) -> Result<impl warp::Reply, errors::DimError> {
     Season::delete(&conn, id, season_num).await?;
-    Ok(Status::Ok)
+    Ok(StatusCode::OK)
 }
 
-#[get("/<id>/season/<season_num>/episode/<ep_num>")]
+/// Method mapped to `GET /api/v1/tv/<id>/season/<season_num>/episode/<ep_num>` returns information
+/// about a episode for a season.
+///
+/// # Arguments
+/// * `id` - id of the tv show.
+/// * `season_num` - the season the episode belongs to
+/// * `ep_num` - the episode we want to get info for
 pub async fn get_episode_by_id(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     season_num: i32,
     ep_num: i32,
     _user: Auth,
-) -> Result<Json<Episode>, errors::DimError> {
-    Ok(Json(Episode::get(&conn, id, season_num, ep_num).await?))
+) -> Result<impl warp::Reply, errors::DimError> {
+    Ok(reply::json(
+        &Episode::get(&conn, id, season_num, ep_num).await?,
+    ))
 }
 
-#[patch(
-    "/<id>/season/<season_num>/episode/<ep_num>",
-    format = "application/json",
-    data = "<episode>"
-)]
+/// Method mapped to `PATCH /api/v1/tv/<id>/season/<season_num>/episode/<ep_num>` lets you patch
+/// information about a episode.
+///
+/// # Arguments
+/// * `id` - id of a tv show.
+/// * `season_num` - the season the episode belongs to.
+/// * `ep_num` - the episode we want to patch.
+///
+/// # Data
+/// This route additionally requires you to pass in a json object by the format of
+/// `database::episode::UpdateEpisode`.
 pub async fn patch_episode_by_id(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     season_num: i32,
     ep_num: i32,
-    episode: Json<UpdateEpisode>,
+    episode: UpdateEpisode,
     _user: Auth,
-) -> Result<Status, errors::DimError> {
+) -> Result<impl warp::Reply, errors::DimError> {
     episode.update(&conn, id, season_num, ep_num).await?;
-    Ok(Status::NoContent)
+    Ok(StatusCode::NO_CONTENT)
 }
 
-#[delete("/<id>/season/<season_num>/episode/<ep_num>")]
+/// Method mapped to `DELETE /api/v1/tv/<id>/season/<season_num>/episode/<ep_num>` allows you to
+/// delete a episode belonging to some season.
+///
+/// # Arguments
+/// * `id` - id of the tv show the episode belongs to
+/// * `season_num` - season the episode belongs to
+/// * `ep_num` - the episode we want to delete
 pub async fn delete_episode_by_id(
-    conn: State<'_, DbConnection>,
+    conn: DbConnection,
     id: i32,
     season_num: i32,
     ep_num: i32,
     _user: Auth,
-) -> Result<Status, errors::DimError> {
+) -> Result<impl warp::Reply, errors::DimError> {
     Episode::delete(&conn, id, season_num, ep_num).await?;
-    Ok(Status::Ok)
+    Ok(StatusCode::OK)
 }
