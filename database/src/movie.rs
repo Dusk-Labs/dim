f6f1246f6c2024b7dda3b0521d048d386bc3ce6b
Valerian G <valerian.garleanu@pm.me>
2021-06-22 17:15:59 +0000

    refactored movie.rs

--- database/src/movie.rs
+++ database/src/movie.rs
@@ -1,52 +1,22 @@
 use crate::media::MediaTrait;
-use crate::schema::movie;
-use crate::streamable_media::{StreamableMedia, StreamableTrait};
 use crate::DatabaseError;
 
-use async_trait::async_trait;
-use tokio_diesel::*;
-
-/// Struct represents a Movie entry in the database
-#[derive(Clone, Identifiable, Queryable, Associations)]
-#[belongs_to(StreamableMedia, foreign_key = "id")]
-#[table_name = "movie"]
-pub struct Movie {
-    /// id of the movie that is also a foreign key to a media entry.
-    id: i32,
-}
-
 /// Struct reperesents a insertable movie entry
-#[derive(Clone, Insertable)]
-#[table_name = "movie"]
-pub struct InsertableMovie {
-    /// id of a media entry that should be used as a foreign key.
-    id: i32,
-}
+#[derive(Clone, Copy)]
+pub struct InsertableMovie;
 
-#[async_trait]
-impl StreamableTrait for InsertableMovie {
-    /// Method returns a new instance of InsertableMovie, this is a trait method because it is used
-    /// to indicate that this specific media entry can be streamed.
-    ///
-    /// # Arguments
-    /// * `conn` - diesel connection reference to postgres
-    /// * `id` - id of the movie we are inserting, this id should already exist in the media table.
-    fn new(id: i32) -> Self {
-        Self { id }
-    }
-
+impl InsertableMovie {
     /// Method inserts the object into the table movie returning its id which should be equivalent
     /// to the field id.
     ///
     /// # Arguments
     /// * `conn` - diesel connection reference to postgres
-    async fn insert(&self, conn: &crate::DbConnection) -> Result<i32, DatabaseError> {
-        diesel::insert_into(movie::table)
-            .values(self.clone())
-            .execute_async(conn)
-            .await?;
-
-        Ok(self.id)
+    /// * `id` - id of the media that should be a movie
+    pub async fn insert(conn: &crate::DbConnection, id: i64) -> Result<i64, DatabaseError> {
+        Ok(sqlx::query!("INSERT INTO movie (id) VALUES ($1)", id)
+            .execute(conn)
+            .await?
+            .last_insert_rowid())
     }
 }
 
