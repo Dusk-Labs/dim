7bb05c584b2757f8f068ca027fb692fbcb13f4f4
Valerian G <valerian.garleanu@pm.me>
2021-06-22 17:15:01 +0000

    further cleanup of src/episode.rs

--- database/src/episode.rs
+++ database/src/episode.rs
@@ -4,13 +4,9 @@ use crate::media::InsertableMedia;
 use crate::media::Media;
 use crate::media::UpdateMedia;
 use crate::season::Season;
-use crate::tv::TVShow;
 use crate::DatabaseError;
 
 use serde::{Deserialize, Serialize};
-
-use futures::stream;
-use futures::StreamExt;
 
 /// Episode struct encapsulates a media entry representing a episode
 #[derive(Clone, Serialize, Debug)]
@@ -39,31 +35,6 @@ pub struct EpisodeWrapper {
     pub episode_: i64,
 }
 
-#[derive(Debug)]
-pub struct InsertableEpisode {
-    pub media: InsertableMedia,
-    pub seasonid: i64,
-    pub episode: i64,
-}
-
-pub struct InsertableEpisodeWrapper {
-    pub episode_: i64,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct UpdateEpisode {
-    pub seasonid: Option<i64>,
-    pub episode: Option<i64>,
-
-    #[serde(flatten)]
-    pub media: UpdateMedia,
-}
-
-pub struct UpdateEpisodeWrapper {
-    pub seasonid: Option<i64>,
-    pub episode_: Option<i64>,
-}
-
 impl Episode {
     pub async fn get_first_for_season(
         conn: &crate::DbConnection,
@@ -90,27 +61,25 @@ impl Episode {
     ///
     pub async fn get_all_of_tv(
         conn: &crate::DbConnection,
-        media: &Media,
-    ) -> Result<Vec<Episode>, DatabaseError> {
-        let _ = sqlx::query!("SELECT * FROM tv_show WHERE id = ?", media.id)
-            .fetch_one(conn)
-            .await?;
-
+        tv_show_id: i64,
+    ) -> Result<Vec<Self>, DatabaseError> {
         let mut episodes = vec![];
 
-        for season in Season::get_all(conn, media.id).await? {
-            let wrappers = sqlx::query_as!(
-                EpisodeWrapper,
-                r#"SELECT id , episode_ , seasonid   FROM episode WHERE seasonid = ?"#,
-                season.id
-            )
-            .fetch_all(conn)
-            .await?;
+        let wrappers = sqlx::query_as!(
+            EpisodeWrapper,
+            "SELECT episode.* FROM episode
+                INNER JOIN season ON season.id = episode.seasonid
+                INNER JOIN tv_show ON tv_show.id = season.tvshowid
+                WHERE tv_show.id = ?
+                ORDER BY season.season_number, episode.episode_",
+                tv_show_id
+        )
+        .fetch_all(conn)
+        .await?;
 
-            for wrapper in wrappers {
-                if let Ok(episode) = Media::get(conn, wrapper.id as i64).await {
-                    episodes.push(wrapper.into_episode(episode))
-                }
+        for wrapper in wrappers {
+            if let Ok(episode) = Media::get(conn, wrapper.id as i64).await {
+                episodes.push(wrapper.into_episode(episode))
             }
         }
 
@@ -125,12 +94,12 @@ impl Episode {
     ///
     pub async fn get_all_of_season(
         conn: &crate::DbConnection,
-        media: &Season,
+        season_id: i64,
     ) -> Result<Vec<Episode>, DatabaseError> {
         let wrappers = sqlx::query_as!(
             EpisodeWrapper,
-            r#"SELECT id , episode_ , seasonid   FROM episode WHERE seasonid = ?"#,
-            media.id
+            r#"SELECT id , episode_ , seasonid FROM episode WHERE seasonid = ?"#,
+            season_id 
         )
         .fetch_all(conn)
         .await?;
@@ -160,16 +129,15 @@ impl Episode {
         season_num: i64,
         ep_num: i64,
     ) -> Result<Episode, DatabaseError> {
-        let _ = sqlx::query!("SELECT * FROM tv_show WHERE id = ?", tv_id)
-            .fetch_one(conn)
-            .await?;
-
-        let season = Season::get(conn, tv_id, season_num).await?;
-
         let wrapper = sqlx::query_as!(
             EpisodeWrapper,
-            r#"SELECT id , seasonid , episode_  FROM episode WHERE seasonid = ? AND episode_ = ?"#,
-            season.id,
+            r#"SELECT episode.*  FROM episode
+            INNER JOIN season ON season.id = episode.seasonid
+            WHERE season.tvshowid = ?
+            AND season.season_number = ?
+            AND episode.episode_ = ?"#,
+            tv_id,
+            season_num,
             ep_num
         )
         .fetch_one(conn)
@@ -187,7 +155,6 @@ impl Episode {
     /// * `id` - The id of a tv show we target
     /// * `season_num` - The season we are targetting
     /// * `ep_num` - Episode we are targetting
-    ///
     pub async fn delete(
         conn: &crate::DbConnection,
         tv_id: i64,
@@ -205,51 +172,43 @@ impl Episode {
     }
 }
 
+#[derive(Debug)]
+pub struct InsertableEpisode {
+    pub media: InsertableMedia,
+    pub seasonid: i64,
+    pub episode: i64,
+}
+
 impl InsertableEpisode {
     /// Method inserts a new episode into the database
     ///
     /// # Arguments
     /// * `conn` - diesel connection reference to postgres
     /// * `show_id` - the id of the tv show we are trying to asociate this episode with
-    ///
     pub async fn insert(
         &self,
         conn: &crate::DbConnection,
-        tv_id: i64,
         media_id: i64,
     ) -> Result<i64, DatabaseError> {
-        let _ = sqlx::query!("SELECT * FROM tv_show WHERE id = ?", tv_id)
-            .fetch_one(conn)
-            .await?;
-        let _ = Season::get(conn, tv_id, self.seasonid).await?;
+        let tx = conn.begin().await?;
 
-        if let Some(res) = sqlx::query!(
-            "SELECT id FROM episode WHERE seasonid = ? AND episode_ = ?",
+        if let Some(r) = sqlx::query!(
+            "SELECT id FROM episode WHERE episode.seasonid = ? AND episode.episode_ = ?",
             self.seasonid,
             self.episode
-        )
-        .fetch_optional(conn)
-        .await?
-        {
-            return Ok(res.id);
+        ).fetch_optional(conn).await? {
+            return Ok(r.id);
         }
+        
+        let result = sqlx::query!(
+            "INSERT INTO episode (id, episode_, seasonid)
+            VALUES ($1, $2, $3)",
+            media_id, self.episode, self.seasonid
+        ).execute(conn).await?.last_insert_rowid();
 
-        let res = sqlx::query!(
-            "INSERT INTO episode (id, seasonid, episode_) VALUES($1, $2, $3)",
-            media_id,
-            self.seasonid,
-            self.episode
-        )
-        .execute(conn)
-        .await?;
+        tx.commit().await?;
 
-        Ok(res.last_insert_rowid())
-    }
-
-    fn into(&self) -> InsertableEpisodeWrapper {
-        InsertableEpisodeWrapper {
-            episode_: self.episode,
-        }
+        Ok(result)
     }
 }
 
@@ -264,40 +223,37 @@ impl EpisodeWrapper {
     }
 }
 
+#[derive(Deserialize, Debug)]
+pub struct UpdateEpisode {
+    pub seasonid: Option<i64>,
+    pub episode: Option<i64>,
+
+    #[serde(flatten)]
+    pub media: UpdateMedia,
+}
+
 impl UpdateEpisode {
     /// Method updates the rows of a episode.
     ///
     /// # Arguments
     /// * `conn` - diesel connection reference to postgres
-    /// * `id` - id of the tv show we'd like to discriminate against
-    /// * `season_num` - the season number we want to discriminate against
-    /// * `ep_num` - episode number of the entry that we want to update info of
-    ///
+    /// * `id` - id of the episode we wish to update. 
     pub async fn update(
         &self,
         conn: &crate::DbConnection,
         id: i64,
-        season_num: i64,
-        ep_num: i64,
-    ) -> Result<(), DatabaseError> {
-        let episode = Episode::get(conn, id, season_num, ep_num).await?;
+    ) -> Result<usize, DatabaseError> {
+        let tx = conn.begin().await?;
 
-        self.media.update(conn, episode.id).await?;
+        self.media.update(conn, id).await?;
 
-        todo!();
+        crate::opt_update!(conn, tx,
+            "UPDATE episode SET seasonid = ? WHERE id = ?" => (self.seasonid, id),
+            "UPDATE episode SET episode_ = ? WHERE id = ?" => (self.episode, id)
+        );
 
-        // let _ = diesel::update(&*episode)
-        //     .set(self.into())
-        //     .execute_async(conn)
-        //     .await;
+        tx.commit().await?;
 
-        Ok(())
-    }
-
-    fn into(&self) -> UpdateEpisodeWrapper {
-        UpdateEpisodeWrapper {
-            seasonid: self.seasonid,
-            episode_: self.episode,
-        }
+        Ok(1)
     }
 }
