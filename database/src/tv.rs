e1ee9539bbd48d591fa266480a51f922204b2580
Valerian G <valerian.garleanu@pm.me>
2021-06-23 12:50:02 +0000

    removed unneeded code

--- database/src/tv.rs
+++ database/src/tv.rs
@@ -1,38 +1,13 @@
-use std::convert::TryInto;
-
 use crate::media::*;
 use crate::DatabaseError;
 
-use async_trait::async_trait;
-use cfg_if::cfg_if;
 use serde::{Deserialize, Serialize};
-
-/// Trait used as a marker to mark media entries that cannot be streamed, as in not being directly
-/// linked to a file on the filesystem. For example tv shows.
-#[async_trait]
-pub trait StaticTrait {
-    /// Required method returning a instance of a object we'd like to mark as static.
-    ///
-    /// # Arguments
-    /// * `id` - id of a media object.
-    fn new(id: i64) -> Self;
-    /// Required method that inserts Self into the database returning its id.
-    async fn insert(&self, conn: &crate::DbConnection) -> Result<i64, DatabaseError>;
-}
 
 /// Struct represents a tv show entry in the database.
 /// This is mostly used as a marker to mark shows from movies, and episodes.
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct TVShow {
     /// id of a media object we marked as a tv show.
-    pub id: i64,
-}
-
-/// Struct represents a insertable tv show entry in the database.
-/// This is mostly used as a marker to mark shows from movies, and episodes.
-#[derive(Deserialize, PartialEq, Debug, Clone)]
-pub struct InsertableTVShow {
-    /// id of a media object we'd like to mark as a tv show.
     pub id: i64,
 }
 
@@ -48,7 +23,8 @@ impl TVShow {
             r#"SELECT 
                 media.id, media.library_id, media.name, media.description,
                 media.rating, media.year, media.added, media.poster_path, 
-                media.backdrop_path, media.media_type as "media_type: _" FROM media INNER JOIN tv_show ON media.id = tv_show.id"#
+                media.backdrop_path, media.media_type as "media_type: _" 
+                FROM media INNER JOIN tv_show ON media.id = tv_show.id"#
         )
         .fetch_all(conn)
         .await?
@@ -63,7 +39,8 @@ impl TVShow {
             r#"SELECT 
                 media.id, media.library_id, media.name, media.description,
                 media.rating, media.year, media.added, media.poster_path, 
-                media.backdrop_path, media.media_type as "media_type: _" FROM media WHERE media.id = ?"#,
+                media.backdrop_path, media.media_type as "media_type: _"
+                FROM media WHERE media.id = ?"#,
             self.id
         )
         .fetch_one(conn)
@@ -71,33 +48,16 @@ impl TVShow {
 
         Ok(media)
     }
-}
-
-#[async_trait]
-impl StaticTrait for InsertableTVShow {
-    fn new(id: i64) -> Self {
-        Self { id }
-    }
 
     /// Method inserts a new tv show in the database.
     ///
     /// # Arguments
     /// * `conn` - diesel connection reference to postgres
-    async fn insert(&self, conn: &crate::DbConnection) -> Result<i64, DatabaseError> {
-        let res = sqlx::query!("INSERT INTO tv_show (id) VALUES ($1)", self.id)
+    /// * `id` - id of a media object that should be a tv show.
+    pub async fn insert(conn: &crate::DbConnection, id: i64) -> Result<i64, DatabaseError> {
+        Ok(sqlx::query!("INSERT INTO tv_show (id) VALUES ($1)", id)
             .execute(conn)
-            .await?;
-
-        cfg_if! {
-            if #[cfg(feature = "postgres")] {
-                Ok(query.returning(tv_show::id)
-                    .on_conflict_do_nothing()
-                    .get_result_async(conn).await?)
-            } else {
-                Ok(res.last_insert_rowid().try_into().expect("can't convert rowid from i64 to i64."))
-            }
-        }
+            .await?
+            .last_insert_rowid())
     }
 }
-
-impl MediaTrait for InsertableTVShow {}
