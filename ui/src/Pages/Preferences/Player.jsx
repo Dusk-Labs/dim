9a7a93a73a5428d6625a259ad459ec8ccbe07888
Eray Chumak <eray_chumak@protonmail.com>
2021-07-31 21:59:02 +0100

    Add preferences video player component

---
+++ ui/src/Pages/Preferences/Player.jsx
@@ -0,0 +1,29 @@
+import { useCallback } from "react";
+import Toggle from "../../Components/Toggle";
+
+import "./Player.scss";
+
+function VideoPlayer() {
+  const toggleShowSubsDefault = useCallback((state) => {
+    console.log(state);
+  }, []);
+
+  return (
+    <div className="preferencesVideoPlayer">
+      <section>
+        <h2>Subtitles</h2>
+        <Toggle
+          disabled
+          name="Show subtitles by default"
+          onToggle={toggleShowSubsDefault}
+        />
+      </section>
+      <section>
+        <h2>Audio</h2>
+        {/* <p className="desc">Manage your default preferred subtitle language</p> */}
+      </section>
+    </div>
+  );
+}
+
+export default VideoPlayer;
