9c8547594a8a528e2f5ebf4f0c61d1d64eecf58a
Eray Chumak <eray_chumak@protonmail.com>
2021-07-31 21:58:23 +0100

    Add preferences advanced general section component

---
+++ ui/src/Pages/Preferences/Advanced/General.jsx
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateGlobalSettings } from "../../../actions/settings";
+import Button from "../../../Components/Misc/Button";
+import Toggle from "../../../Components/Toggle";
+import Field from "../../Auth/Field";
+
+function General() {
+  const dispatch = useDispatch();
+  const settings = useSelector(store => store.settings);
+
+  const [port, setPort] = useState("");
+  const [portErr, setPortErr] = useState("");
+  const [verbose, setVerbose] = useState(false);
+
+  useEffect(() => {
+    const { data } = settings.globalSettings;
+
+    setPort(data.port);
+    setVerbose(data.verbose);
+  }, [settings]);
+
+  const toggleVerbose = useCallback((state) => {
+    dispatch(updateGlobalSettings({
+      verbose: state
+    }));
+  }, [dispatch]);
+
+  const updatePort = useCallback(() => {
+  }, []);
+
+  const undoUpdatePort = useCallback(() => {
+    setPort(settings.globalSettings.data.port);
+    setPortErr("");
+  }, [settings.globalSettings.data.port]);
+
+  return (
+    <section>
+      <h2>General</h2>
+      <div className="toggles">
+        <Toggle
+          name="Verbose logging"
+          onToggle={toggleVerbose}
+          state={verbose}
+        />
+        <Field
+          name="Port"
+          data={[port, setPort]}
+          error={[portErr, setPortErr]}
+        />
+      </div>
+      {port !== settings.globalSettings.data.port && (
+        <div className="options">
+          <Button onClick={updatePort}>
+            Update
+          </Button>
+          <Button type="secondary" onClick={undoUpdatePort}>
+            Cancel
+          </Button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default General;
