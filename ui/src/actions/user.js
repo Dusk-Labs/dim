cc65c1be54919343b53aede9e706a7de4aebc93c
Eray Chumak <eray_chumak@protonmail.com>
2021-08-01 17:17:43 +0100

    Add change username action and reducer

--- ui/src/actions/user.js
+++ ui/src/actions/user.js
@@ -1,7 +1,11 @@
 import {
   FETCH_USER_START,
   FETCH_USER_OK,
-  FETCH_USER_ERR
+  FETCH_USER_ERR,
+  NOTIFICATIONS_ADD,
+  CHANGE_USERNAME_START,
+  CHANGE_USERNAME_ERR,
+  CHANGE_USERNAME_OK
 } from "./types.js";
 
 export const fetchUser = () => async (dispatch, getState) => {
@@ -34,6 +38,52 @@ export const fetchUser = () => async (dispatch, getState) => {
   } catch(err) {
     dispatch({
       type: FETCH_USER_ERR,
+      payload: err
+    });
+  }
+};
+
+export const changeUsername = (newUsername) => async (dispatch, getState) => {
+  dispatch({ type: CHANGE_USERNAME_START });
+
+  const token = getState().auth.token;
+
+  const config = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "authorization": token
+    },
+    body: JSON.stringify({
+      new_username: newUsername
+    })
+  };
+
+  try {
+    const res = await fetch("/api/v1/auth/username", config);
+
+    console.log(res);
+
+    if (res.status !== 200) {
+      dispatch({
+        type: CHANGE_USERNAME_ERR,
+        payload: res.statusText
+      });
+
+      return;
+    }
+
+    dispatch({ type: CHANGE_USERNAME_OK });
+
+    dispatch({
+      type: NOTIFICATIONS_ADD,
+      payload: {
+        msg: "Your username has now been updated."
+      }
+    });
+  } catch(err) {
+    dispatch({
+      type: CHANGE_USERNAME_ERR,
       payload: err
     });
   }
--- ui/src/reducers/user.js
+++ ui/src/reducers/user.js
@@ -1,20 +1,31 @@
 import {
   FETCH_USER_START,
   FETCH_USER_OK,
-  FETCH_USER_ERR
+  FETCH_USER_ERR,
+  CHANGE_USERNAME_ERR,
+  CHANGE_USERNAME_START,
+  CHANGE_USERNAME_OK
 } from "../actions/types.js";
+
+const changeUsername = {
+  changing: false,
+  changed: true,
+  error: null
+};
 
 const initialState = {
   info: {},
   fetching: false,
   fetched: false,
-  error: null
+  error: null,
+  changeUsername
 };
 
 export default function userReducer(state = initialState, action) {
   switch(action.type) {
     case FETCH_USER_START:
       return {
+        ...state,
         info: {},
         fetching: true,
         fetched: false,
@@ -33,6 +44,33 @@ export default function userReducer(state = initialState, action) {
         fetching: false,
         fetched: true,
         error: action.payload
+      };
+    case CHANGE_USERNAME_START:
+      return {
+        ...state,
+        changeUsername: {
+          changing: true,
+          changed: false,
+          error: null
+        }
+      };
+    case CHANGE_USERNAME_OK:
+      return {
+        ...state,
+        changeUsername: {
+          ...state.changeUsername,
+          changing: false,
+          changed: true
+        }
+      };
+    case CHANGE_USERNAME_ERR:
+      return {
+        ...state,
+        changeUsername: {
+          ...state.changeUsername,
+          changing: false,
+          err: action.payload
+        }
       };
     default:
       return state;
