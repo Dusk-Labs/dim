8a166cb9f93f9c2e2eaf3755760a4e07ebb611b4
Eray Chumak <eray_chumak@protonmail.com>
2021-08-01 15:54:53 +0100

    Dispatch user preference

--- ui/src/Pages/Preferences/Appearance/Sidebar.jsx
+++ ui/src/Pages/Preferences/Appearance/Sidebar.jsx
@@ -1,10 +1,31 @@
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { updateUserSettings } from "../../../actions/settings";
 import Toggle from "../../../Components/Toggle";
 
 function Sidebar() {
+  const dispatch = useDispatch();
+  const settings = useSelector(store => store.settings);
+
+  const [compactSidebar, setCompactSidebar] = useState(false);
+
+  useEffect(() => {
+    setCompactSidebar(settings.userSettings.data.is_sidebar_compact);
+  }, [settings.userSettings.data.is_sidebar_compact]);
+
+  const handleToggle = useCallback((state) => {
+    dispatch(updateUserSettings({
+      is_sidebar_compact: state
+    }));
+  }, [dispatch]);
+
   return (
     <section>
       <h2>Sidebar</h2>
       <Toggle
+        onToggle={handleToggle}
+        state={compactSidebar}
         name="Keep the sidebar always in compact mode"
       />
     </section>
