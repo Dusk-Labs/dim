cfe9892d533cd43cdf6cdbbd68ded54a6ce4bd86
Eray Chumak <eray_chumak@protonmail.com>
2021-08-03 21:43:56 +0100

    Add Blind theme

---
+++ ui/src/Themes/Blind.js
@@ -0,0 +1,23 @@
+const Blind = {
+  borderRadius: "10px",
+  accentColor: "#f7931e",
+  accentTextColor: "#1a1a1a",
+  criticalColor: "#e91619",
+  criticalTextColor: "#fff",
+  primaryTextColor: "#000",
+  secondaryTextColor: "#222",
+  tertiaryTextColor: "#444",
+  primaryColor: "#fff",
+  secondaryColor: "#eee",
+  tertiaryColor: "#ddd",
+  quaternaryColor: "#ccc",
+  quinaryColor: "#bbb",
+  senaryColor: "#aaa",
+  septenaryColor: "#999",
+  bannerPrimaryColor: "#3a3a3a",
+  bannerPrimaryTextColor: "#fff",
+  bannerSecondaryTextColor: "#ddd",
+  bannerTertiaryTextColor: "#ccc"
+};
+
+export default Blind;
