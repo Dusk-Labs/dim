ea27506131b16b2aee87d0ba81dcb3645b82e4fa
Eray Chumak <eray_chumak@protonmail.com>
2021-08-03 22:01:02 +0100

    Save theme preference

--- ui/src/Pages/Preferences/Appearance/Themes.jsx
+++ ui/src/Pages/Preferences/Appearance/Themes.jsx
@@ -1,31 +1,43 @@
 import DefaultTheme from "../../../assets/themes/Default";
 import Blind from "../../../assets/themes/Blind";
-import LightsOff from "../../../assets/themes/LightsOff";
+// import LightsOff from "../../../assets/themes/LightsOff";
+import { useDispatch, useSelector } from "react-redux";
+import { useCallback } from "react";
+import { updateUserSettings } from "../../../actions/settings";
 
 function Themes() {
+  const dispatch = useDispatch();
+  const userSettings = useSelector(store => store.settings.userSettings);
+
+  const setTheme = useCallback((theme) => {
+    dispatch(updateUserSettings({
+      theme
+    }));
+  }, [dispatch]);
+
   return (
     <section>
       <h2>Themes</h2>
       <p className="desc">Change the look and feel of Dim across all your devices.</p>
       <div className="themes">
-        <div className="themeContainer">
-          <div className="theme active">
+        <div className="themeContainer" onClick={() => setTheme("Dark")}>
+          <div className={`theme ${userSettings.data.theme === "Dark" ? "active" : ""}`}>
             <DefaultTheme/>
           </div>
-          <p>Default</p>
+          <p>Dark</p>
         </div>
-        <div className="themeContainer">
-          <div className="theme">
+        <div className="themeContainer" onClick={() => setTheme("Blind")}>
+          <div className={`theme ${userSettings.data.theme === "Blind" ? "active" : ""}`}>
             <Blind/>
           </div>
           <p>Blind</p>
         </div>
-        <div className="themeContainer">
+        {/* <div className="themeContainer">
           <div className="theme">
             <LightsOff/>
           </div>
           <p>Lights Off</p>
-        </div>
+        </div> */}
       </div>
     </section>
   );
