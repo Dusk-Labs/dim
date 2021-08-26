f8429a3d12eff84b2ef7e2ad6f195a596fa75ae5
Valerian G <valerian.garleanu@pm.me>
2021-08-26 22:40:04 +0100

    fix dockerfile

--- dim/src/build.rs
+++ dim/src/build.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
     if Path::new("../ui/build").exists() {
@@ -6,6 +7,14 @@ fn main() {
     } else {
         println!("cargo:warning=`ui/build` does not exist.");
         println!("cargo:warning=If you wish to embed the webui, run `yarn build` in `ui`.");
+    }
+
+    if !Path::new("./dim_dev.db").exists() {
+        println!("cargo:warning=Generating dim_dev.db from latest migrations.");
+        Command::new("sqlite3")
+            .args(&["-init", "database/migrations/*.sql", "dim_dev.db"])
+            .output()
+            .expect("Failed to generate dim_dev.db from latest migrations.");
     }
 
     println!("cargo:rerun-if-changed=ui/build");
