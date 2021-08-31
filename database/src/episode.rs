9033a98ac725890d8aeddfe690b34dd0eb8d8192
Valerian G <valerian.garleanu@pm.me>
2021-08-31 17:57:29 +0100

    add method to query episode and season of a media

--- database/src/episode.rs
+++ database/src/episode.rs
@@ -186,6 +186,25 @@ impl Episode {
         Ok(wrapper.into_episode(ep))
     }
 
+    pub async fn get_season_episode_by_id(
+        conn: &crate::DbConnection,
+        episode_id: i64
+    ) -> Result<(i64, i64), DatabaseError> {
+        struct Record {
+            episode: i64,
+            season: i64,
+        }
+
+        let result = sqlx::query_as!(Record,
+            "SELECT episode_ as episode, season.season_number as season FROM episode
+            INNER JOIN season ON season.id = episode.seasonid
+            WHERE episode.id = ?",
+            episode_id
+        ).fetch_one(conn).await?;
+
+        Ok((result.season, result.episode))
+    }
+
     /// Method deletes a episode based on the tv show id, season number, and episode number
     ///
     /// # Arguments
