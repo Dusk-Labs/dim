7d6706c1428ba27155ad8831f15076e4170ecdb9
Valerian G <valerian@dusklabs.io>
2021-09-26 16:02:33 +0100

    remove junk files

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ $DIM_ENABLE_SSL == "1" ]];
-then
-    cd /opt/dim && ./dim --priv-key /opt/dim/config/priv_key.pem --ssl-cert /opt/dim/config/cert.pem --cache-dir /opt/dim/transcoding;
-else
-    cd /opt/dim && ./dim --cache-dir /opt/dim/transcoding;
-fi
