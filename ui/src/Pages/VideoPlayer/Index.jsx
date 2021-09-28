8955f2224bf2630d1465cc67a8b9d8cb92c6f41d
Taurai M <tau.mutaz@gmail.com>
2021-09-28 19:33:44 +0100

    bump nightfall and fix routes for vtt subtitles

--- ui/src/Pages/VideoPlayer/Index.jsx
+++ ui/src/Pages/VideoPlayer/Index.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef } from "react";
 import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { MediaPlayer } from "dashjs";
+import dashjs, { MediaPlayer } from "dashjs";
 
 import { setTracks, setGID, setManifestState, updateVideo, incIdleCount, clearVideoData } from "../../actions/video";
 import { VideoPlayerContext } from "./Context";
@@ -97,6 +97,8 @@ function VideoPlayer() {
     const includes = `${videoTracks.list.map(track => track.id).join(",")},${audioTracks.list.map(track => track.id).join(",")}`;
     const url = `/api/v1/stream/${video.gid}/manifest.mpd?start_num=0&should_kill=false&includes=${includes}`;
     const mediaPlayer = MediaPlayer().create();
+
+    console.log(dashjs);
 
     // even with these settings, high bitrate movies fail.
     // The only solution is to have a constant bitrate and cosistent segments.
