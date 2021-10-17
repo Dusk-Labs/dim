2d1740c10a4c242956c76f05123cb40a2ef7720b
Valerian G <valerian@dusklabs.io>
2021-10-17 20:02:50 +0100

    Auto-logout if /whoami returns Unauthorized

--- ui/src/Components/Sidebar/Index.jsx
+++ ui/src/Components/Sidebar/Index.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef } from "react";
-import { useDispatch} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { fetchUser } from "../../actions/user.js";
 import { fetchUserSettings } from "../../actions/settings.js";
 import { fetchLibraries } from "../../actions/library.js";
+import { logout } from "../../actions/auth.js";
 
 import Profile from "./Profile/Index";
 import Search from "./Search";
@@ -14,6 +15,7 @@ import General from "./General";
 import "./Index.scss";
 
 function Sidebar() {
+  const user = useSelector(store => store.user);
   const dispatch = useDispatch();
   const divContainer = useRef(null);
 
@@ -22,6 +24,13 @@ function Sidebar() {
     dispatch(fetchUser());
     dispatch(fetchUserSettings());
   }, [dispatch]);
+
+  useEffect(() => {
+    if(user.error === "Unauthorized") {
+      console.log("[auth] token expired, logging out.");
+      dispatch(logout());
+    }
+  }, [dispatch, user.error]);
 
   return (
     <nav className="sidebar" ref={divContainer}>
