458567bc8cb21da8e5371bf982c5d59fd34a1966
Eray Chumak <eray_chumak@protonmail.com>
2021-10-20 00:44:11 +0100

    Revert "Auto-logout if /whoami returns Unauthorized"
    This reverts commit 2d1740c10a4c242956c76f05123cb40a2ef7720b.

--- ui/src/Components/Sidebar/Index.jsx
+++ ui/src/Components/Sidebar/Index.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
 
 import { fetchUser } from "../../actions/user.js";
 import { fetchUserSettings } from "../../actions/settings.js";
 import { fetchLibraries } from "../../actions/library.js";
-import { logout } from "../../actions/auth.js";
 
 import Profile from "./Profile/Index";
 import Search from "./Search";
@@ -15,7 +14,6 @@ import General from "./General";
 import "./Index.scss";
 
 function Sidebar() {
-  const user = useSelector(store => store.user);
   const dispatch = useDispatch();
   const divContainer = useRef(null);
 
@@ -24,13 +22,6 @@ function Sidebar() {
     dispatch(fetchUser());
     dispatch(fetchUserSettings());
   }, [dispatch]);
-
-  useEffect(() => {
-    if(user.error === "Unauthorized") {
-      console.log("[auth] token expired, logging out.");
-      dispatch(logout());
-    }
-  }, [dispatch, user.error]);
 
   return (
     <nav className="sidebar" ref={divContainer}>
