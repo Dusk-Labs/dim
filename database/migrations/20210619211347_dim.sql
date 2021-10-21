91088cbba158ee59cc81b8713fa249e2645abb02
Valerian G <valerian@dusklabs.io>
2021-10-21 13:37:01 +0100

    Use left outer joins for joining assets to seasons and medias in case they dont have any assets

--- database/migrations/20210619211347_dim.sql
+++ database/migrations/20210619211347_dim.sql
@@ -43,8 +43,8 @@ CREATE TABLE _tblmedia (
 CREATE VIEW media AS
 SELECT _tblmedia.*, pp.local_path as poster_path, bp.local_path as backdrop_path
 FROM _tblmedia
-LEFT JOIN assets pp ON _tblmedia.poster = pp.id
-LEFT JOIN assets bp ON _tblmedia.backdrop = bp.id;
+LEFT OUTER JOIN assets pp ON _tblmedia.poster = pp.id
+LEFT OUTER JOIN assets bp ON _tblmedia.backdrop = bp.id;
 
 CREATE TRIGGER media_delete
 INSTEAD OF DELETE ON media
@@ -86,7 +86,7 @@ CREATE VIEW season AS
 SELECT _tblseason.id, _tblseason.season_number,
     _tblseason.tvshowid, _tblseason.added, assets.local_path as poster
 FROM _tblseason
-JOIN assets ON _tblseason.poster = assets.id;
+LEFT OUTER JOIN assets ON _tblseason.poster = assets.id;
 
 CREATE TRIGGER season_delete
 INSTEAD OF DELETE ON season
