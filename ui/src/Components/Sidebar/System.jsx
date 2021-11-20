cf828006d3b51de486d74d055463d0bed4d906da
MrTimscampi <iamtimscampi@gmail.com>
2021-11-20 17:11:22 +0100

    Re-order sidebar

--- ui/src/Components/Sidebar/System.jsx
+++ ui/src/Components/Sidebar/System.jsx
@@ -1,16 +1,16 @@
 import { NavLink } from "react-router-dom";
 
-import HomeIcon from "../../assets/Icons/Home";
+import WrenchIcon from "../../assets/Icons/Wrench";
 
 const General = () => (
   <section className="yourAccount">
     <header>
-      <h4>General</h4>
+      <h4>System</h4>
     </header>
     <div className="list">
-      <NavLink className="item" to="/" exact>
-        <HomeIcon/>
-        <p>Dashboard</p>
+      <NavLink to="/preferences" className="item">
+        <WrenchIcon/>
+        <p>Preferences</p>
       </NavLink>
     </div>
   </section>
