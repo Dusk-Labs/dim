bb81bcf88d9f4400a166f03d118eca7c58abe35d
MrTimscampi <iamtimscampi@gmail.com>
2021-11-20 16:59:11 +0100

    Make theme-color meta tag reactive

--- ui/src/Controllers/Theme.jsx
+++ ui/src/Controllers/Theme.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 
 import DefaultTheme from "../Themes/Default";
 import BlindTheme from "../Themes/Blind";
@@ -7,6 +8,7 @@ import LightsOff from "../Themes/LightsOff";
 
 function ThemeController() {
   const userSettings = useSelector(store => store.settings.userSettings);
+  const [themeColor, setThemeColor] = useState("#1a1a1a");
 
   useEffect(() => {
     switch(userSettings.data.theme) {
@@ -14,23 +16,30 @@ function ThemeController() {
         for (const prop in DefaultTheme) {
           document.documentElement.style.setProperty(`--${prop}`, `${DefaultTheme[prop]}`);
         }
+        setThemeColor(userSettings.data.theme.primaryColor);
         break;
       case "Light":
         for (const prop in BlindTheme) {
           document.documentElement.style.setProperty(`--${prop}`, `${BlindTheme[prop]}`);
         }
+        setThemeColor(userSettings.data.theme.primaryColor);
         break;
       case "Black":
         for (const prop in LightsOff) {
           document.documentElement.style.setProperty(`--${prop}`, `${LightsOff[prop]}`);
         }
+        setThemeColor(userSettings.data.theme.primaryColor);
         break;
       default:
         break;
     }
-  }, [userSettings.data.theme]);
+  }, [setThemeColor, userSettings.data.theme]);
 
-  return null;
+  return (
+    <Helmet>
+      <meta name="theme-color" content={themeColor} />
+    </Helmet>
+  );
 }
 
 export default ThemeController;
