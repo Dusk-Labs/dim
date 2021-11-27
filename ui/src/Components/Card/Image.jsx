00fcb9e0264ab1b5ca1d91caa39bf7863cc660dc
Eray Chumak <eray_chumak@protonmail.com>
2021-11-27 13:36:56 +0000

    Lazy load card images on scroll (#316)
    * Render image component upon viewport intersection
    
    * remove console log
    
    * log media object when no match is found
    
    Co-authored-by: Valerian G <valerian@dusklabs.io>

--- ui/src/Components/Card/Image.jsx
+++ ui/src/Components/Card/Image.jsx
@@ -1,29 +1,58 @@
-
+import { useRef, useState, useCallback, useEffect } from "react";
 import DimLogo from "../../assets/DimLogo";
 import ImageLoad from "../ImageLoad";
 
-const CardImage = (props) => (
-  <div className="cardImageWrapper">
-    <ImageLoad src={props.src} triggerAnimation="onHideImage">
-      {({imageSrc, loaded, error, setErr}) => (
-        <>
-          {loaded && !error && (
-            <img src={imageSrc} alt="cover" onError={() => setErr(true)}/>
+const CardImage = (props) => {
+  const imageWrapperDiv = useRef(null);
+  const [observed, setObserved] = useState(false);
+
+  const handleIntersection = useCallback((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setObserved(true);
+        observer.disconnect();
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!imageWrapperDiv.current) return;
+
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+    observer.observe(imageWrapperDiv.current);
+  }, [handleIntersection]);
+
+  return (
+    <div className="cardImageWrapper" ref={imageWrapperDiv}>
+      {observed && (
+        <ImageLoad src={props.src} triggerAnimation="onHideImage">
+          {({imageSrc, loaded, error, setErr}) => (
+            <>
+              {loaded && !error && (
+                <img src={imageSrc} alt="cover" onError={() => setErr(true)}/>
+              )}
+              {error && (
+                <div className="placeholder">
+                  <DimLogo/>
+                </div>
+              )}
+              {props.progress !== undefined && (
+                <div className="progress">
+                  <div className="value" style={{width: `${props.progress | 0}%`}}/>
+                </div>
+              )}
+            </>
           )}
-          {error && (
-            <div className="placeholder">
-              <DimLogo/>
-            </div>
-          )}
-          {props.progress !== undefined && (
-            <div className="progress">
-              <div className="value" style={{width: `${props.progress | 0}%`}}/>
-            </div>
-          )}
-        </>
+        </ImageLoad>
       )}
-    </ImageLoad>
-  </div>
-);
+    </div>
+  );
+};
 
 export default CardImage;
