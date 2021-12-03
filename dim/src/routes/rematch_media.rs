70e6e6e1e5224ab06dbe9dabea0c3396105dfee1
Valerian G <valerian@dusklabs.io>
2021-12-03 21:10:31 +0000

    add ability to rematch as a tv show.

--- dim/src/routes/rematch_media.rs
+++ dim/src/routes/rematch_media.rs
@@ -4,7 +4,6 @@ use crate::errors::*;
 use crate::scanners::base::patch_tv_metadata;
 use crate::scanners::tmdb::MediaType as ExternalMediaType;
 use crate::scanners::tmdb::Tmdb;
-use crate::scanners::tv_show::TvShowMatcher;
 use crate::scanners::movie::MovieMatcher;
 use crate::scanners::tv_show::TvShowMatcher;
 
@@ -17,9 +16,9 @@ use http::status::StatusCode;
 const API_KEY: &str = "38c372f5bc572c8aadde7a802638534e";
 
 pub mod filters {
-    use auth::Wrapper as Auth;
-    use crate::routes::global_filters::with_state;
     use crate::core::EventTx;
+    use crate::routes::global_filters::with_state;
+    use auth::Wrapper as Auth;
     use database::DbConnection;
     use serde::Deserialize;
 
@@ -43,17 +42,19 @@ pub mod filters {
             .and(with_state(event_tx))
             .and(auth::with_auth())
             .and_then(
-                |id, RouteArgs {
-                    external_id,
-                    media_type
-                }: RouteArgs,
-                conn: DbConnection,
-                event_tx: EventTx,
-                _: Auth| async move {
+                |id,
+                 RouteArgs {
+                     external_id,
+                     media_type,
+                 }: RouteArgs,
+                 conn: DbConnection,
+                 event_tx: EventTx,
+                 _: Auth| async move {
                     super::rematch_media(conn, event_tx, id, external_id, media_type)
                         .await
                         .map_err(|e| reject::custom(e))
-                })
+                },
+            )
     }
 }
 
@@ -131,7 +132,9 @@ pub async fn rematch_media(
                     event_tx: &event_tx,
                 };
 
-                matcher.inner_match(result.clone().into(), &orphan, &mut tx, Some(id)).await?;
+                matcher
+                    .inner_match(result.clone().into(), &orphan, &mut tx, Some(id))
+                    .await?;
             }
             MediaType::Tv => {
                 let matcher = TvShowMatcher {
