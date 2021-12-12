7056d8606cb2cee7f0423bee7fe6320aca6d9444
Valerian G <valerian@dusklabs.io>
2021-12-12 14:28:19 +0000

    auto-hide next episode button

--- ui/src/Pages/VideoPlayer/NextVideo/Index.jsx
+++ ui/src/Pages/VideoPlayer/NextVideo/Index.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import SelectMediaFile from "../../../Modals/SelectMediaFile/Index";
 import SelectMediaFilePlayButton from "../../../Modals/SelectMediaFile/Activators/PlayButton";
 import { fetchMediaInfo } from "../../../actions/media";
@@ -9,13 +9,22 @@ import "./Index.scss";
 function NextVideo(props) {
   const { id } = props;
   const dispatch = useDispatch();
+  const { video } = useSelector(store => ({
+    video: store.video
+  }));
+
+  const [visibile, setVisible] = useState(true);
+
+  useEffect(() => {
+    setVisible(video.idleCount <= 2);
+  }, [video.idleCount, setVisible]);
 
   useEffect(() => {
     dispatch(fetchMediaInfo(id));
   }, [dispatch]);
 
   return (
-    <div className="nextVideoOverlay">
+    <div className={`nextVideoOverlay ${visibile}`}>
       <SelectMediaFile mediaID={id}>
         <SelectMediaFilePlayButton label="Next Episode" hideIcon={true}/>
       </SelectMediaFile>
