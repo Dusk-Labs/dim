6da162099afd948f7ea941df53f685005701b695
Jimmy Cuadra <jimmycuadra@users.noreply.github.com>
2022-02-12 07:06:10 -0800

    Convert asset files to TypeScript. (#396)
    Maybe not a huge benefit from this, but it might help catch some
    mistakes from time to time. There was only one mistake the compiler
    caught in this batch, which was an attribute in the IMDB asset that was
    using "xlink" instead of "href".

--- ui/src/assets/IMDB.tsx
+++ ui/src/assets/IMDB.tsx
@@ -2,7 +2,7 @@ const DimLogo = () => (
   <svg
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
-    xlink="http://www.w3.org/1999/xlink"
+    href="http://www.w3.org/1999/xlink"
     preserveAspectRatio="xMidYMid meet"
     viewBox="0 0 575 289.83"
   >
