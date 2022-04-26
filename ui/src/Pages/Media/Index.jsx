93e82a39d14a4dae92b1dca8af01841310d3a40e
Valerian G <valerian@dusklabs.io>
2022-04-26 20:52:10 +0100

    fetch mediafile tree for tv shows and movies

--- ui/src/Pages/Media/Index.jsx
+++ ui/src/Pages/Media/Index.jsx
@@ -7,22 +7,17 @@ import Banner from "./Banner";
 import MetaContent from "./MetaContent";
 import Seasons from "./Seasons";
 
-import MatchMedia from "Pages/Library/MatchMedia/Index";
-
 import "./Index.scss";
 
 function Media() {
   const { id } = useParams();
-
   const [activeId, setActiveId] = useState(id);
-
   const { data: media } = useGetMediaQuery(id);
 
   return (
     <div className="mediaPage">
       <Banner />
       <div className="mediaContent">
-        <MatchMedia />
         <div className="meta-content">
           <MetaContent activeId={activeId} />
         </div>
