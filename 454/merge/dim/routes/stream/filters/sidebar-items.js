c7e3029d690a7ae8325382580ffe254ed767246e
vgarleanu <vgarleanu@users.noreply.github.com>
2022-06-25 19:58:30 +0000

    Deploying to docs from @ Dusk-Labs/dim@b3166fa642f77c5af6815b51e1b7c02295c60423 🚀

---
+++ 454/merge/dim/routes/stream/filters/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["get_chunk",""],["get_init",""],["get_subtitle",""],["get_subtitle_ass",""],["kill_session",""],["return_manifest",""],["return_virtual_manifest",""],["session_get_stderr",""],["should_client_hard_seek",""]]});
---
+++ 454/merge/dim/websocket/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ClientActions",""],["CtrlEvent",""]],"fn":[["event_socket",""]],"trait":[["IntoCtrlEvent",""]]});
