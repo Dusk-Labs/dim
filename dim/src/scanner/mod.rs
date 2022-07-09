c060c549064726990ef0ee23fb344d7c5c3afd1f
Valerian G <valerian@dusklabs.io>
2022-07-09 23:28:29 +0200

    add some more helper methods for starting a scan

--- dim/src/scanner/mod.rs
+++ dim/src/scanner/mod.rs
@@ -6,18 +6,23 @@ mod movie;
 mod tests;
 mod tv_show;
 
+use self::mediafile::Error as CreatorError;
+use self::mediafile::MediafileCreator;
+use super::external::filename::FilenameMetadata;
+use super::external::filename::Metadata;
+use super::external::ExternalQuery;
+use crate::core::EventTx;
+
 use anitomy::Anitomy;
 use async_trait::async_trait;
 
+use database::library::Library;
+use database::library::MediaType;
 use database::mediafile::InsertableMediaFile;
 use database::mediafile::MediaFile;
 
 use futures::FutureExt;
 use itertools::Itertools;
-
-use super::external::filename::FilenameMetadata;
-use super::external::filename::Metadata;
-use super::external::ExternalQuery;
 
 use std::ffi::OsStr;
 use std::future::Future;
@@ -25,13 +30,13 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::pin::Pin;
 use std::sync::Arc;
+use std::time::Instant;
 
 use torrent_name_parser::Metadata as TorrentMetadata;
+use tracing::info;
+use tracing::instrument;
 use tracing::warn;
 use walkdir::WalkDir;
-
-use self::mediafile::Error as CreatorError;
-use self::mediafile::MediafileCreator;
 
 pub(super) static SUPPORTED_EXTS: &[&str] = &["mp4", "mkv", "avi", "webm"];
 
@@ -113,11 +118,11 @@ pub trait MediaMatcher {
     );
 }
 
-pub async fn scan_directory(
+pub async fn insert_mediafiles(
     conn: &mut database::DbConnection,
     library_id: i64,
     dirs: Vec<impl AsRef<Path>>,
-) {
+) -> Result<Vec<WorkUnit>, Box<dyn std::error::Error>> {
     let subfiles = get_subfiles(dirs.into_iter());
     let parsed = parse_filenames(subfiles.iter());
 
@@ -144,38 +149,104 @@ pub async fn scan_directory(
             futures::future::join_all(chunk).await;
 
         for result in results {
-            insertables.push(result.expect("Failed to create insertable."));
+            insertables.push(result?);
         }
     }
 
     let mut mediafiles = vec![];
 
     for chunk in insertables.chunks(128) {
-        mediafiles.append(
-            &mut instance
-                .insert_batch(chunk.iter())
-                .await
-                .expect("Failed to insert batch."),
-        );
+        mediafiles.append(&mut instance.insert_batch(chunk.iter()).await?);
     }
 
-    let matcher = Arc::new(movie::MovieMatcher);
-    let provider = Arc::new(crate::external::mock::MockProvider);
-
-    for unit in mediafiles
+    Ok(mediafiles
         .into_iter()
         .zip(parsed.into_iter())
-        .map(|(file, (_, meta))| WorkUnit(file, meta))
-        .chunks(128)
-        .into_iter()
-    {
+        .map(|(mfile, (_, metadata))| WorkUnit(mfile, metadata))
+        .collect())
+}
+
+#[instrument(skip(conn, dirs, tx))]
+pub async fn start_custom(
+    conn: &mut database::DbConnection,
+    library_id: i64,
+    dirs: Vec<impl AsRef<Path>>,
+    tx: EventTx,
+    media_type: MediaType,
+) -> Result<(), Box<dyn std::error::Error>> {
+    info!(library_id, "Scanning library");
+
+    tx.send(
+        events::Message {
+            id: library_id,
+            event_type: events::PushEventType::EventStartedScanning,
+        }
+        .to_string(),
+    )?;
+
+    let matcher = match media_type {
+        MediaType::Movie => Arc::new(movie::MovieMatcher) as Arc<dyn MediaMatcher>,
+        _ => unimplemented!(),
+    };
+
+    let provider = Arc::new(crate::external::mock::MockProvider);
+
+    let now = Instant::now();
+    let workunits = insert_mediafiles(conn, library_id, dirs).await?;
+
+    info!(
+        library_id,
+        units = workunits.len(),
+        elapsed_ms = now.elapsed().as_millis(),
+        "Walked and inserted mediafiles."
+    );
+
+    let now = Instant::now();
+
+    // TODO: We can receive work over a channel so that we can in parallel create new mediafiles
+    // and match objects.
+    for unit in workunits.into_iter().chunks(128).into_iter() {
         let mut lock = conn.writer().lock_owned().await;
-        let mut tx = database::write_tx(&mut lock).await.unwrap();
+        let mut tx = database::write_tx(&mut lock).await?;
 
         matcher
             .batch_match(&mut tx, provider.clone(), unit.collect())
             .await;
 
-        tx.commit().await.unwrap();
+        tx.commit().await?;
     }
+
+    info!(
+        library_id,
+        elapsed_ms = now.elapsed().as_millis(),
+        "Finished scanning library."
+    );
+
+    tx.send(
+        events::Message {
+            id: library_id,
+            event_type: events::PushEventType::EventStoppedScanning,
+        }
+        .to_string(),
+    )?;
+
+    Ok(())
+}
+
+pub async fn start(
+    conn: &mut database::DbConnection,
+    library_id: i64,
+    tx: EventTx,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut tx_ = conn.read().begin().await?;
+
+    let lib = Library::get_one(&mut tx_, library_id).await?;
+
+    start_custom(conn, library_id, lib.locations, tx, lib.media_type).await
+}
+
+/// Function formats the path where assets are stored.
+pub fn format_path(x: Option<String>) -> String {
+    x.map(|x| format!("images/{}", x.trim_start_matches('/')))
+        .unwrap_or_default()
 }
