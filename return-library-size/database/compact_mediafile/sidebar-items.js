b079bd41eca1103d15f4a33b49c5e9b9a7a37013
vgarleanu <valerian.garleanu@protonmail.com>
2022-08-17 14:50:08 +0000

    Deploying to docs from @ Dusk-Labs/dim@b21ebc55f0e3dffacaab606197c9ad8d7b83e5b7 🚀

---
+++ return-library-size/database/compact_mediafile/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["CompactMediafile","A compact version of `MediaFile`. Useful in cases where we need to request some basic info for a lot of mediafiles, and as such a `SELECT *` is not viable."]]};
---
+++ return-library-size/dim/routes/auth/filters/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["admin_exists",""],["login",""],["register",""]]};
