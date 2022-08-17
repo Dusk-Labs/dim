0db26cf3990e42b21d5df81e2e1179e09c089311
vgarleanu <vgarleanu@users.noreply.github.com>
2022-08-17 14:49:49 +0000

    Deploying to docs from @ Dusk-Labs/dim@a92a3d16817f3375fdeb59a96537edb26e0af121 🚀

---
+++ 463/merge/dim/scanners/base/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["ScannerError",""]],"fn":[["patch_tv_metadata",""]],"struct":[["MetadataExtractor",""],["MetadataMatcher",""]]};
---
+++ 463/merge/source-files.js
@@ -0,0 +1,6 @@
+var sourcesIndex = {};
+sourcesIndex["auth"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["database"] = {"name":"","files":["asset.rs","compact_mediafile.rs","episode.rs","error.rs","genre.rs","lib.rs","library.rs","media.rs","mediafile.rs","movie.rs","progress.rs","query_ext.rs","rw_pool.rs","season.rs","tv.rs","user.rs","utils.rs"]};
+sourcesIndex["dim"] = {"name":"","dirs":[{"name":"external","files":["mod.rs"]},{"name":"routes","files":["auth.rs","dashboard.rs","general.rs","host.rs","invites.rs","library.rs","media.rs","mediafile.rs","mod.rs","rematch_media.rs","settings.rs","statik.rs","stream.rs","tv.rs","user.rs"]},{"name":"scanners","files":["base.rs","mod.rs","movie.rs","scanner_daemon.rs","tmdb.rs","tv_show.rs"]},{"name":"streaming","files":["ffprobe.rs","mod.rs"]}],"files":["core.rs","errors.rs","fetcher.rs","lib.rs","logger.rs","stream_tracking.rs","tree.rs","utils.rs","websocket.rs"]};
+sourcesIndex["events"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
