a42064804b6bbaf073a508b5b11b23074d231679
Jeanzle Albalate <jeanzle.albalate208@gmail.com>
2022-09-19 17:47:14 +0100

    made changes on debugging lines and const

--- ui/src/Pages/Preferences/Invites/ManageInvites.jsx
+++ ui/src/Pages/Preferences/Invites/ManageInvites.jsx
@@ -10,8 +10,6 @@ import {
 import TrashIcon from "../../../assets/Icons/Trash";
 
 function ManageInvites() {
-  const href = window.location.host;
-  console.log(href);
   const dispatch = useDispatch();
 
   const { user, auth } = useSelector((store) => ({
