46db73c83299acc34bd46cdfa6b2446e730a5690
jeanzless <100724324+jeanzless@users.noreply.github.com>
2022-09-19 17:48:56 +0100

    ui::invites: implement clipboard copy for invite token (#474)
    * ui::invites: implement clipboard copy for invite token
    
    * added error handler to clipboard copy method
    
    * added the current host/register URL on the clipboard when you copy
    
    * made changes on debugging lines and const
    
    Co-authored-by: jeanzle <jeanzle@pop-os.localdomain>

--- ui/src/Pages/Preferences/Invites/ManageInvites.jsx
+++ ui/src/Pages/Preferences/Invites/ManageInvites.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { addNotification } from "slices/notifications";
 import { formatHHMMSSDate } from "../../../Helpers/utils";
 import {
   fetchInvites,
@@ -34,18 +34,42 @@ function ManageInvites() {
     [dispatch]
   );
 
+  const toClipboard = (token) => {
+    const href = window.location.host;
+    navigator.clipboard.writeText(`${href}/register?token=${token}`).then(
+      () => {
+        dispatch(
+          addNotification({
+            msg: "Successfuly copied to your clipboard.",
+          })
+        );
+      },
+      () => {
+        dispatch(
+          addNotification({
+            msg: "There was an error copying your id to your clipboard",
+          })
+        );
+      }
+    );
+  };
+
   const tokens = auth.invites.items.map((token, i) => {
     const { hours, mins, secs, date, month, year } = formatHHMMSSDate(
       token.created
     );
 
     return (
-      <div className="token" key={i}>
+      <div className="token" key={i} onClick={() => toClipboard(token.id)}>
         <p>{token.id}</p>
         <p>
           {hours}:{mins}:{secs} on the {date}/{month}/{year}
         </p>
-        {token.claimed_by ? <p>{token.claimed_by}</p> : <p>Available</p>}
+        {token.claimed_by ? (
+          <p>Claimed by {token.claimed_by}</p>
+        ) : (
+          <p>Available</p>
+        )}
         {user.info.username !== token.claimed_by && (
           <button onClick={() => delInviteToken(token.id)}>
             <TrashIcon />
