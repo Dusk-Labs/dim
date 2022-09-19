e4cd43d281aee4a0e7137f7c7a55b7dbe09bf0d8
jeanzless <100724324+jeanzless@users.noreply.github.com>
2022-09-19 18:57:49 +0000

    Deploying to docs from @ Dusk-Labs/dim@dabf660cf216f876d1dd07a8d09ce7d96dd9d4f2 🚀

---
+++ register-improvements/database/query_ext/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"trait":[["QueryExt","Trait contains some extensions for `sqlx`."]]};
---
+++ register-improvements/dim/routes/general/filters/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["get_directory_structure",""],["search",""]]};
