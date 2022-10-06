4ec9a0111392bf5da3b68c07725f2e8258e50c00
Jeanzle Albalate <jeanzle.albalate208@gmail.com>
2022-10-06 15:54:57 +0100

    when you click on a genre you will be redirected to a library with said genre

--- ui/src/Pages/SearchResults/Index.tsx
+++ ui/src/Pages/SearchResults/Index.tsx
@@ -10,24 +10,28 @@ function SearchResults() {
   const search = location.search;
   const searchParams = new URLSearchParams(search);
   const query = searchParams.get("query");
+  const genre = searchParams.get("genre");
   const [title, setTitle] = useState(document.title);
   const {
     data: items,
     error,
     isFetching,
-  } = useSearchQuery(query ? search : skipToken);
+  } = useSearchQuery(query || genre ? search : skipToken);
 
   useEffect(() => {
+    if (genre) {
+      setTitle(`Dim - Query results for '${genre}'`);
+    }
     if (query) {
       setTitle(`Dim - Query results for '${query}'`);
     }
-  }, [query]);
+  }, [query, genre]);
 
   useEffect(() => {
     document.title = title;
-  }, [title]);
+  }, [title, genre]);
 
-  if (!query) {
+  if (!query && !genre) {
     return (
       <div className="card_list">
         No search query. Use the search box in the sidebar to search for media.
