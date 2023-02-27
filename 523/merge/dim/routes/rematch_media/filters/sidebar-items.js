7d3674f27e99e25897317a52789f041c8571a229
rodrimarchese <rodrimarchese@users.noreply.github.com>
2023-02-27 15:57:21 +0000

    Deploying to docs from @ Dusk-Labs/dim@b8094944d8f816000fadc070c208e4e0931d0186 🚀

---
+++ 523/merge/dim/routes/rematch_media/filters/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["rematch_media_by_id",""]]};
---
+++ 523/merge/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"dim":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"dim/external/enum.MediaSearchType.html\" title=\"enum dim::external::MediaSearchType\">MediaSearchType</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
