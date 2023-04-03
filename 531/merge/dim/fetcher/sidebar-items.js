934c878e36993f183d8e487a51778f6a464c70b1
vgarleanu <vgarleanu@users.noreply.github.com>
2023-04-03 18:10:31 +0000

    Deploying to docs from @ Dusk-Labs/dim@b9c576e611459975c5d08c2ab1ca0a59825972d7 🚀

---
+++ 531/merge/dim/fetcher/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["insert_into_queue",""]]};
---
+++ 531/merge/dim/websocket/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["ClientActions",""],["CtrlEvent",""]],"fn":[["event_socket",""]],"trait":[["IntoCtrlEvent",""]]};
