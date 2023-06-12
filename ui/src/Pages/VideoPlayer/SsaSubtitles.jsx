a5888baf3b1f88c3cb3ce8c581e2097722008f66
Cas <6506529+ThaUnknown@users.noreply.github.com>
2023-06-12 17:30:44 +0200

    feat: use JASSUB for ASS rendering (#552)
    * feat: use JASSUB for ASS rendering
    
    * feat: update JASSUB, don't static copy files

--- ui/src/Pages/VideoPlayer/SsaSubtitles.jsx
+++ ui/src/Pages/VideoPlayer/SsaSubtitles.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 import { VideoPlayerContext } from "./Context";
 
-import SubtitleOctopus from "@jellyfin/libass-wasm/dist/js/subtitles-octopus";
+import JASSUB from "jassub";
 
 import "./Subtitles.scss";
 
@@ -17,12 +17,12 @@ function VideoSubtitles() {
 
   const isAssEnabled = localStorage.getItem("enable_ssa") === "true";
   const isAss = !!(isAssEnabled && currentSub?.chunk_path?.endsWith("ass"));
-  const [octopus, setOctopus] = useState();
+  const [jassub, setJASSUB] = useState();
   const { videoRef } = useContext(VideoPlayerContext);
 
   useEffect(() => {
     if (
-      octopus ||
+      jassub ||
       !video.textTrackEnabled ||
       video.prevSubs === subtitle.current ||
       !isAss ||
@@ -35,23 +35,31 @@ function VideoSubtitles() {
     const chunk_path = `//${window.location.host}/api/v1/stream/${
       subtitle.list[subtitle.current].chunk_path
     }`;
+
+    JASSUB._test();
+
     const options = {
       video: videoRef.current,
       subUrl: chunk_path,
-      workerUrl: "/static/subtitles-octopus-worker.js",
+      dropAllBlur: !JASSUB._supportsSIMD,
+      workerUrl: new URL('jassub/dist/jassub-worker.js', import.meta.url).toString(),
+      wasmUrl: new URL('jassub/dist/jassub-worker.wasm', import.meta.url).toString(),
+      modernWasmUrl: new URL('jassub/dist/jassub-worker-modern.wasm', import.meta.url).toString(),
+      availableFonts: { "liberation sans": "/static/default.woff2" },
+      fonts: ["/static/default.woff2"],
     };
 
-    setOctopus(new SubtitleOctopus(options));
+    setJASSUB(new JASSUB(options));
 
     return () => {
-      console.log("[subtitle] disposing of octopus ctx");
-      if (octopus) octopus.dispose();
+      console.log("[subtitle] disposing of jassub ctx");
+      if (jassub) jassub.destroy();
     };
-  }, [video, videoRef, subtitle, isAss, setOctopus, octopus]);
+  }, [video, videoRef, subtitle, isAss, setJASSUB, jassub]);
 
   useEffect(() => {
     if (
-      !octopus ||
+      !jassub ||
       !video.textTrackEnabled ||
       video.prevSubs === subtitle.current ||
       !isAss
@@ -61,16 +69,16 @@ function VideoSubtitles() {
     const chunk_path = `//${window.location.host}/api/v1/stream/${
       subtitle.list[subtitle.current].chunk_path
     }`;
-    octopus.setTrackByUrl(chunk_path);
-  }, [octopus, video.textTrackEnabled, video.prevSubs, subtitle, isAss]);
+    jassub.setTrackByUrl(chunk_path);
+  }, [jassub, video.textTrackEnabled, video.prevSubs, subtitle, isAss]);
 
   useEffect(() => {
-    if (octopus && !isAss) {
-      console.log("[subtitle] disposing of octopus ctx");
-      octopus.dispose();
-      setOctopus(null);
+    if (jassub && !isAss) {
+      console.log("[subtitle] disposing of jassub ctx");
+      jassub.destroy();
+      setJASSUB(null);
     }
-  }, [octopus, setOctopus, isAss]);
+  }, [jassub, setJASSUB, isAss]);
 
   return null;
 }
